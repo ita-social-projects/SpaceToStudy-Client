2d9e673ee64528abbfb18c448a0905204d2e814f
marian muzyka <mrjnmzk@gmail.com>
2022-03-17 16:57:56 +0200

    add error401page

--- src/pages/error/errorPages.styles.js
+++ src/pages/error/errorPages.styles.js
@@ -1,10 +1,9 @@
 import { makeStyles } from '@mui/styles'
-import errorPhoto from '../../img/error-page/401.svg'
+import errorPhoto401 from '../../img/error-page/401.svg'
 
 export const useStyles = makeStyles((theme) => {
   return {
     root: {
-      width: '50vw',
       marginLeft: 'auto',
       marginRight: 'auto',
       whiteSpace: 'break-spaces',
@@ -12,7 +11,7 @@ export const useStyles = makeStyles((theme) => {
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'flex-start',
-      backgroundImage: `url(${errorPhoto})`,
+      backgroundImage: `url(${errorPhoto401})`,
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center right',
     }
