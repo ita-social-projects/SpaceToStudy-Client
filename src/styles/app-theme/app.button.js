5afa8dd20988c781dd3cd6d29a584fcf88d10e0c
tarasSanotskyi <badsanta2102@gmail.com>
2022-05-04 17:45:31 +0300

    button hotfix

--- src/styles/app-theme/app.button.js
+++ src/styles/app-theme/app.button.js
@@ -4,18 +4,18 @@ const button = {
   styleOverrides: {
     root: {
       lineHeight: '20px',
+      fontSize: '14px',
       opacity: '1',
     },
     sizeSmall: {
       padding: '6px 16px',
-      fontSize: '14px',
     },
     sizeMedium: {
       padding: '10px 24px',
-      fontSize: '14px',
     },
     sizeLarge: {
-      padding: '12px 24px'
+      padding: '12px 24px',
+      fontSize: '16px',
     },
     contained: {
       backgroundColor: palette.primary[900]
