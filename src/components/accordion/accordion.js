3cc50fc3138e65907793971277e36f3a9279e469
Yevhendzyuba <evgendzuba7@gmail.com>
2022-05-14 00:52:13 +0300

    added accordion

---
+++ src/components/accordion/accordion.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import { Typography } from '@mui/material'
+
+const style = {
+  accordion: {
+    borderRadius: '6px',
+    mb: '24px',
+    boxShadow: 'none',
+    '&:hover': {
+      backgroundColor: 'primary.50',
+      borderRadius: '6px'
+    },
+    '&::before': {
+      display: 'none'
+    }
+  },
+  heading: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: '20px',
+    lineHeight: '28px',
+    color: 'primary.900'
+  },
+  subHeading: {
+    fontFamily: 'Poppins',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: '14px',
+    lineHeight: '24px',
+    color: '#FFFFFF'
+  },
+  active: {
+    backgroundColor: 'primary.800',
+    borderRadius: '6px',
+    mb: '24px',
+    '& p': {
+      color: '#FFFFFF'
+    }
+  }
+}
+
+
+const Accordions = ({ id,heading,subHeading }) => {
+  const [expanded, setExpanded] = React.useState('panel1')
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false)
+  }
+  return(
+    <Accordion
+      disableGutters="true"
+      expanded={ expanded === id }
+      onChange={ handleChange(id) }
+      sx={ expanded === id ? style.active : style.accordion }
+    > 
+      <AccordionSummary>
+        <Typography sx={ style.heading }>
+          { heading }
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography sx={ style.subHeading }>
+          { subHeading }
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+  )
+}
+
+export default Accordions
+
--- src/pages/guest-home-page/GuestHomePage.js
+++ src/pages/guest-home-page/GuestHomePage.js
@@ -1,6 +1,65 @@
+import React from 'react'
 import { useContext } from 'react'
-import Comp from '~/components/testConfirmComponent/testConfirm'
+import Comp from '~/components/test-confirm-component/testConfirm'
 import { ModalContext } from '~/context/modal-context'
+import { Box } from '@mui/material'
+import MapLogo from '~/img/guest-home-page/map.jpg'
+import Accordions from '~/components/accordion/accordion'
+
+
+const style = {
+  container: {
+    margin: '0 auto',
+    maxWidth:'1128px'
+  },
+  mapSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0px',
+    position: 'absolute',
+    top: '360px'
+  },
+  image: {
+    width: '744px',
+    height: '470px',
+    mr: '24px'
+  },
+  leftBlock: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    width: '360px',
+    height: '396px'
+  },
+}
+
+const data = [ 
+  {
+    image: MapLogo,
+    id: 'panel1',
+    heading:'Flexible Location', 
+    subHeading:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+  },
+  {
+    // image: '../img/guest-home-page/map.jpg',
+    id: 'panel2',
+    heading:'Individual Time', 
+    subHeading:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+  },
+  {
+    id: 'panel3',
+    heading:'Free choice of teachers', 
+    subHeading:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+  },
+  {
+    
+    id: 'panel4',
+    heading:'Digital Communication', 
+    subHeading:'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+  }
+]
 
 const GuestHomePage = () => {
   const { setModal } = useContext(ModalContext)
@@ -8,14 +67,34 @@ const GuestHomePage = () => {
   const handleModal = () => {
     setModal(<Comp />)
   }
-
+  
+  
   return (
-    <div>
-      This Is GuestHomePage Component
+    <Box sx={ style.container }>
       
       <button onClick={ handleModal }>click</button>
-    </div>
+
+      
+      <Box sx={ style.mapSection }>
+        <Box component="img" src={ MapLogo } sx={ style.image }></Box>
+        <Box sx={ style.leftBlock }>
+          { data.map(({ id,heading,subHeading }) => 
+            (<Accordions
+              heading={ heading } 
+              id={ id }
+              key={ id }
+              subHeading={ subHeading }
+              // eslint-disable-next-line react/jsx-closing-bracket-location
+            />)
+          ) 
+          } 
+        </Box>
+      </Box>
+    </Box>
+    
+    
   )
 }
 
 export default GuestHomePage
+
