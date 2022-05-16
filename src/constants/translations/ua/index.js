263e0cae8e0145e3cfea350f91ffbb3d4e1af417
Yevhendzyuba <evgendzuba7@gmail.com>
2022-05-16 13:53:57 +0300

    fix translations

--- src/constants/translations/ua/index.js
+++ src/constants/translations/ua/index.js
@@ -4,9 +4,10 @@ import button from './button.json'
 import header from './header.json'
 import questions from './questions.json'
 import titles from './titles.json'
+import accordion from './accordion.json'
 
-const ua = {
-  translations: { common, errorPage, button, header, questions, titles }
+const ua = { 
+  translations: { common, errorPage, button, header, questions, titles, accordion } 
 }
 
 export default ua
