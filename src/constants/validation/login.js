3519e9bdc70c1e15bc373a19647f679235c117dd
tarasSanotskyi <badsanta2102@gmail.com>
2022-06-02 10:52:32 +0300

    add ukrainian letters in password regexp

--- src/constants/validation/login.js
+++ src/constants/validation/login.js
@@ -8,7 +8,7 @@ export const email = (value) => {
 
 export const password = (value) => {
   let helperText
-  if (!RegExp(/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).)\S/).test(value)) helperText = 'login.errorMessages.passwordValid'
+  if (!RegExp(/^((?=\S*?[A-ZА-Я])(?=\S*?[a-zа-я])(?=\S*?[0-9]).)\S/).test(value)) helperText = 'login.errorMessages.passwordValid'
   if (value.length < 6 || value.length > 30) helperText = 'login.errorMessages.passwordLength'
   return helperText
 }
