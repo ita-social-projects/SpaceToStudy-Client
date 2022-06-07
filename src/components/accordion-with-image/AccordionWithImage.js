331d4cc1004d5ce6f494f7cfde56e72240469aac
Yevhen Dzyuba <evgendzuba7@gmail.com>
2022-06-07 23:39:53 +0300

    Add mobile adaptivity  (#69)
    * add addaptivity for mobile
    
    * fix
    
    * fix

--- src/components/accordion-with-image/AccordionWithImage.js
+++ src/components/accordion-with-image/AccordionWithImage.js
@@ -5,32 +5,32 @@ import { useState } from 'react'
 const style = {
   feature: {
     display: 'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    px:'24px',
-    overflow:'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+    px: '24px',
+    overflow: 'auto'
   },
   image: {
-    maxHeight:'470px',
-    overflow:'auto',
+    maxHeight: '470px',
+    overflow: 'auto',
     mr: '24px'
   }
 }
 
 const AccordionWithImage = ({ items }) => {
-
   const [activeItemId, setActiveItemId] = useState(0)
- 
-  return(
+
+  return (
     <Box sx={ style.feature }>
       <Box component="img" src={ items[activeItemId].image } sx={ style.image } />
       <Accordions
-        activeIndex={ activeItemId } items={ items } onChange={ (id) => setActiveItemId(id) }
-        styles={ { maxWidth:{ md:'360px',sm:'229px' } } }
+        activeIndex={ activeItemId }
+        items={ items }
+        onChange={ (id) => setActiveItemId(id) }
+        styles={ { maxWidth: { md: '360px', sm: '229px' } } }
       />
     </Box>
   )
 }
-
 
 export default AccordionWithImage
---
+++ src/components/carousel-with-image/CarouselWithImage.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import { useTranslation } from 'react-i18next'
+import MobileStepper from '@mui/material/MobileStepper'
+import Typography from '@mui/material/Typography'
+import SwipeableViews from 'react-swipeable-views'
+import { style } from './carousel-with-image.styles'
+
+const CarouselWithImage = ({ items }) => {
+  const { t } = useTranslation()
+  const [activeStep, setActiveStep] = useState(0)
+  const maxSteps = items.length
+
+  const handleStepChange = (step) => {
+    setActiveStep(step)
+  }
+
+  const carouselWrapper = items.map((item, index) => (
+    <Box key={ index } sx={ style.feature }>
+      <Box component="img" src={ item.image } sx={ style.image } />
+      <Box sx={ style.text }>
+        <Typography sx={ { color: 'basic.white' } } variant={ 'h6' }>
+          { t(item.title) }
+        </Typography>
+        <Typography sx={ { color: 'basic.white', fontSize: '14px' } } variant={ 'subtitle1' }>
+          { t(item.description) }
+        </Typography>
+      </Box>
+      <MobileStepper
+        activeStep={ activeStep } position="static" steps={ maxSteps }
+        sx={ { pt: '16px' } } variant="dots"
+      />
+    </Box>
+  ))
+
+  return (
+    <SwipeableViews enableMouseEvents index={ activeStep } onChangeIndex={ handleStepChange }>
+      { carouselWrapper }
+    </SwipeableViews>
+  )
+}
+export default CarouselWithImage
