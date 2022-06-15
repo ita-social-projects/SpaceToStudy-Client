2584358b21c88b789ef0845fa0bd9cbbcb5add6d
tarasSanotskyi <badsanta2102@gmail.com>
2022-06-15 22:08:48 +0300

    fix

--- src/pages/guest-home-page/GuestHome.js
+++ src/pages/guest-home-page/GuestHome.js
@@ -1,15 +1,16 @@
 import { Box } from '@mui/material'
-import AccordionWithImage from '~/components/accordion-with-image/AccordionWithImage'
 import { descriptionTimes } from '~/components/accordion-with-image/descriptionTimes'
 import WelcomeBlock from '~/containers/guest-home-page/WelcomeBlock'
+import FeatureBlock from '~/containers/guest-home-page/FeatureBlock'
 
 const GuestHomePage = () => {
-
+  
   return (
     <Box>
       <WelcomeBlock />
-      <AccordionWithImage items={ descriptionTimes }  />
+      <FeatureBlock items={ descriptionTimes }  />
     </Box>
   )
 }
+
 export default GuestHomePage
