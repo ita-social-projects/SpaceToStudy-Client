1a548ed35d4e6aa764b61c26269642e627dc3433
DeLettenhove <deletten@gmail.com>
2022-07-03 23:18:49 +0300

    Write tests for useBreakpoints custom hook

---
+++ tests/unit/hooks/use-breakpoints.spec.js
@@ -0,0 +1,38 @@
+import { renderHook } from '@testing-library/react-hooks'
+import mediaQuery from 'css-mediaquery'
+
+import useBreakpoints from '~/hooks/use-breakpoints'
+
+const createMatchMedia = (width) => {
+  return (query) => ({
+    matches: mediaQuery.match(query, { width }),
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  })
+}
+
+describe('useBreakpoints custom hook', () => {
+  it('should return correct value for desktop', () => {
+    window.matchMedia = createMatchMedia(1444)
+
+    const { result } = renderHook(useBreakpoints)
+
+    expect(result.current).toBe('desktop')
+  })
+
+  it('should return correct value for tablet', () => {
+    window.matchMedia = createMatchMedia(768)
+
+    const { result } = renderHook(useBreakpoints)
+
+    expect(result.current).toBe('tablet')
+  })
+
+  it('should return correct value for mobile', () => {
+    window.matchMedia = createMatchMedia(375)
+
+    const { result } = renderHook(useBreakpoints)
+
+    expect(result.current).toBe('mobile')
+  })
+})
