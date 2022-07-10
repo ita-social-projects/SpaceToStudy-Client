388622a991fba5e3997eb55aea7970d28b706ffa
Yevhendzyuba <evgendzuba7@gmail.com>
2022-07-11 00:31:04 +0300

    fix descriptions

--- tests/unit/pages/error/InternalServerError.spec.js
+++ tests/unit/pages/error/InternalServerError.spec.js
@@ -5,17 +5,17 @@ describe('InternalServerError page test', () => {
   beforeEach(() => {
     render(<InternalServerError />)
   })
-  it('Should test if title is in the document', () => {
+  it('should render title', () => {
     const title = screen.getByText('errorPage.500.title')
 
     expect(title).toBeInTheDocument()
   })
-  it('Should test if description is in the document', () => {
+  it('should render description', () => {
     const description = screen.getByText('errorPage.500.description')
 
     expect(description).toBeInTheDocument()
   })
-  it('Should test if error logo is in the document', () => {
+  it('should render error logo', () => {
     const errorLogo = screen.getByAltText('errorLogo')
 
     expect(errorLogo).toBeInTheDocument()
