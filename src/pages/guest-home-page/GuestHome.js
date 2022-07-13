e808a49b7d2c14472acc46e2477bd09ca6a487b2
Yevhendzyuba <evgendzuba7@gmail.com>
2022-07-13 09:55:38 +0300

    add iconbar mapping

--- src/pages/guest-home-page/GuestHome.js
+++ src/pages/guest-home-page/GuestHome.js
@@ -3,10 +3,10 @@ import { Box } from '@mui/material'
 import Welcome from '~/containers/guest-home-page/Welcome'
 import FeatureBlock from '~/containers/guest-home-page/FeatureBlock'
 import WhatCanYouDo from '~/containers/guest-home-page/WhatCanYouDo'
-import HowItWorks from '~/containers/guest-home-page/HowItWorks'
+import HowItWorks from '~/containers/guest-home-page/how-it-works/HowItWorks'
 import WhoWeAre from '~/containers/guest-home-page/who-we-are/WhoWeAre'
 import { descriptionTimes } from '~/components/accordion-with-image/descriptionTimes'
-
+import { iconBarArray } from '~/containers/guest-home-page/how-it-works/iconBarArray'
 const GuestHomePage = () => {
   
   return (
@@ -14,7 +14,7 @@ const GuestHomePage = () => {
       <Welcome />
       <FeatureBlock items={ descriptionTimes }  />
       <WhatCanYouDo />
-      <HowItWorks />
+      <HowItWorks items={ iconBarArray } />
       <WhoWeAre />
     </Box>
   )
