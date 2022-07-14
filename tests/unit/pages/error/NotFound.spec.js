0e60799baf9e5d0475de99f7988d7f2b84ff3e94
tarasSanotskyi <badsanta2102@gmail.com>
2022-07-14 22:48:14 +0300

    create NotFound page

---
+++ tests/unit/pages/error/NotFound.spec.js
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react'
+import NotFound from '~/pages/error/NotFound'
+import { renderWithProviders } from '~tests/test-utils'
+
+describe('InternalServerError page test', () => {
+  
+  beforeEach(() => {
+    renderWithProviders(<NotFound />)
+  })
+
+  it('should render title', () => {
+    const title = screen.getByText('errorPage.404.title')
+
+    expect(title).toBeInTheDocument()
+  })
+
+  it('should render description', () => {
+    const description = screen.getByText('errorPage.404.description')
+
+    expect(description).toBeInTheDocument()
+  })
+
+  it('should render error logo', () => {
+    const errorLogo = screen.getByAltText('flowerpot')
+
+    expect(errorLogo).toBeInTheDocument()
+  })
+})
