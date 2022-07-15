0855da3c7dda32e82df74d7a9f9959373bcac45c
Rostyslav Martynyk <martynykrostyslaw@gmail.com>
2022-07-15 11:43:18 +0300

    delete clearLocalStorage

--- src/services/local-storage-service.js
+++ src/services/local-storage-service.js
@@ -1,32 +1,26 @@
 import { s2s } from '~/constants'
 
-export const clearLocalStorage = () => {
-  const localObject = {
-    accessToken: null
-  }
-  localStorage.setItem(s2s, JSON.stringify(localObject))
-}
-
-if (!localStorage.getItem(s2s)) {
-  clearLocalStorage()
-}
-
 export const getFromLocalStorage = (name) => {
   const localObject = JSON.parse(localStorage.getItem(s2s))
-
+  if (!localObject) {
+    return null
+  }
   return localObject[name]
 }
 
 export const setToLocalStorage = (name, item) => {
-  const localObject = JSON.parse(localStorage.getItem(s2s))
+  let localObject = JSON.parse(localStorage.getItem(s2s))
+  if (!localObject) {
+    localObject = {}
+  }
   localObject[name] = item
-
   localStorage.setItem(s2s, JSON.stringify(localObject))
 }
 
 export const removeFromLocalStorage = (name) => {
   const localObject = JSON.parse(localStorage.getItem(s2s))
-  delete localObject[name]
-
-  localStorage.setItem(s2s, JSON.stringify(localObject))
+  if (localObject) {
+    delete localObject[name]
+    localStorage.setItem(s2s, JSON.stringify(localObject))
+  }
 }
