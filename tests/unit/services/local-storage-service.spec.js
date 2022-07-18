ac5a61e2aef7803a215e5c29a720d8a3b06dcfac
Rostyslav Martynyk <martynykrostyslaw@gmail.com>
2022-07-18 18:43:45 +0300

    wrote tests

---
+++ tests/unit/services/local-storage-service.spec.js
@@ -0,0 +1,54 @@
+import { getFromLocalStorage, setToLocalStorage, removeFromLocalStorage } from '~/services/local-storage-service'
+
+describe('Local storage service test', () => {
+  it('should return null from local storage', () => {
+    Object.defineProperty(window, 'localStorage', {
+      configurable: true,
+      writable: true,
+      value: {
+        getItem: () => null,
+        setItem: jest.fn()
+      }
+    })
+
+    const token = getFromLocalStorage('token')
+    expect(token).toBe(null)
+  })
+
+  it('should set item if local storage is empty', () => {
+    setToLocalStorage('accessToken', 'token')
+    expect(window.localStorage.setItem).toBeCalled()
+  })
+
+  it('should not remove item from local storage', () => {
+    removeFromLocalStorage('accessToken')
+    expect(window.localStorage.setItem).not.toBeCalled()
+  })
+
+  it('should return item from local storage', () => {
+    Object.defineProperty(window, 'localStorage', {
+      configurable: true,
+      writable: true,
+      value: {
+        getItem: () =>
+          JSON.stringify({
+            accessToken: 'token'
+          }),
+        setItem: jest.fn()
+      }
+    })
+
+    const token = getFromLocalStorage('accessToken')
+    expect(token).toBe('token')
+  })
+
+  it('should set item to local storage', () => {
+    setToLocalStorage('accessToken', 'token')
+    expect(window.localStorage.setItem).toBeCalled()
+  })
+
+  it('should remove item from local storage', () => {
+    removeFromLocalStorage('accessToken')
+    expect(window.localStorage.setItem).toBeCalled()
+  })
+})
