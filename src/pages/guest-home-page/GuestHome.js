392d862d9ad4f24e6a2f2a53e3310cbe4a345daa
tarasSanotskyi <badsanta2102@gmail.com>
2022-07-18 18:43:21 +0300

    scroll to top

--- src/pages/guest-home-page/GuestHome.js
+++ src/pages/guest-home-page/GuestHome.js
@@ -4,7 +4,6 @@ import Welcome from '~/containers/guest-home-page/Welcome'
 import FeatureBlock from '~/containers/guest-home-page/FeatureBlock'
 import WhatCanYouDo from '~/containers/guest-home-page/WhatCanYouDo'
 import WhoWeAre from '~/containers/guest-home-page/who-we-are/WhoWeAre'
-import Footer from '~/containers/footer/Footer'
 import { descriptionTimes } from '~/components/accordion-with-image/descriptionTimes'
 
 const GuestHomePage = () => {
@@ -15,7 +14,6 @@ const GuestHomePage = () => {
       <FeatureBlock items={ descriptionTimes }  />
       <WhatCanYouDo />
       <WhoWeAre />
-      <Footer />
     </Box>
   )
 }
