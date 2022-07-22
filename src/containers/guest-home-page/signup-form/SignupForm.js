e70490eca5bbbc6a70a51c3d8a4393c3622ace3b
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-07-22 11:23:33 +0300

    fixed eslint errors

--- src/containers/guest-home-page/signup-form/SignupForm.js
+++ src/containers/guest-home-page/signup-form/SignupForm.js
@@ -14,104 +14,115 @@ const SignupForm = ({ handleSubmit, handleChange, handleBlur, data, errors }) =>
   )
 
   const policyAgreement = (
-    <Box sx={style.box}>
-      <Typography variant="subtitle2">{t('signup.iAgree')}</Typography>
-      <Typography component={Link} sx={style.underlineText} to={'/'} variant="subtitle2">
-        {t('common.labels.terms')}
+    <Box sx={ style.box }>
+      <Typography variant="subtitle2">
+        { t('signup.iAgree') }
       </Typography>
-      <Typography sx={{ ml: '5px' }} variant="subtitle2">
-        {t('signup.and')}
+      <Typography
+        component={ Link } sx={ style.underlineText } to={ '/' }
+        variant="subtitle2"
+      >
+        { t('common.labels.terms') }
       </Typography>
-      <Typography component={Link} sx={style.underlineText} to={'/'} variant="subtitle2">
-        {t('common.labels.privacyPolicy')}
+      <Typography sx={ { ml: '5px' } } variant="subtitle2">
+        { t('signup.and') }
+      </Typography>
+      <Typography
+        component={ Link } sx={ style.underlineText } to={ '/' }
+        variant="subtitle2"
+      >
+        { t('common.labels.privacyPolicy') }
       </Typography>
     </Box>
   )
 
   return (
-    <Box component="form" onSubmit={handleSubmit}>
-      <Box sx={{ display: { md: 'block', lg: 'flex' }, gap: '15px' }}>
+    <Box component="form" onSubmit={ handleSubmit }>
+      <Box sx={ { display: { md: 'block', lg: 'flex' }, gap: '15px' } }>
         <AppTextField
           autoFocus
-          errorMsg={t(errors.firstName)}
+          errorMsg={ t(errors.firstName) }
           fullWidth
-          label={t('common.labels.firstName')}
-          onBlur={handleBlur('firstName')}
-          onChange={handleChange('firstName')}
+          label={ t('common.labels.firstName') }
+          onBlur={ handleBlur('firstName') }
+          onChange={ handleChange('firstName') }
           required
           size="large"
-          sx={{ mb: '5px' }}
+          sx={ { mb: '5px' } }
           type="text"
-          value={data.firstName}
+          value={ data.firstName }
         />
 
         <AppTextField
-          errorMsg={t(errors.lastName)}
+          errorMsg={ t(errors.lastName) }
           fullWidth
-          label={t('common.labels.lastName')}
-          onBlur={handleBlur('lastName')}
-          onChange={handleChange('lastName')}
+          label={ t('common.labels.lastName') }
+          onBlur={ handleBlur('lastName') }
+          onChange={ handleChange('lastName') }
           required
           size="large"
-          sx={{ mb: '5px' }}
+          sx={ { mb: '5px' } }
           type="text"
-          value={data.lastName}
+          value={ data.lastName }
         />
       </Box>
 
       <AppTextField
-        errorMsg={t(errors.email)}
+        errorMsg={ t(errors.email) }
         fullWidth
-        label={t('common.labels.email')}
-        onBlur={handleBlur('email')}
-        onChange={handleChange('email')}
+        label={ t('common.labels.email') }
+        onBlur={ handleBlur('email') }
+        onChange={ handleChange('email') }
         required
         size="large"
-        sx={{ mb: '5px' }}
+        sx={ { mb: '5px' } }
         type="email"
-        value={data.email}
+        value={ data.email }
       />
 
       <AppTextField
-        InputProps={passwordVisibility}
-        errorMsg={t(errors.password)}
+        InputProps={ passwordVisibility }
+        errorMsg={ t(errors.password) }
         fullWidth
-        label={t('common.labels.password')}
-        onBlur={handleBlur('password')}
-        onChange={handleChange('password')}
+        label={ t('common.labels.password') }
+        onBlur={ handleBlur('password') }
+        onChange={ handleChange('password') }
         required
         size="large"
-        sx={{ mb: '5px' }}
-        type={showPassword ? 'text' : 'password'}
-        value={data.password}
+        sx={ { mb: '5px' } }
+        type={ showPassword ? 'text' : 'password' }
+        value={ data.password }
       />
 
       <AppTextField
-        InputProps={confirmPasswordVisibility}
-        errorMsg={t(errors.confirmPassword)}
+        InputProps={ confirmPasswordVisibility }
+        errorMsg={ t(errors.confirmPassword) }
         fullWidth
-        label={t('common.labels.confirmPassword')}
-        onBlur={handleBlur('confirmPassword')}
-        onChange={handleChange('confirmPassword')}
+        label={ t('common.labels.confirmPassword') }
+        onBlur={ handleBlur('confirmPassword') }
+        onChange={ handleChange('confirmPassword') }
         required
         size="large"
-        type={showConfirmPassword ? 'text' : 'password'}
-        value={data.confirmPassword}
+        type={ showConfirmPassword ? 'text' : 'password' }
+        value={ data.confirmPassword }
       />
 
-      <Box sx={style.checkboxContainer}>
+      <Box sx={ style.checkboxContainer }>
         <FormControlLabel
-          control={<Checkbox />}
-          label={policyAgreement}
+          control={ <Checkbox /> }
+          label={ policyAgreement }
           labelPlacement="end"
           size="large"
-          sx={style.checkboxLabel}
+          sx={ style.checkboxLabel }
           variant="subtitle2"
         />
       </Box>
 
-      <Button size="large" sx={style.signupButton} type="submit" variant="contained">
-        {t('common.labels.signup')}
+      <Button
+        size="large" sx={ style.signupButton } type="submit"
+        variant="contained"
+      >
+        { t('common.labels.signup') }
       </Button>
     </Box>
   )
