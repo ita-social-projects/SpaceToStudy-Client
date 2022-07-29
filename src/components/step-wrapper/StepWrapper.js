ff98dbf19117c98afd3bda8270e9d436479c46f8
Rostyslav Martynyk <martynykrostyslaw@gmail.com>
2022-07-29 16:18:10 +0300

    added translation

--- src/components/step-wrapper/StepWrapper.js
+++ src/components/step-wrapper/StepWrapper.js
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material'
 import { Box } from '@mui/system'
 import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import backArrow from '~/assets/img/step-wrapper/backArrow.svg'
 import nextArrow from '~/assets/img/step-wrapper/nextArrow.svg'
@@ -9,6 +10,8 @@ import { styles } from './step-wrapper.styles'
 
 const StepWrapper = ({ children, steps }) => {
   const [activeStep, setActiveStep] = useState(0)
+
+  const { t } = useTranslation()
 
   const isLastStep = activeStep === steps.length - 1
 
@@ -50,14 +53,16 @@ const StepWrapper = ({ children, steps }) => {
           sx={ [styles.btn, { color: 'primary.500' }] }
           variant="outlined"
         >
-          <Box alt="backArrow" component="img" src={ backArrow }></Box>
-          <Box sx={ { ml: '10px' } }>Back</Box>
+          <Box alt="backArrow" component="img" src={ backArrow } />
+          <Box sx={ { ml: '10px' } }>
+            { t('common.back') }
+          </Box>
         </Button>
         <Button onClick={ next } sx={ [styles.btn, { ml: '168px', color: 'primary.50' }] } variant="contained">
           <Box sx={ { mr: '10px' } }>
-            { isLastStep ? 'Finish' : 'Next' }
+            { isLastStep ? t('common.finish') : t('common.next') }
           </Box>
-          <Box alt="nextArrow" component="img" src={ nextArrow }></Box>
+          <Box alt="nextArrow" component="img" src={ nextArrow } />
         </Button>
       </Box>
     </Box>
