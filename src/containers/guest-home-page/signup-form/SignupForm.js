ef977f78d222c3added8261731852ce78a82c5d2
Denys Zakharkevych <84774115+RestingState@users.noreply.github.com>
2022-07-29 12:01:50 +0300

    change registration layout (#188)
    * change registration layout
    
    * added new line
    
    * minor changes

--- src/containers/guest-home-page/signup-form/SignupForm.js
+++ src/containers/guest-home-page/signup-form/SignupForm.js
@@ -1,128 +1,128 @@
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
-import { Box, FormControlLabel, Typography, TextField, Button, Checkbox } from '@mui/material'
+import { Box, FormControlLabel, Typography, Button, Checkbox } from '@mui/material'
 import useInputVisibility from '~/hooks/use-input-visibility'
+import AppTextField from '~/components/app-text-field/AppTextField'
 
 import { style } from './signup-form.style'
 
 const SignupForm = ({ handleSubmit, handleChange, handleBlur, data, errors }) => {
   const { t } = useTranslation()
   const { inputVisibility: passwordVisibility, showInputText: showPassword } = useInputVisibility(errors.password)
-  const { inputVisibility: confirmPasswordVisibility, showInputText: showConfirmPassword } = useInputVisibility(errors.confirmPassword)
+  const { inputVisibility: confirmPasswordVisibility, showInputText: showConfirmPassword } = useInputVisibility(
+    errors.confirmPassword
+  )
 
   const policyAgreement = (
     <Box sx={ style.box }>
       <Typography variant='subtitle2'>
         { t( 'signup.iAgree' ) }
       </Typography>
-      <Typography 
-        component={ Link }  
-        sx={ style.underlineText } to={ '/' } 
-        variant='subtitle2'
+      <Typography
+        component={ Link } sx={ style.underlineText } to={ '/' }
+        variant="subtitle2"
       >
         { t( 'common.labels.terms' ) }
       </Typography>
       <Typography sx={ { ml: '5px' } } variant='subtitle2'>
         { t( 'signup.and' ) }
       </Typography>
-      <Typography 
-        component={ Link }  
-        sx={ style.underlineText } to={ '/' } 
-        variant='subtitle2'
+      <Typography
+        component={ Link } sx={ style.underlineText } to={ '/' }
+        variant="subtitle2"
       >
         { t( 'common.labels.privacyPolicy' ) }
       </Typography>
     </Box>
   )
-    
+
   return (
     <Box component='form' onSubmit={ handleSubmit }>
-      <TextField
-        autoFocus
-        error={ Boolean(errors.firstName) }
-        fullWidth 
-        helperText={ t(errors.firstName, { name: 'First name' }) }
-        label={ t( 'common.labels.firstName' ) }
-        onBlur={ handleBlur('firstName') }
-        onChange={ handleChange('firstName') }
-        required
-        size='large'
-        sx={ { mb: '16px' } }
-        type='text'
-        value={ data.firstName }
-      />
+      <Box sx={ { display: { md: 'block', lg: 'flex' }, gap: '15px' } }>
+        <AppTextField
+          autoFocus
+          errorMsg={ t(errors.firstName) }
+          fullWidth
+          label={ t('common.labels.firstName') }
+          onBlur={ handleBlur('firstName') }
+          onChange={ handleChange('firstName') }
+          required
+          size="large"
+          sx={ { mb: '5px' } }
+          type="text"
+          value={ data.firstName }
+        />
 
-      <TextField
-        error={ Boolean(errors.lastName) }
-        fullWidth 
-        helperText={ t(errors.lastName, { name: 'Last name' }) }
-        label={ t( 'common.labels.lastName' ) }
-        onBlur={ handleBlur('lastName') }
-        onChange={ handleChange('lastName') }
-        required
-        size='large'
-        sx={ { mb: '16px' } }
-        type='text'
-        value={ data.lastName }
-      />
+        <AppTextField
+          errorMsg={ t(errors.lastName) }
+          fullWidth
+          label={ t('common.labels.lastName') }
+          onBlur={ handleBlur('lastName') }
+          onChange={ handleChange('lastName') }
+          required
+          size="large"
+          sx={ { mb: '5px' } }
+          type="text"
+          value={ data.lastName }
+        />
+      </Box>
 
-      <TextField
-        error={ Boolean(errors.email) }
-        fullWidth 
-        helperText={ t(errors.email) }
-        label={ t( 'common.labels.email' ) }
+      <AppTextField
+        errorMsg={ t(errors.email) }
+        fullWidth
+        label={ t('common.labels.email') }
         onBlur={ handleBlur('email') }
         onChange={ handleChange('email') }
         required
-        size='large'
-        sx={ { mb: '16px' } }
-        type='email'
+        size="large"
+        sx={ { mb: '5px' } }
+        type="email"
         value={ data.email }
       />
-      
-      <TextField 
+
+      <AppTextField
         InputProps={ passwordVisibility }
-        error={ Boolean(errors.password) } 
+        errorMsg={ t(errors.password) }
         fullWidth
-        helperText={ t(errors.password) }
-        label={ t( 'common.labels.password' ) }
+        label={ t('common.labels.password') }
         onBlur={ handleBlur('password') }
         onChange={ handleChange('password') }
         required
-        sx={ { mb: '16px' } }
-        type={ (showPassword ? 'text' : 'password') }
+        size="large"
+        sx={ { mb: '5px' } }
+        type={ showPassword ? 'text' : 'password' }
         value={ data.password }
       />
 
-      <TextField 
+      <AppTextField
         InputProps={ confirmPasswordVisibility }
-        error={ Boolean(errors.confirmPassword) } 
+        errorMsg={ t(errors.confirmPassword) }
         fullWidth
-        helperText={ t(errors.confirmPassword) }
-        label={ t( 'common.labels.confirmPassword' ) }
+        label={ t('common.labels.confirmPassword') }
         onBlur={ handleBlur('confirmPassword') }
         onChange={ handleChange('confirmPassword') }
         required
-        type={ (showConfirmPassword ? 'text' : 'password') }
+        size="large"
+        type={ showConfirmPassword ? 'text' : 'password' }
         value={ data.confirmPassword }
       />
-        
-      <Box sx={ style.checkboxContainer } >
+
+      <Box sx={ style.checkboxContainer }>
         <FormControlLabel
           control={ <Checkbox /> }
           label={ policyAgreement }
-          labelPlacement='end'
-          size='large'
+          labelPlacement="end"
+          size="large"
           sx={ style.checkboxLabel }
-          variant='subtitle2'
-        />       
+          variant="subtitle2"
+        />
       </Box>
-        
+
       <Button
-        size='large' sx={ style.signupButton } type='submit'
+        size="large" sx={ style.signupButton } type="submit"
         variant="contained"
       >
-        { t( 'common.labels.signup' ) }
+        { t('common.labels.signup') }
       </Button>
     </Box>
   )
