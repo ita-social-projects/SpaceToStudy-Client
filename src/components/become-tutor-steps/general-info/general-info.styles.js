2b44cda23b8e9df24e956bad7038aeac66941c05
Lubomyr <lubomrkashuba@gmail.com>
2022-08-04 18:02:28 +0300

    corrections

--- src/components/become-tutor-steps/general-info/general-info.styles.js
+++ src/components/become-tutor-steps/general-info/general-info.styles.js
@@ -1,11 +1,11 @@
 export const styles = {
   container: {
     display: 'flex',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    columnGap: 2
   },
   img: {
-    borderRadius: '20px',
-    overflow: 'auto'
+    borderRadius: '20px'
   },
   form: {
     maxWidth: '360px',
