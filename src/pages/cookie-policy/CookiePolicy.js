d926560c3564d1c72f1358860eac4dfa1b4804c4
Anatoliy Sarakhman <anatolej1111@gmail.com>
2022-08-10 16:16:21 +0300

    fix dublication

--- src/pages/cookie-policy/CookiePolicy.js
+++ src/pages/cookie-policy/CookiePolicy.js
@@ -1,10 +1,30 @@
 import { Container, Typography, Box } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 
+import { cookieItemsData } from './cookie-policy.constants'
+
 import { styles } from './cookie-policy.styles'
 
 const CookiePolicy = () => {
   const { t } = useTranslation()
+
+  const cookieItems = cookieItemsData.map((item, index) => {
+    return (
+      <Box key={ index } sx={ styles.wrapper }>
+        <Typography sx={ styles.titleStyles }>
+          { t(item.title) }
+        </Typography>
+
+        <Typography sx={ item.subtitle && styles.subtitleStyles }>
+          { t(item.subtitle) }
+        </Typography>
+
+        <Typography sx={ styles.descriptionStyles }>
+          { t(item.description) }
+        </Typography>
+      </Box>
+    )
+  })
 
   return (
     <Container data-testid="sectionContainer" sx={ styles.container }>
@@ -17,94 +37,7 @@ const CookiePolicy = () => {
           { t('cookiePolicyPage.cookiePolicy.description') }
         </Typography>
       </Box>
-
-      <Box sx={ styles.wrapper }>
-        <Typography sx={ styles.titleStyles }>
-          { t('cookiePolicyPage.whatAreCookies.title') }
-        </Typography>
-
-        <Typography sx={ styles.descriptionStyles }>
-          { t('cookiePolicyPage.whatAreCookies.description') }
-        </Typography>
-      </Box>
-
-      <Box sx={ styles.wrapper }>
-        <Typography sx={ styles.titleStyles }>
-          { t('cookiePolicyPage.howWeUseCookies.title') }
-        </Typography>
-
-        <Typography sx={ styles.descriptionStyles }>
-          { t('cookiePolicyPage.howWeUseCookies.description') }
-        </Typography>
-      </Box>
-
-      <Box sx={ styles.wrapper }>
-        <Typography sx={ styles.titleStyles }>
-          { t('cookiePolicyPage.disablingCookies.title') }
-        </Typography>
-
-        <Typography sx={ styles.descriptionStyles }>
-          { t('cookiePolicyPage.disablingCookies.description') }
-        </Typography>
-      </Box>
-
-      <Box sx={ styles.wrapper }>
-        <Typography sx={ styles.titleStyles }>
-          { t('cookiePolicyPage.theCookiesWeSet.title') }
-        </Typography>
-
-        <Typography sx={ styles.subtitleStyles }>
-          { t('cookiePolicyPage.theCookiesWeSet.account.title') }
-        </Typography>
-
-        <Typography sx={ styles.descriptionStyles }>
-          { t('cookiePolicyPage.theCookiesWeSet.account.description') }
-        </Typography>
-      </Box>
-
-      <Box sx={ styles.wrapper }>
-        <Typography sx={ styles.subtitleStyles }>
-          { t('cookiePolicyPage.theCookiesWeSet.login.title') }
-        </Typography>
-
-        <Typography sx={ styles.descriptionStyles }>
-          { t('cookiePolicyPage.theCookiesWeSet.login.description') }
-        </Typography>
-      </Box>
-
-      <Box sx={ styles.wrapper }>
-        <Typography sx={ styles.subtitleStyles }>
-          { t('cookiePolicyPage.theCookiesWeSet.site.title') }
-        </Typography>
-
-        <Typography sx={ styles.descriptionStyles }>
-          { t('cookiePolicyPage.theCookiesWeSet.site.description') }
-        </Typography>
-      </Box>
-
-      <Box sx={ styles.wrapper }>
-        <Typography sx={ styles.titleStyles }>
-          { t('cookiePolicyPage.thirdPartyCookies.title') }
-        </Typography>
-
-        <Typography sx={ styles.descriptionStyles }>
-          { t('cookiePolicyPage.thirdPartyCookies.description') }
-        </Typography>
-
-        <Typography sx={ styles.subtitleStyles }>
-          { t('cookiePolicyPage.thirdPartyCookies.subtitle') }
-        </Typography>
-      </Box>
-
-      <Box sx={ styles.wrapper }>
-        <Typography sx={ styles.titleStyles }>
-          { t('cookiePolicyPage.moreInformation.title') }
-        </Typography>
-
-        <Typography sx={ styles.subtitleStyles }>
-          { t('cookiePolicyPage.moreInformation.subtitle') }  
-        </Typography>
-      </Box>
+      { cookieItems }
     </Container>
   )
 }
---
+++ src/pages/cookie-policy/cookie-policy.constants.js
@@ -0,0 +1,36 @@
+export const cookieItemsData = [
+  {
+    title: 'cookiePolicyPage.whatAreCookies.title',
+    description: 'cookiePolicyPage.whatAreCookies.description'
+  },
+  {
+    title: 'cookiePolicyPage.howWeUseCookies.title',
+    description: 'cookiePolicyPage.howWeUseCookies.description'
+  },
+  {
+    title: 'cookiePolicyPage.disablingCookies.title',
+    description: 'cookiePolicyPage.disablingCookies.description'
+  },
+  {
+    title: 'cookiePolicyPage.theCookiesWeSet.title',
+    subtitle: 'cookiePolicyPage.theCookiesWeSet.account.title',
+    description: 'cookiePolicyPage.theCookiesWeSet.account.description'
+  },
+  {
+    subtitle: 'cookiePolicyPage.theCookiesWeSet.login.title',
+    description: 'cookiePolicyPage.theCookiesWeSet.login.description'
+  },
+  {
+    subtitle: 'cookiePolicyPage.theCookiesWeSet.site.title',
+    description: 'cookiePolicyPage.theCookiesWeSet.site.description'
+  },
+  {
+    title : 'cookiePolicyPage.thirdPartyCookies.title',
+    subtitle: 'cookiePolicyPage.thirdPartyCookies.subtitle',
+    description: 'cookiePolicyPage.thirdPartyCookies.description'
+  },
+  {
+    title : 'cookiePolicyPage.moreInformation.title',
+    subtitle: 'cookiePolicyPage.moreInformation.subtitle',
+  }
+]
