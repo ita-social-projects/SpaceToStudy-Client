a744104da580c0601a39fe1cb3775ecc3f04a8a0
MichaelSh <gmerdok@gmail.com>
2022-08-11 23:59:50 +0300

    added way to open login modal with link

--- src/pages/guest-home-page/GuestHome.js
+++ src/pages/guest-home-page/GuestHome.js
@@ -1,12 +1,20 @@
 import { Box } from '@mui/material'
+import { useEffect,useContext } from 'react'
+import { useLocation } from 'react-router-dom'
 
+import { ModalContext } from '~/context/modal-context'
 import Welcome from '~/containers/guest-home-page/Welcome'
 import FeatureBlock from '~/containers/guest-home-page/FeatureBlock'
 import WhatCanYouDo from '~/containers/guest-home-page/WhatCanYouDo'
 import HowItWorks from '~/containers/guest-home-page/how-it-works/HowItWorks'
 import WhoWeAre from '~/containers/guest-home-page/who-we-are/WhoWeAre'
+import LoginDialog from '~/containers/guest-home-page/login-dialog/LoginDialog'
 import { descriptionTimes } from '~/components/accordion-with-image/descriptionTimes'
 const GuestHomePage = () => {
+  const { setModal } = useContext(ModalContext)
+  const { hash } = useLocation()
+
+  useEffect(()=> hash === '#login' && setModal(<LoginDialog />), [])
   
   return (
     <Box data-testid="guestHome">
