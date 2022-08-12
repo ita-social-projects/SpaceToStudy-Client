8b2c04b1bd6c8b411a9d188af225170171b2f6ba
tarasSanotskyi <badsanta2102@gmail.com>
2022-08-12 20:31:23 +0300

    fix

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -4,13 +4,12 @@ const Loader = ({ size }) => {
   return (
     <Box
       data-testid="loader"
-      sx={ { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' } }
+      sx={ { display:'flex', justifyContent: 'center', alignItems: 'center', height: '100%' } }
     >
       <CircularProgress 
         color={ 'basic' }
         size={ size }
         sx={ { transform: 'translate(-50%, -50%)' } }
-        // sx={ { position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)' } }
       />
     </Box>
   )
