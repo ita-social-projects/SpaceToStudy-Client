202f7e27bbda23cfc5d0600e0cf4491b6895c32d
greentoss <mrsoberman1@gmail.com>
2022-08-19 00:41:53 +0300

    removed console

--- src/components/email-confirm-modal/email-confirm-modal.js
+++ src/components/email-confirm-modal/email-confirm-modal.js
@@ -19,8 +19,6 @@ const EmailConfirmModal = ( { confirmToken, setModal } ) => {
 
   const { response, error, loading } = useAxios( { service: serviceFunction } )
 
-  console.log({ response, error, loading }, 'useAxios response')
-
   const openLoginDialog = () => {
     setModal(<LoginDialog />)
   }
