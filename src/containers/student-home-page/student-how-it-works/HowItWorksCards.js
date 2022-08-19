de1d656b3dafff2d8774115f2fe1d604ee0da435
Sarakhman Anatoliy <63456632+Tolik170@users.noreply.github.com>
2022-08-19 10:29:06 +0300

    add student how it works block (#224)
    * add student how it works block
    
    * test fix
    
    * fix
    
    * fix
    
    * fix
    
    * use Link

---
+++ src/containers/student-home-page/student-how-it-works/HowItWorksCards.js
@@ -0,0 +1,27 @@
+import howItWorksStudentFirst from '~/assets/img/guest-home-page/howItWorksStudentFirst.svg'
+import howItWorksStudentSecond from '~/assets/img/guest-home-page/howItWorksStudentSecond.svg'
+import howItWorksStudentThird from '~/assets/img/guest-home-page/howItWorksStudentThird.svg'
+import howItWorksStudentFourth from '~/assets/img/guest-home-page/howItWorksStudentFourth.svg'
+
+export const howItWorksCards = [
+  {
+    image: howItWorksStudentSecond,
+    title: 'studentHomePage.howItWorks.selectAMentor.title',
+    description: 'studentHomePage.howItWorks.selectAMentor.description'
+  },
+  {
+    image: howItWorksStudentThird,
+    title: 'studentHomePage.howItWorks.sendRequest.title',
+    description: 'studentHomePage.howItWorks.sendRequest.description'
+  },
+  {
+    image: howItWorksStudentFourth,
+    title: 'studentHomePage.howItWorks.startLearning.title',
+    description: 'studentHomePage.howItWorks.startLearning.description'
+  },
+  {
+    image: howItWorksStudentFirst,
+    title: 'studentHomePage.howItWorks.writeFeedback.title',
+    description: 'studentHomePage.howItWorks.writeFeedback.description'
+  }
+]
---
+++ tests/unit/containers/student-home-page/student-how-it-works/StudentHowItWorks.spec.js
@@ -0,0 +1,37 @@
+import { screen, fireEvent } from '@testing-library/react'
+
+import { renderWithProviders } from '~tests/test-utils'
+import StudentHowItWorks from "~/containers/student-home-page/student-how-it-works/StudentHowItWorks";
+
+describe('StudentHowItWorks test', () => {
+
+    beforeEach(() => {
+        renderWithProviders(<StudentHowItWorks />)
+    })
+
+    it('should have section title and description', async () => {
+        const title = screen.getByText('studentHomePage.howItWorks.title')
+        const description = screen.getByText('studentHomePage.howItWorks.description')
+
+        expect(title).toBeInTheDocument()
+        expect(description).toBeInTheDocument()
+      })
+
+    it('should redirect to find mentor page when click on the find mentor button', async () => {
+        const findMentorButton = screen.getByText('studentHomePage.findMentorBlock.button')
+        fireEvent.click(findMentorButton)
+        const findMentorText = screen.getByText(/FindMentor/i)
+    
+        expect(findMentorText).toBeInTheDocument()
+      })
+
+      it('should have start learning image, title and description', async () => {
+        const img = screen.getByAltText('studentHomePage.howItWorks.startLearning.title')
+        const title = screen.getByText('studentHomePage.howItWorks.startLearning.title')
+        const description = screen.getByText('studentHomePage.howItWorks.startLearning.description')
+
+        expect(img).toBeInTheDocument()
+        expect(title).toBeInTheDocument()
+        expect(description).toBeInTheDocument()
+      })
+})
