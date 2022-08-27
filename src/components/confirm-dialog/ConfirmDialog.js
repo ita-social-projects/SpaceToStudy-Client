5af0d5d35508bb1ef5c117522db9205e3c7434ac
Yurii Dmytruk <86732805+dmytruk-y@users.noreply.github.com>
2022-08-27 12:49:13 +0300

    Add min-width for confirmation modal (#273)

--- src/components/confirm-dialog/ConfirmDialog.js
+++ src/components/confirm-dialog/ConfirmDialog.js
@@ -2,23 +2,28 @@ import { useTranslation } from 'react-i18next'
 import { Button, Dialog, DialogActions, DialogContent, IconButton, Typography } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 
+import { style } from '~/components/confirm-dialog/confirm-dialog.style'
+
 const ConfirmDialog = ({ message, title, open, onConfirm, onDismiss }) => {
   const { t } = useTranslation()
 
   return (
-    <Dialog data-testid='confirmDialog' onClose={ onDismiss } open={ open }>
-      <Typography sx={ { p: '13px 16px' } } variant='h6'>
+    <Dialog
+      PaperProps={ { sx: style.root } } data-testid='confirmDialog' onClose={ onDismiss }
+      open={ open }
+    >
+      <Typography sx={ style.title } variant='h6'>
         { t(title) }
       </Typography>
-      <IconButton onClick={ onDismiss } sx={ { position: 'absolute', right: 16, top: 16, p: 0 } }>
+      <IconButton onClick={ onDismiss } sx={ style.icon }>
         <CloseIcon />
       </IconButton>
-      <DialogContent dividers sx={ { p: 2 } }>
+      <DialogContent dividers sx={ style.content }>
         <Typography variant='subtitle1'>
           { t(message) }
         </Typography>
       </DialogContent>
-      <DialogActions sx={ { p: '12px' } }>
+      <DialogActions sx={ style.actions }>
         <Button onClick={ onConfirm } size='large' variant='tonal'>
           { t('common.yes') }
         </Button>
---
+++ src/components/confirm-dialog/confirm-dialog.style.js
@@ -0,0 +1,20 @@
+export const style = {
+  root: {
+    minWidth: { xs: '280px', sm: '400px' }
+  },
+  icon: {
+    position: 'absolute',
+    right: 16,
+    top: 16,
+    p: 0
+  },
+  title: {
+    p: '13px 16px'
+  },
+  content: {
+    p: 2
+  },
+  actions: {
+    p: '12px'
+  }
+}
