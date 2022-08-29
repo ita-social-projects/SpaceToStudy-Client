2d6302826992be048b24e7d3af19d7135c20e17f
Lubomyr <lubomrkashuba@gmail.com>
2022-08-29 14:21:05 +0300

    add test

--- tests/unit/containers/guest-home-page/login-dialog/LoginDialog.spec.js
+++ tests/unit/containers/guest-home-page/login-dialog/LoginDialog.spec.js
@@ -1,15 +1,10 @@
 import { screen, render, fireEvent } from '@testing-library/react'
-import MockAdapter from 'axios-mock-adapter'
 import { MemoryRouter } from 'react-router-dom'
-import { URLs } from '~/constants/request'
 import LoginDialog from '~/containers/guest-home-page/login-dialog/LoginDialog'
 import { ModalProvider } from '~/context/modal-context'
 import { SnackBarProvider } from '~/context/snackbar-context'
-import { axiosClient } from '~/plugins/axiosClient'
 
 const mockDispatch = jest.fn()
-
-const mockAxiosClient = new MockAdapter(axiosClient)
 
 jest.mock('react-redux', () => ({
   useDispatch: () => mockDispatch.mockReturnValue({ unwrap: () => '' })
@@ -66,20 +61,6 @@ describe('Login dialog test', () => {
     const error = screen.getByText('common.errorMessages.emptyField')
 
     expect(error).toBeInTheDocument()
-  })
-
-  it('should show snackbar with error', async () => {
-    mockAxiosClient.onPost(URLs.auth.login).reply(404, { code: 'error' })
-    const inputEmail = screen.getByLabelText(/common.labels.email/i)
-    fireEvent.change(inputEmail, { target: { value: 'test@gmail.com' } })
-
-    const inputPassword = screen.getByLabelText(/common.labels.password/i)
-    fireEvent.change(inputPassword, { target: { value: '12345678a/A' } })
-
-    const button = screen.getByText('common.labels.login')
-    fireEvent.click(button)
-
-    screen.debug()
   })
 
   it('should dispatch after button submit', async () => {
