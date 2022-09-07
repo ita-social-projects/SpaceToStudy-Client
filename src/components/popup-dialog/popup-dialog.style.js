41a4e10a1442aca1a6ec192d56940049ebac7768
MichaelSh <gmerdok@gmail.com>
2022-09-07 12:07:02 +0300

    returned padding to modal

---
+++ src/components/popup-dialog/popup-dialog.style.js
@@ -0,0 +1,16 @@
+export const style = {
+  box: {
+    margin: { xs: '0 auto', sm: 0 },
+    padding: { xs: 0, sm: 2, md: 4 },
+    display: { xl: 'flex' },
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: { xl: '100%' }
+  },
+  icon: {
+    color: 'primary.900',
+    position: 'absolute',
+    right: { xs: '8px', sm: '20px' },
+    top: { xs: '8px', sm: '20px' }
+  }
+}
