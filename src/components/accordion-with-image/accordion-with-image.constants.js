bad385581b607759f0b692c3bed9b77440e304b0
Yevhendzyuba <evgendzuba7@gmail.com>
2022-09-09 02:36:32 +0300

    fix conflicts

---
+++ src/components/accordion-with-image/accordion-with-image.constants.js
@@ -0,0 +1,24 @@
+import MapLogo from '~/assets/img/guest-home-page/map.svg'
+
+export const descriptionTimes = [
+  {
+    image: MapLogo,
+    title: 'guestHomePage.accordion.flexibleLocation.title',
+    description: 'guestHomePage.accordion.flexibleLocation.description'
+  },
+  {
+    image: MapLogo,
+    title: 'guestHomePage.accordion.individualTime.title',
+    description: 'guestHomePage.accordion.individualTime.description'
+  },
+  {
+    image: MapLogo,
+    title: 'guestHomePage.accordion.freeChoiceOfTeachers.title',
+    description: 'guestHomePage.accordion.freeChoiceOfTeachers.description'
+  },
+  {
+    image: MapLogo,
+    title: 'guestHomePage.accordion.digitalCommunication.title',
+    description: 'guestHomePage.accordion.digitalCommunication.description'
+  }
+]
