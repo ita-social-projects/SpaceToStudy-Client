c10ccbbeca1520052b686e2300738e5533b52981
tarasSanotskyi <badsanta2102@gmail.com>
2022-09-25 17:55:48 +0300

    create scroll to top component and add unit test, rename scroll to top button

---
+++ src/components/scroll-to-top-button/ScrollToTopButton.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+
+import IconButton from '@mui/material/IconButton'
+import Box from '@mui/material/Box'
+import ArrowUpwardRoundedIcon from '@mui/icons-material/ArrowUpwardRounded'
+
+import { styles } from '~/components/scroll-to-top-button/ScrollToTopButton.styles'
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const scroll = () => (window.scrollY > 450 ? setIsVisible(true) : setIsVisible(false))
+
+  useEffect(() => {
+    window.addEventListener('scroll', scroll)
+    return () => window.removeEventListener('scroll', scroll)
+  }, [])
+
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+  return (
+    isVisible && (
+      <Box sx={ styles.root }>
+        <IconButton onClick={ goToTop } sx={ styles.button }>
+          <ArrowUpwardRoundedIcon sx={ styles.icon } />
+        </IconButton>
+      </Box>
+    )
+  )
+}
+
+export default ScrollToTopButton
--- src/containers/app-content/AppContent.js
+++ src/containers/app-content/AppContent.js
@@ -2,8 +2,9 @@ import Box from '@mui/material/Box'
 
 import AppHeader from '~/containers/layout/app-header/AppHeader'
 import AppMain from '~/containers/layout/app-main/AppMain'
-import ScrollToTop from '~/components/scroll-to-top/ScrollToTop'
+import ScrollToTopButton from '~/components/scroll-to-top-button/ScrollToTopButton'
 import Footer from '~/containers/layout/footer/Footer'
+import ScrollToTop from '~/components/scroll-to-top/ScrollToTop'
 
 const styles = {
   content: {
@@ -25,8 +26,9 @@ const AppContent = () => {
   return (
     <Box data-testid='AppContent' sx={ styles.content }>
       <AppHeader />
-      <AppMain />
       <ScrollToTop />
+      <AppMain />
+      <ScrollToTopButton />
       <Footer />
     </Box>
   )
