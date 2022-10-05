bbf6c3b08f397b45939e6bf47b3255e077bed5a6
Evhenij Pipa <zhuvchuk1@gmail.com>
2022-10-05 13:11:56 +0300

    added tablet breakpoint fixes

--- src/containers/student-home-page/popular-categories/popular-categories.styles.js
+++ src/containers/student-home-page/popular-categories/popular-categories.styles.js
@@ -1,7 +1,10 @@
 export const styles = {
   container: {
     display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fit, minmax(288px, 1fr))',
+    gridTemplateColumns: {
+      xs: 'repeat(auto-fit, minmax(288px, 1fr))',
+      sm: 'repeat(auto-fit, minmax(260px, 1fr))'
+    },
     gridAutoRows: '126px',
     gridGap: '24px',
     mb: '32px'
