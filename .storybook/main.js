c119967767ddb9bedbe7b3b7219e601457c0722c
Yevhen Dzyuba <evgendzuba7@gmail.com>
2023-02-10 14:59:54 +0200

    fix storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
 const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-vite'
-  },
-  features: {
-    storyStoreV7: true
+    builder: '@storybook/builder-webpack5'
   },
   webpackFinal: (config) => {
     config.resolve.alias = {
