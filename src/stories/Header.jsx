05e083ca2057b1d967dfea55e42a93a6ccf1dcf2
Yevhen Dzyuba <evgendzuba7@gmail.com>
2023-02-11 20:50:16 +0200

    fix lint errors

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
-import { Button } from './Button';
-import './header.css';
+import { Button } from './Button'
+import './header.css'
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
     <div className="wrapper">
       <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          height="32" viewBox="0 0 32 32" width="32"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g fill="none" fillRule="evenodd">
             <path
               d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
@@ -27,31 +29,39 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
         <h1>Acme</h1>
       </div>
       <div>
-        {user ? (
+        { user ? (
           <>
             <span className="welcome">
-              Welcome, <b>{user.name}</b>!
+              Welcome, 
+              { ' ' }
+              <b>
+                { user.name }
+              </b>
+              !
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <Button label="Log out" onClick={ onLogout } size="small" />
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button label="Log in" onClick={ onLogin } size="small" />
+            <Button
+              label="Sign up" onClick={ onCreateAccount } primary
+              size="small"
+            />
           </>
-        )}
+        ) }
       </div>
     </div>
   </header>
-);
+)
 
 Header.propTypes = {
   user: PropTypes.shape({}),
   onLogin: PropTypes.func.isRequired,
   onLogout: PropTypes.func.isRequired,
   onCreateAccount: PropTypes.func.isRequired,
-};
+}
 
 Header.defaultProps = {
   user: null,
-};
+}
