ad918c77eb9b2284da1b9fa6ca2c41939edd3266
Diana Tsivka <tsivkadi@gmail.com>
2023-02-16 17:23:39 +0200

    added AppChip component to storybook

---
+++ src/stories/AppChip.stories.js
@@ -0,0 +1,34 @@
+import AppChip from '~/components/app-chip/AppChip'
+
+export default {
+  title: 'AppChip',
+  component: AppChip,
+  argTypes: {
+    handleDelete: {
+      type: 'function',
+      action: 'handleDelete',
+      description: 'Function that handles delete'
+    },
+    label: {
+      type: 'string',
+      control: 'text',
+      description: 'Label of the chip'
+    }
+  }
+}
+
+const Chip = (args) => <AppChip {...args}>{args.label}</AppChip>
+
+export const Default = Chip.bind({})
+
+Default.args = {
+  label: 'Text'
+}
+
+Default.decorators = [
+  (Story) => (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Story />
+    </div>
+  )
+]
