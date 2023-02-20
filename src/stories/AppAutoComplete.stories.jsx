ce5024066c4450d001d0578324fd167b373fa528
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2023-02-20 16:10:13 +0200

    Added new storybook component - AppAutoComplete

---
+++ src/stories/AppAutoComplete.stories.jsx
@@ -0,0 +1,51 @@
+import AppAutoComplete from '~/components/app-auto-complete/AppAutoComplete'
+
+export default {
+  title: 'AppAutoComplete',
+  component: AppAutoComplete,
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+      description: 'Default state of component'
+    },
+    fieldValue: {
+      type: 'string',
+      description: 'Basic value inside field input'
+    },
+    loading: {
+      type: 'boolean',
+      description: 'Loading AutoComplete component'
+    },
+    onChange: {
+      type: 'function',
+      description: 'Function will be called when autocomplete item will be changed'
+    },
+    options: {
+      type: 'array',
+      description: 'Default options in autocomplete item'
+    },
+    filterOptions: {
+      type: 'function',
+      description: 'Function that describes filter options'
+    }
+  }
+}
+
+const Template = (args) => <AppAutoComplete { ...args } />
+
+export const Desktop = Template.bind({})
+
+Desktop.args = {
+  disabled: false,
+  fieldValue: 'Type AutoComplete item here',
+  loading: false,
+  options: ['AutoCompleteItme-1', 'AutoCompleteItme-2', 'AutoCompleteItme-3']
+}
+
+Desktop.decorators = [
+  (Story) => (
+    <div style={ { maxWidth: '500px' } }>
+      <Story />
+    </div>
+  )
+]
