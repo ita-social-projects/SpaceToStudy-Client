e434ec489da95334e540bd1675b4866960bf0355
Sarakhman Anatoliy <63456632+Tolik170@users.noreply.github.com>
2023-03-01 13:41:55 +0200

    added test for DateFilter Component (#496)
    * added test for DateFilter Component
    
    * fix test

---
+++ tests/unit/components/date-filter/DateFilter.spec.jsx
@@ -0,0 +1,52 @@
+import { vi } from 'vitest'
+import { fireEvent, screen, render } from '@testing-library/react'
+
+import DateFilter from '~/components/enhanced-table/date-filter/DateFilter'
+
+const props = {
+  filter: { from: '', to: '' },
+  setFilter: vi.fn(),
+  clearFilter: vi.fn(),
+}
+
+const dateMock = new Date('2023-01-01')
+
+describe('DateFilter test', () => {
+  beforeEach(() => {
+    render(
+      <DateFilter { ...props } />
+
+    )
+  })
+
+  it('Should open, and change value in calendar', () => {
+    const calendarIcon = screen.getByTestId('calendar-icon') 
+
+    fireEvent.click(calendarIcon)
+
+    const dateFilterBtn = screen.getAllByLabelText('Choose date')
+
+    fireEvent.click(dateFilterBtn[0])
+
+    const dateFromInput = screen.getByLabelText('date-filter-from')
+
+    fireEvent.change(dateFromInput, { target: { value: dateMock } })
+
+    fireEvent.keyDown(document, { key: 'Escape' })
+
+    expect(props.setFilter).toHaveBeenCalled()
+  })
+
+  it('Should clear value in calendar', async () => {
+
+    const dateToInput = screen.getByLabelText('date-filter-to')
+
+    fireEvent.change(dateToInput, { target: { value: dateMock } })
+
+    const clearIcon = screen.getByTestId('clear-icon') 
+
+    fireEvent.click(clearIcon)
+
+    expect(props.clearFilter).toHaveBeenCalled()
+  })
+})
