b57efbf52728c4e0a020496a09e3a719e492da8d
Dmytro Honchar <honchar@gmail.com>
2023-03-01 18:48:37 +0200

    implemented ViewSwitcher component

---
+++ src/components/view-switcher/ViewSwitcher.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import ToggleButton from '@mui/material/ToggleButton'
+
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted'
+import GridViewIcon from '@mui/icons-material/GridView'
+
+import { cardsViews } from '~/constants'
+import { styles } from './ViewSwitcher.styles'
+
+const ViewSwitcher = ({ setOffersView }) => {
+  const [cardsView, setCardsView ] = useState(cardsViews.inline)
+
+  const changeCardsView = (_event,view) => {
+    setOffersView(view)
+    setCardsView(view)
+  }
+
+  return (
+    <>
+      <ToggleButton
+        aria-label='inline card view' onClick={ changeCardsView }
+        selected={ cardsView === cardsViews.inline }
+        sx={ { ...styles.toggleButton, marginRight:'8px' } }
+        value={ cardsViews.inline }
+      >
+        <FormatListBulletedIcon sx={ styles.icon } />
+      </ToggleButton>
+      <ToggleButton
+        aria-label='grid card view'
+        onClick={ changeCardsView }
+        selected={ cardsView === cardsViews.grid } sx={ { ...styles.toggleButton } }
+        value={ cardsViews.grid }
+      >
+        <GridViewIcon sx={ styles.icon }  />
+      </ToggleButton>
+    </>
+  )
+}
+
+export default ViewSwitcher
---
+++ src/components/view-switcher/ViewSwitcher.styles.js
@@ -0,0 +1,13 @@
+export const styles = {
+  icon: {
+    fontSize: '24px',
+    color: 'primary.900',
+  },
+  toggleButton:{
+    borderColor:'primary.200',
+    '&.Mui-selected':{
+      borderColor:'primary.900',
+      transition:'.16s ease'
+    }
+  }
+}
