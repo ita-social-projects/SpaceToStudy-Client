e0f4dc3f1a2c308d7bc214e06c8116bb96fa4ce7
Dmytro Honchar <honchar@gmail.com>
2023-03-03 10:34:54 +0200

    hot fix

--- src/components/view-switcher/ViewSwitcher.styles.js
+++ src/components/view-switcher/ViewSwitcher.styles.js
@@ -1,16 +1,16 @@
+const toggleButtonStyle = {
+  borderColor: 'primary.200',
+  '&.Mui-selected': {
+    borderColor: 'primary.900',
+    transition: '.16s ease',
+    backgroundColor: 'transparent'
+  }
+}
 export const styles = {
   icon: {
     fontSize: '24px',
     color: 'primary.900'
   },
-  toggleButton: {
-    borderColor: 'primary.200',
-    '&.Mui-selected': {
-      borderColor: 'primary.900',
-      transition: '.16s ease',
-      backgroundColor: 'transparent'
-    }
-  },
-  gridButton: this.toggleButton,
-  inlineButton: { ...this.toggleButton, marginRight: '8px' }
+  gridButton: toggleButtonStyle,
+  inlineButton: { ...toggleButtonStyle, marginRight: '8px' }
 }
