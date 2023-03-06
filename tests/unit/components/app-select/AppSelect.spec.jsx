5eb4b947c3de851761171a9bd3a43ed3088bd031
Dmytro Honchar <honchar@gmail.com>
2023-03-06 20:58:08 +0200

    refactor spaces

--- tests/unit/components/app-select/AppSelect.spec.jsx
+++ tests/unit/components/app-select/AppSelect.spec.jsx
@@ -17,23 +17,17 @@ describe('AppSelect component', () => {
   it('should sort by newest if props sortBy state is newest', () => {
     render(
       <AppSelect
-        selectTitle={ 'Sort by' }
-        setSortedValue={ mockedSetSortedValue }  
+        fields={ mockedSortingFields }
+        selectTitle={ 'Sort by' }  
+        setSortedValue={ mockedSetSortedValue }
         sortedValue={ sortByFields.newest }
-        sortingFields={ mockedSortingFields }
       />)
-<<<<<<< HEAD
 
     const mockedSelect = screen.getByTestId(selectId)
     expect(mockedSelect.value).toBe(sortByFields.newest)
 
     fireEvent.change(mockedSelect, { target: { value: sortByFields.popularity } })
-    
-=======
-    const mockedSelect = screen.getByTestId(selectId)
-    expect(mockedSelect.value).toBe(sortByFields.newest)
-    fireEvent.change(mockedSelect, { target: { value: sortByFields.popularity } })
->>>>>>> b2a9a38 (changed unit test and updated vars name)
+
     expect(mockedSetSortedValue).toBeCalledWith(sortByFields.popularity)
   })
 })
