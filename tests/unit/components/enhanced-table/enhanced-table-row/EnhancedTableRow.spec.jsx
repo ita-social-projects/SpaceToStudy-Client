e35b2191768d2177d7ee53e1df1a6b8855ebe808
Diana Tsivka <tsivkadi@gmail.com>
2023-03-07 13:04:23 +0200

    refactored code

--- tests/unit/components/enhanced-table/enhanced-table-row/EnhancedTableRow.spec.jsx
+++ tests/unit/components/enhanced-table/enhanced-table-row/EnhancedTableRow.spec.jsx
@@ -47,43 +47,56 @@ describe('EnhancedTableRow component', () => {
     render(<EnhancedTableRow isItemSelected={ false } item={ mockItem } refetchData={ refetchDataMock } />)
   })
 
-  it('renders table row with all columns', () => {
+  it('should render table row with correct data', () => {
     expect(screen.getByText('John Smith')).toBeInTheDocument()
-    expect(screen.getByText('john@email.com')).toBeInTheDocument()
     expect(calculatedCellValueMock).toHaveBeenCalled()
   })
 
-  it('renders table row with checkbox and menu icon', () => {
-    expect(screen.queryByRole('checkbox')).toBeInTheDocument()
-    expect(screen.queryByTestId('menu-icon')).toBeInTheDocument()
-  })
-
-  it('calls handleSelectClick when checkbox is clicked', () => {
+  it('should call handleSelectClick when checkbox is clicked', () => {
     const checkbox = screen.getByRole('checkbox')
+
     fireEvent.click(checkbox)
+
     expect(handleSelectClickMock).toHaveBeenCalled()
   })
 
-  it('renders action menu when menu icon is clicked', async() => {
+  it('should render action menu when menu icon is clicked', async() => {
     const menuIcon = screen.getByTestId('menu-icon')
+
     fireEvent.click(menuIcon)
-    expect(await screen.findByText('Delete')).toBeInTheDocument()
+
+    const menuItem = await screen.findByText('Delete')
+
+    expect(menuItem).toBeInTheDocument()
   })
 
-  it('calls onAction function when clicking on the menu item', async() => {
+  it('should call  onAction function when clicking on the menu item', async() => {
     const menuIcon = screen.getByTestId('menu-icon')
+
     fireEvent.click(menuIcon)
-    await waitFor(() => {
-      fireEvent.click(screen.getByText('Delete'))
-      expect(rowActions[0].func).toHaveBeenCalledWith(mockItem._id)
-    })
+
+    const menuItem = await screen.findByText('Delete')
+
+    fireEvent.click(menuItem)
+
+    await waitFor(() => expect(rowActions[0].func).toHaveBeenCalledWith(mockItem._id))
   })
-  it('calls closeMenu function when clicking outside of the menu', async() => {
+
+  it('should close menu when "escape" is pressed', async() => {
     const menuIcon = screen.getByTestId('menu-icon')
+
     fireEvent.click(menuIcon)
-    await waitFor(() => expect(screen.getByText('Delete')).toBeInTheDocument())
-    fireEvent.click(screen.getByRole('presentation').firstChild)
-    expect(menuIcon).toBeInTheDocument()
+
+    const menuItem = screen.queryByText('Delete')
+
+    await waitFor(() => expect(menuItem).toBeInTheDocument())
+
+    fireEvent.keyDown(menuItem, {
+      key: 'Escape',
+      code: 'Escape'
+    })
+
+    await waitFor(() => expect(screen.queryByText('Delete')).toBeNull())
   })
 
 })
