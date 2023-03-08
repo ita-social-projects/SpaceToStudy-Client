20011515b4d407a4064c6a350a1f5a8f08def8cd
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2023-03-08 16:08:22 +0200

    Refactor code and change test cases

--- tests/unit/components/enhanced-table/enhanced-table-pagination/EnhancedTablePagination.spec.jsx
+++ tests/unit/components/enhanced-table/enhanced-table-pagination/EnhancedTablePagination.spec.jsx
@@ -1,54 +1,37 @@
 import EnhancedTablePagination from '~/components/enhanced-table/enhanced-table-pagination/EnhancedTablePagination'
 import { fireEvent, render, screen } from '@testing-library/react'
-import { useTableContext } from '~/context/table-context'
-import { vi } from 'vitest'
+import { TableProvider } from '~/context/table-context'
 
-const handlePageSubmitMock = vi.fn()
-
-vi.mock('~/context/table-context', () => ({
-  useTableContext: vi.fn()
-}))
-
-vi.mock('~/hooks/table/use-pagination', () => ({
-  default: () => ({ handlePageSubmit: handlePageSubmitMock })
-}))
-
-const mockedProps = 100
-
-const page = 1
-const pageInput = 10
-const rowsPerPage = 5
+const itemsCountMock = 100
 
 describe('EnhancedTablePagination test', () => {
   beforeEach(() => {
-    useTableContext.mockReturnValue({
-      page,
-      pageInput,
-      rowsPerPage
-    })
-    render(<EnhancedTablePagination handlePageSubmit={ handlePageSubmitMock } itemsCount={ mockedProps } />)
+    render(
+      <TableProvider>
+        <EnhancedTablePagination itemsCount={ itemsCountMock } />
+      </TableProvider>
+    )
   })
 
-  it('Renders correctly number of rows', () => {
-    const labelRowsPerPage = screen.getByText('table.numberOfRows')
-    expect(labelRowsPerPage).toBeInTheDocument()
-  })
-  it('render Go to Page  element in component', () => {
-    const goToPage = screen.getByText('table.goToPage')
-    expect(goToPage).toBeInTheDocument()
-  })
-  it('should render correctly button in component', () => {
-    const buttonName = screen.getByText('table.go')
-    expect(buttonName).toBeInTheDocument()
-  })
-  it('should correctly display number of rows', () => {
-    const displayedRowsLabel = screen.getByText('6-10 table.of 100')
-    expect(displayedRowsLabel).toBeInTheDocument()
+  it('Should render first page', () => {
+    const firstPageRows = screen.getByText(`1-5 table.of ${itemsCountMock}`)
+
+    expect(firstPageRows).toBeInTheDocument()
   })
 
-  it('should call handlePageSubmit when button was clicked', () => {
+  it('Should change page from 1 to 2', () => {
+    const inputField = screen.getByTestId('testid-page-input')
+
+    fireEvent.change(inputField, { target: { value: 2 } })
+
+    expect(inputField.value).toBe('2')
+
     const button = screen.getByText('table.go')
+
     fireEvent.click(button)
-    expect(handlePageSubmitMock).toHaveBeenCalled()
+
+    const secondPageRows = screen.getByText(`6-10 table.of ${itemsCountMock}`)
+
+    expect(secondPageRows).toBeInTheDocument()
   })
 })
