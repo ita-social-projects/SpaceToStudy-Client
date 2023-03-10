d1575fe47e96f83f36f3f7bebf72b2ce3ee5ae2e
Mykhailo Shchur <97129041+MickhaelSh@users.noreply.github.com>
2023-03-10 14:49:16 +0200

    Added tests for enhanced table head component (#526)
    * added tests for enhanced table head component
    
    * fixed naming

---
+++ tests/unit/components/enhanced-table/enhanced-table-head/EnhancedTableHead.spec.jsx
@@ -0,0 +1,54 @@
+import { vi } from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { TableProvider } from '~/context/table-context'
+import EnhancedTableHead from '~/components/enhanced-table/enhanced-table-head/EnhancedTableHead'
+
+const columnsMock = [
+  {
+    label: 'Name',
+    field: 'name',
+    dataType: 'string',
+  },
+  {
+    label: 'Email',
+    field: 'email',
+    dataType: 'string'
+  },
+]
+const initialSortMock = { order: 'asc', orderBy: 'email' }
+const itemsCountMock = 10
+const onSelectAllClickMock = vi.fn()
+
+describe('EnhancedTableHead', () => {
+  beforeEach(()=>{
+    render(
+      <TableProvider columns={ columnsMock } initialSort={ initialSortMock } isSelection>
+        <table>
+          <EnhancedTableHead
+            itemsCount={ itemsCountMock }
+            onSelectAllClick={ onSelectAllClickMock }
+          />
+        </table>
+      </TableProvider>
+    )
+  })
+
+  it('renders all columns in the table', () => {
+    const nameColumn = screen.getByText(columnsMock[0].label)
+    const emailColumn = screen.getByText(columnsMock[1].label)
+    
+    expect(nameColumn).toBeInTheDocument()
+    expect(emailColumn).toBeInTheDocument()
+  })
+  it('Should show table actions when isSelection is true', () => {
+    const tableActions = screen.getByText('table.actions')
+
+    expect(tableActions).toBeInTheDocument()
+  })
+  it('Should call function onSelectAllClick when checkbox value is changed', () => {
+    const actionColumn = screen.getByRole('checkbox')
+    fireEvent.click(actionColumn)
+
+    expect(onSelectAllClickMock).toHaveBeenCalled()
+  })
+})
