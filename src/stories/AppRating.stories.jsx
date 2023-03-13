3ccaa992b3c147f4a42e04d27cfdf10037b8af92
MickhaelSh <gmerdok@gmail.com>
2023-03-13 18:45:20 +0200

    fixed storybook

--- src/stories/AppRating.stories.jsx
+++ src/stories/AppRating.stories.jsx
@@ -10,40 +10,32 @@ export default {
         min: 1,
         max: 5,
         step: 0.1
-      }
-    },
-    size: {
-      control: {
-        type: 'inline-radio',
-        options: ['small', 'medium', 'large']
-      }
+      },
+      description: 'The rating value, ranging from 1 to 5 with a step of 0.1',
     },
     reviews: {
       control: {
         type: 'number'
-      }
-    },
-    withBackground: {
-      control: {
-        type: 'boolean'
-      }
+      },
+      description: 'The number of reviews',
     },
     numberVariant:{
       control: {
         type: 'inline-radio',
         options: ['big', 'small', '']
-      }
+      },
+      description: 'The variant of the number of reviews, either big or small'
     },
     mobile: {
       control: {
         type: 'boolean'
-      }
+      },
+      description: 'Whether to display the component in mobile view'
     },
-    spacing: {
-      control: {
-        type: 'number'
-      }
-    }
+    sx: {
+      control: null,
+      description: 'The style props of the component'
+    },
   }
 }
 
@@ -52,8 +44,14 @@ const Template = (args) => <AppRating { ...args } />
 export const Default = Template.bind({})
 Default.args = {
   value: 4.5,
-  withBackground: true,
-  spacing: 1,
   mobile: false,
+  sx:{
+    root:{ backgroundColor:'primary.50' },
+    stars: {
+      '& .MuiRating-icon': {
+        mx: '1px'
+      }
+    },
+  },
   numberVariant:''
 }
