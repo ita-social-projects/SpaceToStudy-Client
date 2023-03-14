ef023e7e24405717cf79937b87447b8c45fef2d1
mxrcury <demetrgonchar@gmail.com>
2023-03-14 20:24:21 +0200

    added unit test

---
+++ tests/unit/components/CheckboxList/CheckboxList.spec.jsx
@@ -0,0 +1,22 @@
+import { render, getByLabelText, fireEvent } from '@testing-library/react'
+import { vi, describe, it, expect } from 'vitest'
+import CheckboxList from '~/components/checkbox-list/CheckboxList';
+
+const mockedItemToChange = { title: 'Beginner', checked: false }
+
+const mockedItems =  [ mockedItemToChange, { title: 'Intermediate', checked: false }, { title: 'Advanced', checked: false } ]
+
+const mockedGetCheckbox = vi.fn()
+
+describe('CheckboxList component', () => {
+  it('should get checked state of checkbox on click', () => {
+    const { getByLabelText } = render(<CheckboxList title='Levels' items={ mockedItems } getCheckbox={ mockedGetCheckbox }  />)
+    
+    const checkbox = getByLabelText('Beginner')
+    expect(checkbox.checked).toBe(false)
+
+    fireEvent.change(checkbox, { target:{ checked: true }})
+
+    expect(mockedGetCheckbox).toHaveBeenCalled()
+  })
+})
