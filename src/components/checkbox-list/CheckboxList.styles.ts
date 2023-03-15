01404c18b2cdb120faed2f6a3c24a125b2c6b417
mxrcury <demetrgonchar@gmail.com>
2023-03-15 16:35:59 +0200

    separate ts type

--- src/components/checkbox-list/CheckboxList.styles.ts
+++ src/components/checkbox-list/CheckboxList.styles.ts
@@ -9,9 +9,15 @@ export const styles = {
     mb:'15px'
   },
   checkbox:{
+<<<<<<< HEAD
     '&.MuiCheckbox-root':{
       p:0,
       mr:'8px'
+=======
+    ml:'-10px',
+    '&.MuiCheckbox-root':{
+      padding:0
+>>>>>>> f6766e3 (separate ts type)
     }
   }
 }
--- src/components/checkbox-list/CheckboxList.types.ts
+++ src/components/checkbox-list/CheckboxList.types.ts
@@ -1,4 +1,8 @@
 export type CheckboxType = {
     title: string
     checked: boolean
+<<<<<<< HEAD
 }
+=======
+}
+>>>>>>> f6766e3 (separate ts type)
