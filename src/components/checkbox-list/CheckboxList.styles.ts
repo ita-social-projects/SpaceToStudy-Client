a5b9d5aeb07cde5a5167e9e3525dace4bdb0520b
mxrcury <demetrgonchar@gmail.com>
2023-03-15 17:03:01 +0200

    changed styles by removing negative values and update stories to jsx

--- src/components/checkbox-list/CheckboxList.styles.ts
+++ src/components/checkbox-list/CheckboxList.styles.ts
@@ -5,6 +5,7 @@ export const styles = {
     mb:'8px'
   },
   title:{
+<<<<<<< HEAD
     color:'primary.700',
     mb:'15px'
   },
@@ -18,6 +19,14 @@ export const styles = {
     '&.MuiCheckbox-root':{
       padding:0
 >>>>>>> f6766e3 (separate ts type)
+=======
+    color:'primary.700'
+  },
+  checkbox:{
+    '&.MuiCheckbox-root':{
+      p:0,
+      mr:'8px',
+>>>>>>> 5752b50 (changed styles by removing negative values and update stories to jsx)
     }
   }
 }
