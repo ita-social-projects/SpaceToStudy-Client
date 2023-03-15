e50220bdf41cf364fe5c8f6a414621afd3d02366
mxrcury <demetrgonchar@gmail.com>
2023-03-15 17:05:03 +0200

    added line and stories updated

---
+++ src/stories/CheckboxList.stories.jsx
@@ -0,0 +1,30 @@
+import CheckboxList from '~/components/checkbox-list/CheckboxList'
+
+export default {
+  title: 'CheckboxList',
+  component: CheckboxList,
+  argTypes: {
+    items: [],
+    title: {
+      type: 'string',
+      description: 'Title of this component'
+    },
+    getCheckbox: {
+      type: 'function',
+      description: 'Function for getting value of changed checkbox',
+      action:'changed'
+    }
+  }
+}
+
+const Template = (args) => <CheckboxList { ...args } />
+
+export const Default = Template.bind({})
+
+const defaultItems = [{ title: 'Beginner', checked: false }, { title: 'Intermediate', checked: false }, { title: 'Advanced', checked: false }]
+
+Default.args = {
+    items:defaultItems,
+    title:'Level',
+    getCheckbox:(checkbox) => console.log('Changed checkbox:', checkbox)
+}
