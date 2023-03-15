e3bc10f913553edd196a07ba95f436232286ba77
mxrcury <demetrgonchar@gmail.com>
2023-03-15 17:05:03 +0200

    added line and stories updated

--- src/components/checkbox-list/CheckboxList.types.ts
+++ src/components/checkbox-list/CheckboxList.types.ts
@@ -2,7 +2,11 @@ export type CheckboxType = {
     title: string
     checked: boolean
 <<<<<<< HEAD
+<<<<<<< HEAD
 }
 =======
 }
 >>>>>>> f6766e3 (separate ts type)
+=======
+}
+>>>>>>> e50220b (added line and stories updated)
--- src/stories/CheckboxList.stories.jsx
+++ src/stories/CheckboxList.stories.jsx
@@ -9,9 +9,15 @@ export default {
       type: 'string',
       description: 'Title of this component'
     },
+<<<<<<< HEAD
     getCheckboxes: {
       type: 'function',
       description: 'Function for getting value of all checkboxes',
+=======
+    getCheckbox: {
+      type: 'function',
+      description: 'Function for getting value of changed checkbox',
+>>>>>>> e50220b (added line and stories updated)
       action:'changed'
     }
   }
@@ -24,7 +30,13 @@ export const Default = Template.bind({})
 const defaultItems = [{ title: 'Beginner', checked: false }, { title: 'Intermediate', checked: false }, { title: 'Advanced', checked: false }]
 
 Default.args = {
+<<<<<<< HEAD
   items:defaultItems,
   title:'Level',
   getCheckboxes:(checkboxes) => console.log('Checkboxes: ', checkboxes)
+=======
+    items:defaultItems,
+    title:'Level',
+    getCheckbox:(checkbox) => console.log('Changed checkbox:', checkbox)
+>>>>>>> e50220b (added line and stories updated)
 }
