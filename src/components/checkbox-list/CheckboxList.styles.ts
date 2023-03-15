d6528268f40b67f05dff8f64f04efff9189fdc15
mxrcury <demetrgonchar@gmail.com>
2023-03-15 17:13:25 +0200

    changed title margin

--- src/components/checkbox-list/CheckboxList.styles.ts
+++ src/components/checkbox-list/CheckboxList.styles.ts
@@ -5,28 +5,13 @@ export const styles = {
     mb:'8px'
   },
   title:{
-<<<<<<< HEAD
     color:'primary.700',
     mb:'15px'
   },
   checkbox:{
-<<<<<<< HEAD
     '&.MuiCheckbox-root':{
       p:0,
       mr:'8px'
-=======
-    ml:'-10px',
-    '&.MuiCheckbox-root':{
-      padding:0
->>>>>>> f6766e3 (separate ts type)
-=======
-    color:'primary.700'
-  },
-  checkbox:{
-    '&.MuiCheckbox-root':{
-      p:0,
-      mr:'8px',
->>>>>>> 5752b50 (changed styles by removing negative values and update stories to jsx)
     }
   }
 }
