c69bf24532b8892b261e4935d341f6ea93c2e109
mxrcury <demetrgonchar@gmail.com>
2023-03-16 16:12:19 +0200

    hot fix

--- tests/unit/components/checkbox-list/CheckboxList.spec.jsx
+++ tests/unit/components/checkbox-list/CheckboxList.spec.jsx
@@ -1,5 +1,4 @@
 import { render , fireEvent, screen } from '@testing-library/react'
-<<<<<<< HEAD
 import { vi, expect } from 'vitest'
 import CheckboxList from '~/components/checkbox-list/CheckboxList'
 
@@ -7,14 +6,6 @@ const mockedItemToChange = { title: 'Beginner', checked: false }
 const mockedItemChanged = { ...mockedItemToChange, checked: true }
 
 const mockedItems =  [ { title: 'Intermediate', checked: false }, { title: 'Advanced', checked: false } ]
-=======
-import { vi } from 'vitest'
-import CheckboxList from '~/components/checkbox-list/CheckboxList'
-
-const mockedItemToChange = { title: 'Beginner', checked: false }
-
-const mockedItems =  [ mockedItemToChange, { title: 'Intermediate', checked: false }, { title: 'Advanced', checked: false } ]
->>>>>>> f086cc4 (updated AppSelect component to tsx and added som unit tests)
 
 const mockedGetCheckbox = vi.fn()
 
@@ -22,31 +13,19 @@ const titleId = 'checkboxes-list-title'
 
 describe('CheckboxList component', () => {
   it('should get checked state of checkbox on click', () => {
-<<<<<<< HEAD
     render(<CheckboxList getCheckboxes={ mockedGetCheckbox } items={ [ mockedItemToChange,...mockedItems] } title='Levels'  />)
-=======
-    render(<CheckboxList getCheckboxes={ mockedGetCheckbox } items={ mockedItems } title='Levels'  />)
->>>>>>> f086cc4 (updated AppSelect component to tsx and added som unit tests)
     
     const checkbox = screen.getByLabelText('Beginner')
     expect(checkbox.checked).toBe(false)
     
     fireEvent.click(checkbox)
 
-<<<<<<< HEAD
     expect(mockedGetCheckbox).toHaveBeenCalledWith([mockedItemChanged, ...mockedItems])
-=======
-    expect(mockedGetCheckbox).toHaveBeenCalledWith(mockedItems)
->>>>>>> f086cc4 (updated AppSelect component to tsx and added som unit tests)
   })
   it('should not render title element if no title in props was inserted', () => {
     render(<CheckboxList getCheckboxes={ mockedGetCheckbox } items={ mockedItems } />)
     
     const title = screen.queryByLabelText(titleId)
-<<<<<<< HEAD
-=======
-    console.log(title)
->>>>>>> f086cc4 (updated AppSelect component to tsx and added som unit tests)
 
     expect(title).toBeNull()
   })
