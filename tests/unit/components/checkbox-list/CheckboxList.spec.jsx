2fbf8708540069d57775673e04c17a1b4dee30ef
mxrcury <demetrgonchar@gmail.com>
2023-03-21 21:38:18 +0200

    fixed test

--- tests/unit/components/checkbox-list/CheckboxList.spec.jsx
+++ tests/unit/components/checkbox-list/CheckboxList.spec.jsx
@@ -7,7 +7,7 @@ const mockedItemChanged = { ...mockedItemToChange, checked: true }
 
 const mockedItems =  [ { title: 'Intermediate', checked: false }, { title: 'Advanced', checked: false } ]
 
-const mockedGetCheckbox = vi.fn()
+const mockedGetCheckbox = vi.fn((...args) => console.log(...args))
 
 const titleId = 'checkboxes-list-title'
 
