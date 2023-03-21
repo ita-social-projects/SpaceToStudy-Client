cc2b70468cd559cd1d4ffd3c1ae11948e59aaa2c
mxrcury <demetrgonchar@gmail.com>
2023-03-21 21:59:44 +0200

    fixed stories

--- src/stories/CheckboxList.stories.jsx
+++ src/stories/CheckboxList.stories.jsx
@@ -9,21 +9,9 @@ export default {
       type: 'string',
       description: 'Title of this component'
     },
-<<<<<<< HEAD
-<<<<<<< HEAD
     getCheckboxes: {
       type: 'function',
       description: 'Function for getting value of all checkboxes',
-=======
-    getCheckbox: {
-      type: 'function',
-      description: 'Function for getting value of changed checkbox',
->>>>>>> e50220b (added line and stories updated)
-=======
-    getCheckboxes: {
-      type: 'function',
-      description: 'Function for getting value of all checkboxes',
->>>>>>> f086cc4 (updated AppSelect component to tsx and added som unit tests)
       action:'changed'
     }
   }
@@ -36,19 +24,7 @@ export const Default = Template.bind({})
 const defaultItems = [{ title: 'Beginner', checked: false }, { title: 'Intermediate', checked: false }, { title: 'Advanced', checked: false }]
 
 Default.args = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   items:defaultItems,
   title:'Level',
   getCheckboxes:(checkboxes) => console.log('Checkboxes: ', checkboxes)
-=======
-    items:defaultItems,
-    title:'Level',
-    getCheckbox:(checkbox) => console.log('Changed checkbox:', checkbox)
->>>>>>> e50220b (added line and stories updated)
-=======
-  items:defaultItems,
-  title:'Level',
-  getCheckboxes:(checkboxes) => console.log('Checkboxes: ', checkboxes)
->>>>>>> f086cc4 (updated AppSelect component to tsx and added som unit tests)
 }
