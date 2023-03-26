11f6483fccf1c808bf4d720066a62644811443cd
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2023-03-26 11:54:23 +0300

    added RadioButtonList component

---
+++ src/components/radio-button-list/RadioButtonList.styles.ts
@@ -0,0 +1,10 @@
+export const styles = {
+  title: {
+    color: 'primary.700',
+    mb: '15px'
+  },
+  radioItems: {
+    color: 'black',
+    mb: '8px'
+  }
+}
---
+++ src/components/radio-button-list/RadioButtonList.types.ts
@@ -0,0 +1,4 @@
+export type RadioButtonType = {
+  title: string
+  value: number
+}
