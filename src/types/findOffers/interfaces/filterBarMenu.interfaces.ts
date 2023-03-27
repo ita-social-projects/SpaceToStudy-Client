8aadf728eed0453ebb88ec4a2c43d3b2d66215fc
mxrcury <demetrgonchar@gmail.com>
2023-03-27 13:07:16 +0300

    changed interface

--- src/types/findOffers/interfaces/filterBarMenu.interfaces.ts
+++ src/types/findOffers/interfaces/filterBarMenu.interfaces.ts
@@ -1,5 +1,8 @@
+import { CardsViewTypes } from '~/types'
+
 export interface BarMenuFilters {
   [key: string]: string | boolean,
   isActiveOffersType: boolean,
   sortBy: string,
+  offersView: CardsViewTypes
 }
