f27883b0d53bc2058918f351447b71df2052aef7
ArturBekhDEV <102412173+ArturBekhDEV@users.noreply.github.com>
2023-04-20 19:15:49 +0300

    fixed conflicts and change style to AppCard

--- src/components/app-card/AppCard.jsx
+++
@@ -1,9 +0,0 @@
-import Box from '@mui/material/Box'
-
-import { styles } from '~/components/app-card/AppCard.styles'
-
-const AppCard = ({ children, isClickable = true, sx }) => {
-  return <Box sx={{ ...styles.container(isClickable), ...sx }}>{children}</Box>
-}
-
-export default AppCard
--- src/components/app-card/AppCard.tsx
+++ src/components/app-card/AppCard.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 
 import Box from '@mui/material/Box'
+import { SxProps } from '@mui/material'
 
 import { styles } from '~/components/app-card/AppCard.styles'
 
@@ -9,13 +10,19 @@ interface AppCardProps {
   children: ReactNode
   isClickable?: boolean
   link?: string
+  sx?: SxProps
 }
 
-const AppCard: FC<AppCardProps> = ({ children, isClickable = true, link }) => {
+const AppCard: FC<AppCardProps> = ({
+  children,
+  isClickable = true,
+  link,
+  sx = {}
+}) => {
   return (
     <Box
       component={link ? Link : Box}
-      sx={styles.container(isClickable)}
+      sx={{ ...styles.container(isClickable), ...sx }}
       to={link}
     >
       {children}
