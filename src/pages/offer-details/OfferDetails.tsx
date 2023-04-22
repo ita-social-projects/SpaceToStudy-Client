9da05e8f826901e3ff0ffdbbdfa7de2cdfc8a4e2
Yura Didenko <didenkoyura225@gmail.com>
2023-04-22 12:24:00 +0300

    Added route for ofer details

---
+++ src/pages/offer-details/OfferDetails.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+
+const OfferDetails = () => {
+  return (
+    <Container sx={{ flex: 1 }}>
+      <Box>Offer Details</Box>
+    </Container>
+  )
+}
+
+export default OfferDetails
