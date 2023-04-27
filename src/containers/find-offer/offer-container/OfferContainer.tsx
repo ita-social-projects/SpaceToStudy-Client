d317b94b4b14729086357852d441e3c2247c6ba6
Pavlo Diakun <rubber883@gmail.com>
2023-04-27 10:01:47 +0300

    Fixed imports and putted out .map from JSX

--- src/containers/find-offer/offer-container/OfferContainer.tsx
+++ src/containers/find-offer/offer-container/OfferContainer.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react'
 import Box from '@mui/material/Box'
-import { Grid } from '@mui/material'
+import Grid from '@mui/material/Grid'
 import { CardsViewTypes, OfferResponse, CardsViewEnums } from '~/types'
-import OfferCardSquare from '../offer-card-square/OfferCardSquare'
+import OfferCardSquare from '~/containers/find-offer/offer-card-square/OfferCardSquare'
 import OfferCard from '~/components/offer-card/OfferCard'
 
 interface OfferContainerProps {
@@ -16,6 +16,24 @@ const OfferContainer: FC<OfferContainerProps> = ({ viewMode, offerCards }) => {
   }
 
   const columnNumber = viewMode === CardsViewEnums.Grid ? 12 : 1
+  const cardsArray =
+    viewMode === CardsViewEnums.Grid
+      ? offerCards.map((el) => (
+          <Grid item key={el._id} sm={4}>
+            <OfferCardSquare
+              offer={el}
+              onBookmarkClick={() => onBookmarkClick(el._id)}
+            />
+          </Grid>
+        ))
+      : offerCards.map((el) => (
+          <Grid item key={el._id} sm={4}>
+            <OfferCard
+              offer={el}
+              onBookmarkClick={() => onBookmarkClick(el._id)}
+            />
+          </Grid>
+        ))
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -24,23 +42,7 @@ const OfferContainer: FC<OfferContainerProps> = ({ viewMode, offerCards }) => {
         container
         spacing={{ md: 3 }}
       >
-        {viewMode === CardsViewEnums.Grid
-          ? offerCards.map((el) => (
-              <Grid item key={el._id} sm={4}>
-                <OfferCardSquare
-                  offer={el}
-                  onBookmarkClick={() => onBookmarkClick(el._id)}
-                />
-              </Grid>
-            ))
-          : offerCards.map((el) => (
-              <Grid item key={el._id} sm={4}>
-                <OfferCard
-                  offer={el}
-                  onBookmarkClick={() => onBookmarkClick(el._id)}
-                />
-              </Grid>
-            ))}
+        {cardsArray}
       </Grid>
     </Box>
   )
