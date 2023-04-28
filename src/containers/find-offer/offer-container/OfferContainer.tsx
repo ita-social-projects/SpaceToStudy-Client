1f5018dccfa4bf54755fde67a3387f6d710196fc
Pavlo Diakun <39992977+FryDay969@users.noreply.github.com>
2023-04-28 18:16:37 +0300

    Bug/fix of offer container (#652)
    * rebase commit
    
    * Padination implementation
    
    * Recomposed the components
    
    * delete unnecessary prop
    
    * Fixed grid
    
    * Folder moved, path changed
    
    * Fixed imports and putted out .map from JSX
    
    * Mapping refactored
    
    * Adaptability added to container
    
    * Refactored code
    
    * Test added
    
    * Test updated
    
    * Cleaned
    
    * fix

---
+++ src/containers/find-offer/offer-container/OfferContainer.tsx
@@ -0,0 +1,47 @@
+import useBreakpoints from '~/hooks/use-breakpoints'
+import { FC } from 'react'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import { CardsViewTypes, OfferResponse, CardsViewEnums } from '~/types'
+import OfferCardSquare from '~/containers/find-offer/offer-card-square/OfferCardSquare'
+import OfferCard from '~/components/offer-card/OfferCard'
+
+interface OfferContainerProps {
+  viewMode: CardsViewTypes
+  offerCards: OfferResponse[]
+}
+
+const OfferContainer: FC<OfferContainerProps> = ({ viewMode, offerCards }) => {
+  const { isMobile, isDesktop } = useBreakpoints()
+  const onBookmarkClick = (id: string) => {
+    console.log(id)
+  }
+
+  const columnNumber = viewMode === CardsViewEnums.Grid ? 12 : 1
+
+  const renderSquareCard =
+    isMobile || (isDesktop && viewMode === CardsViewEnums.Grid)
+
+  const offerItems = offerCards.map((el) => (
+    <Grid item key={el._id} sm={4}>
+      {renderSquareCard ? (
+        <OfferCardSquare
+          offer={el}
+          onBookmarkClick={() => onBookmarkClick(el._id)}
+        />
+      ) : (
+        <OfferCard offer={el} onBookmarkClick={() => onBookmarkClick(el._id)} />
+      )}
+    </Grid>
+  ))
+
+  return (
+    <Box data-testid='OfferContainer' sx={{ flexGrow: 1 }}>
+      <Grid columns={{ xs: 1, md: columnNumber }} container spacing={{ md: 3 }}>
+        {offerItems}
+      </Grid>
+    </Box>
+  )
+}
+
+export default OfferContainer
