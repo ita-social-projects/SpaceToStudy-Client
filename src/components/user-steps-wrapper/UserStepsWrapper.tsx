7993c16e0efe1e983a97dceb94e15f1c1c851d3d
Sarakhman Anatoliy <63456632+Tolik170@users.noreply.github.com>
2023-05-12 15:22:48 +0300

    implemented student steppers (#704)

--- src/components/user-steps-wrapper/UserStepsWrapper.tsx
+++ src/components/user-steps-wrapper/UserStepsWrapper.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { FC, useState } from 'react'
 import StepWrapper from '~/components/step-wrapper/StepWrapper'
 
 import { StepProvider } from '~/context/step-context'
-import { stepLabels } from '~/containers/tutor-home-page/constants'
 
 import GeneralInfoStep from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep'
 import AddPhotoStep from '~/containers/tutor-home-page/add-photo-step/AddPhotoStep'
 import SubjectsStep from '~/containers/tutor-home-page/subjects-step/SubjectsStep'
 import LanguageStep from '~/containers/tutor-home-page/language-step/LanguageStep'
 
-const BecomeATutor = () => {
+import {
+  studentStepLabels,
+  tutorStepLabels,
+  initialValues
+} from '~/components/user-steps-wrapper/constants'
+import { student } from '~/constants'
+
+interface UserStepsWrapperProps {
+  userRole: string
+}
+
+const UserStepsWrapper: FC<UserStepsWrapperProps> = ({ userRole }) => {
   const [isUserFetched, setIsUserFetched] = useState(false)
 
   const childrenArr = [
@@ -23,11 +33,13 @@ const BecomeATutor = () => {
     <AddPhotoStep key='4' />
   ]
 
+  const stepLabels = userRole === student ? studentStepLabels : tutorStepLabels
+
   return (
-    <StepProvider>
+    <StepProvider initialValues={initialValues} stepLabels={stepLabels}>
       <StepWrapper steps={stepLabels}>{childrenArr}</StepWrapper>
     </StepProvider>
   )
 }
 
-export default BecomeATutor
+export default UserStepsWrapper
