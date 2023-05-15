7f37ca843f211779609c770928d90665bab84ce2
MickhaelSh <gmerdok@gmail.com>
2023-05-15 22:45:35 +0300

    changed name in mocked coop

--- src/pages/my-cooperations/MyCooperations.tsx
+++ src/pages/my-cooperations/MyCooperations.tsx
@@ -12,8 +12,8 @@ const mockedCoop = {
     subject: { _id: 'id', name: 'Quantum Mechanics' }
   },
   user: {
-    firstName: 'Tolik',
-    lastName: 'Bolik',
+    firstName: 'Kathryn',
+    lastName: 'Murphy',
     photo:
       'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
   },
