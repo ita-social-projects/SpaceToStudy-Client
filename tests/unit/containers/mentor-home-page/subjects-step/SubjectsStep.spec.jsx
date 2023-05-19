fce756113b13805861f5cb9aff2725109cd93597
Pavlo Diakun <39992977+FryDay969@users.noreply.github.com>
2023-05-19 22:38:23 +0300

    Added changes (#738)

--- tests/unit/containers/mentor-home-page/subjects-step/SubjectsStep.spec.jsx
+++ tests/unit/containers/mentor-home-page/subjects-step/SubjectsStep.spec.jsx
@@ -1,13 +1,40 @@
-import { render, screen, fireEvent } from '@testing-library/react'
-
+import { screen, fireEvent } from '@testing-library/react'
+import { renderWithProviders } from '~tests/test-utils'
 import SubjectsStep from '~/containers/tutor-home-page/subjects-step/SubjectsStep'
 import { ModalProvider } from '~/context/modal-context'
 import { StepProvider } from '~/context/step-context'
+
+import { categoryService } from '~/services/category-service'
+import { subjectService } from '~/services/subject-service'
+
+vi.mock('axios')
+vi.mock('~/services/category-service')
+vi.mock('~/services/subject-service')
 
 import {
   initialValues,
   tutorStepLabels
 } from '~/components/user-steps-wrapper/constants'
+
+const mockCategoriesNames = [
+  { _id: '1', name: 'Category 1' },
+  { _id: '2', name: 'Category 2' },
+  { _id: '3', name: 'Category 3' },
+  { _id: '4', name: 'Category 4' }
+]
+
+const mockSubjectsNames = [
+  { _id: '5', name: 'Subject 1' },
+  { _id: '6', name: 'Subject 2' }
+]
+
+categoryService.getCategoriesNames.mockResolvedValue({
+  data: mockCategoriesNames
+})
+
+subjectService.getSubjectsNames.mockResolvedValue({
+  data: mockSubjectsNames
+})
 
 const btnsBox = (
   <div>
@@ -18,7 +45,7 @@ const btnsBox = (
 
 describe('SubjectsStep test with some data', () => {
   beforeEach(() => {
-    render(
+    renderWithProviders(
       <ModalProvider>
         <StepProvider
           initialValues={initialValues}
@@ -28,61 +55,85 @@ describe('SubjectsStep test with some data', () => {
         </StepProvider>
       </ModalProvider>
     )
-    const addSubject = screen.getByTestId('add-subject')
-    const firstField = screen.getByLabelText(
-      /becomeTutor.categories.mainSubjectsLabel/i
-    )
-
-    expect(addSubject).toBeInTheDocument()
-
-    fireEvent.click(firstField)
-    fireEvent.change(firstField, { target: { value: 'Languages' } })
-    fireEvent.keyDown(firstField, { key: 'ArrowDown' })
-    fireEvent.keyDown(firstField, { key: 'Enter' })
-
-    const secondField = screen.getByLabelText(
-      /becomeTutor.categories.subjectLabel/i
-    )
-
-    fireEvent.click(secondField)
-    fireEvent.change(secondField, { target: { value: 'Danish' } })
-    fireEvent.keyDown(secondField, { key: 'ArrowDown' })
-    fireEvent.keyDown(secondField, { key: 'Enter' })
-    fireEvent.click(addSubject)
   })
 
   it('should add a new subject', async () => {
     const addSubject = screen.getByTestId('add-subject')
-    const firstField = screen.getByLabelText(
-      /becomeTutor.categories.mainSubjectsLabel/i
-    )
 
     expect(addSubject).toBeInTheDocument()
 
-    fireEvent.click(firstField)
-    fireEvent.change(firstField, { target: { value: 'Languages' } })
-    fireEvent.keyDown(firstField, { key: 'ArrowDown' })
-    fireEvent.keyDown(firstField, { key: 'Enter' })
-
-    const secondField = screen.getByLabelText(
-      /becomeTutor.categories.subjectLabel/i
+    const firstField = screen.getAllByLabelText(
+      'becomeTutor.categories.mainSubjectsLabel'
     )
 
-    fireEvent.click(secondField)
-    fireEvent.change(secondField, { target: { value: 'English' } })
-    fireEvent.keyDown(secondField, { key: 'ArrowDown' })
-    fireEvent.keyDown(secondField, { key: 'Enter' })
+    fireEvent.click(firstField[0])
+    fireEvent.change(firstField[0], {
+      target: { value: 'Category 2' }
+    })
+    fireEvent.keyDown(firstField[0], { key: 'ArrowDown' })
+    fireEvent.keyDown(firstField[0], { key: 'Enter' })
+
+    expect(firstField[0].value).toBe('Category 2')
+
+    const secondField = screen.getByLabelText(
+      'becomeTutor.categories.subjectLabel'
+    )
+
+    fireEvent.click(await secondField)
+    fireEvent.change(await secondField, {
+      target: { value: 'Subject 2' }
+    })
+    fireEvent.keyDown(await secondField, { key: 'ArrowDown' })
+    fireEvent.keyDown(await secondField, { key: 'Enter' })
+
+    expect(secondField.value).toBe('Subject 2')
+
     fireEvent.click(addSubject)
-    const chips = await screen.findAllByTestId('chip')
-    expect(chips.length).toBe(2)
+
+    const chip = await screen.findByTestId('chip')
+    expect(chip).toBeInTheDocument()
   })
 
-  it('should delete a subject', () => {
-    const closeBtn = screen.queryAllByTestId('close-btn')[0]
-    const firstChip = screen.queryAllByTestId('chip')
+  it('should delete a subject', async () => {
+    const addSubject = screen.getByTestId('add-subject')
 
-    expect(firstChip.length).toBe(1)
-    fireEvent.click(closeBtn)
+    expect(addSubject).toBeInTheDocument()
+
+    const firstField = screen.getAllByLabelText(
+      'becomeTutor.categories.mainSubjectsLabel'
+    )
+
+    fireEvent.click(firstField[0])
+    fireEvent.change(firstField[0], {
+      target: { value: 'Category 2' }
+    })
+    fireEvent.keyDown(firstField[0], { key: 'ArrowDown' })
+    fireEvent.keyDown(firstField[0], { key: 'Enter' })
+
+    expect(firstField[0].value).toBe('Category 2')
+
+    const secondField = screen.getByLabelText(
+      'becomeTutor.categories.subjectLabel'
+    )
+
+    fireEvent.click(await secondField)
+    fireEvent.change(await secondField, {
+      target: { value: 'Subject 2' }
+    })
+    fireEvent.keyDown(await secondField, { key: 'ArrowDown' })
+    fireEvent.keyDown(await secondField, { key: 'Enter' })
+
+    expect(secondField.value).toBe('Subject 2')
+
+    fireEvent.click(addSubject)
+
+    const chip = await screen.findByTestId('chip')
+    expect(chip).toBeInTheDocument()
+
+    const closeBtn = screen.getByTestId('close-btn')
+
+    fireEvent.click(await closeBtn)
+    expect(chip).not.toBeInTheDocument()
   })
 
   it('should show an error message "All fields must be filled"', () => {
@@ -116,7 +167,7 @@ describe('SubjectsStep test with some data', () => {
     )
 
     fireEvent.click(firstField)
-    fireEvent.change(firstField, { target: { value: 'Languages' } })
+    fireEvent.change(firstField, { target: { value: 'Category 2' } })
     fireEvent.keyDown(firstField, { key: 'ArrowDown' })
     fireEvent.keyDown(firstField, { key: 'Enter' })
 
@@ -124,10 +175,12 @@ describe('SubjectsStep test with some data', () => {
       /becomeTutor.categories.subjectLabel/i
     )
 
-    fireEvent.click(secondField)
-    fireEvent.change(secondField, { target: { value: 'Ukranian' } })
-    fireEvent.keyDown(secondField, { key: 'ArrowDown' })
-    fireEvent.keyDown(secondField, { key: 'Enter' })
+    fireEvent.click(await secondField)
+    fireEvent.change(await secondField, {
+      target: { value: 'Subject 2' }
+    })
+    fireEvent.keyDown(await secondField, { key: 'ArrowDown' })
+    fireEvent.keyDown(await secondField, { key: 'Enter' })
     fireEvent.click(addSubject)
     fireEvent.click(addSubject)
 
