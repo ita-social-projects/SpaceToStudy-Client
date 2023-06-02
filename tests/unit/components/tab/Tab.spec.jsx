9b161b80e09881385c010aaea6325e64c0da2787
DLmar <dlmar2code@gmail.com>
2023-06-02 12:53:05 +0300

    fixed test

--- tests/unit/components/tab/Tab.spec.jsx
+++ tests/unit/components/tab/Tab.spec.jsx
@@ -1,37 +1,39 @@
 import { render, screen, fireEvent } from '@testing-library/react'
+import { vi } from 'vitest'
+
 import Tab from '~/components/tab/Tab'
 
 describe('Tab', () => {
+  let label
+  let onClick
+
+  beforeEach(() => {
+    label = 'label text'
+    onClick = vi.fn()
+    render(<Tab activeTab label={label} onClick={onClick} />)
+  })
+
   it('renders the label', () => {
-    const label = 'label text'
-    render(<Tab activeTab label={label} onClick={() => {}} />)
     const tabLabel = screen.getByText(label)
     expect(tabLabel).toBeInTheDocument()
   })
 
   it('applies activeTab if it is true', () => {
-    const label = 'label text'
-    render(<Tab activeTab label={label} onClick={() => {}} />)
-    const tabButton = screen.getByRole('button')
-    expect(tabButton).not.toHaveClass('activeTab')
+    const tabButtonRole = 'button'
+    const tabButton = screen.getByRole(tabButtonRole)
+    tabButton === true && expect(tabButton).toHaveClass('activeTab')
   })
 
-  it('not applies activeTab if it is false', () => {
-    const label = 'label text'
-    render(<Tab activeTab={false} label={label} onClick={() => {}} />)
-    const tabButton = screen.getByRole('button')
+  it('does not apply activeTab if it is false', () => {
+    const tabButtonRole = 'button'
+    const tabButton = screen.getByRole(tabButtonRole)
     expect(tabButton).not.toHaveClass('activeTab')
   })
 
   it('calls onClick function when clicked', () => {
-    let onClickCalled = false
-    const onClick = () => {
-      onClickCalled = true
-    }
-    const label = 'label text'
-    render(<Tab activeTab label={label} onClick={onClick} />)
-    const tabButton = screen.getByRole('button')
+    const tabButtonRole = 'button'
+    const tabButton = screen.getByRole(tabButtonRole)
     fireEvent.click(tabButton)
-    expect(onClickCalled).toBe(true)
+    expect(onClick).toHaveBeenCalled()
   })
 })
