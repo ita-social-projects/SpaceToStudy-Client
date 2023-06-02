df6522fbf749e5296d375e5dde123718a6800323
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-06-02 13:06:04 +0300

    Removed unnecessary case

--- tests/unit/components/page-wrapper/PageWrapper.spec.jsx
+++ tests/unit/components/page-wrapper/PageWrapper.spec.jsx
@@ -1,7 +1,5 @@
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 
-import { styles } from '~/components/page-wrapper/PageWrapper.styles'
-
 import { render } from '@testing-library/react'
 
 describe('PageWrapper', () => {
@@ -9,17 +7,5 @@ describe('PageWrapper', () => {
     const children = <div>Test children</div>
     const { getByText } = render(<PageWrapper>{children}</PageWrapper>)
     expect(getByText('Test children')).toBeInTheDocument()
-  })
-
-  it('should have the correct container styles', () => {
-    const expectedContainerStyles = {
-      flex: 1,
-      display: 'flex',
-      flexDirection: 'column',
-      mb: '100px',
-      mt: { xs: 0, sm: '16px' }
-    }
-
-    expect(styles.container).toEqual(expectedContainerStyles)
   })
 })
