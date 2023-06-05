64bed3fe87ba1282933a213368d8cd363a385a50
mxrcury <demetrgonchar@gmail.com>
2023-06-05 18:59:09 +0300

    removed unnecessary styles

--- tests/unit/components/app-carousel/AppCarousel.spec.jsx
+++ tests/unit/components/app-carousel/AppCarousel.spec.jsx
@@ -1,37 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import { beforeEach } from 'vitest'
 import AppCarousel from '~/components/app-carousel/AppCarousel'
-import palette from '~/styles/app-theme/app.pallete'
 
 const childrenText = 'I am a children'
-
-const arrowStyles = { typography: 'h5', color: 'primary.500' }
-
-const carouselSettings = {
-  slidesToShow: 3,
-  defaultControlsConfig: {
-    pagingDotsStyle: { display: 'block' }
-  },
-  leftButtonStyles: {
-    display: 'inline-flex',
-    left: '-34px',
-    stroke: palette.primary[500],
-    strokeWidth: 2
-  },
-  rightButtonStyles: {
-    display: 'inline-flex',
-    right: '-34px',
-    stroke: palette.primary[500],
-    strokeWidth: 2
-  },
-  leftArrowStyles: arrowStyles,
-  rightArrowStyles: arrowStyles
-}
 
 describe('AppCarousel test', () => {
   beforeEach(() => {
     render(
-      <AppCarousel settings={carouselSettings}>
+      <AppCarousel settings={{}}>
         <p>{childrenText}</p>
       </AppCarousel>
     )
