d7cdb41456432861859fbdfe00fb35e95a13887a
Taras Chaika <45912519+TSlashDreamy@users.noreply.github.com>
2023-06-07 11:13:23 +0300

    Added proper route to tutor categories page (#821)
    * added proper route to categories page
    
    * removed useless faq route

--- src/router/constants/tutorRoutes.ts
+++ src/router/constants/tutorRoutes.ts
@@ -1,7 +1,8 @@
+import { authRoutes } from '~/router/constants/authRoutes'
+
 export const tutorRoutes = {
   navBar: {
-    categories: { route: 'categories', path: '/tutor/#categories' },
-    faq: { route: 'faq', path: '/tutor/#faq' }
+    categories: { route: 'categories', path: authRoutes.categories.path }
   },
   accountMenu: {
     myProfile: { route: 'my-profile', path: '/tutor/my-profile' },
