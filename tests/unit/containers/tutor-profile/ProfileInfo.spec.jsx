87d341de187aeec16e169046615e0cf0389de9a4
Pavlo Diakun <rubber883@gmail.com>
2023-06-08 23:23:08 +0300

    Test Update

--- tests/unit/containers/tutor-profile/ProfileInfo.spec.jsx
+++ tests/unit/containers/tutor-profile/ProfileInfo.spec.jsx
@@ -12,6 +12,40 @@ Object.assign(window.navigator, {
   }
 })
 
+const userData = {
+  _id: '64822a1433ebe4890079bb60',
+  role: ['tutor'],
+  firstName: 'Commander',
+  lastName: 'Shepard',
+  email: 'rubber883@gmail.com',
+  mainSubjects: {
+    student: [
+      {
+        _id: '645b9f4a1c0272f5cde0e11e',
+        name: 'Danish',
+        category: '6459347d943e375d1c0a1912',
+        totalOffers: 0
+      },
+      {
+        _id: '6422d995d898aa732d038e8f',
+        name: 'Guitar',
+        category: '6421ed8ed991d46a84721dfa',
+        totalOffers: 4
+      },
+      {
+        _id: '6422ad6a74c1353b96c7c132',
+        name: 'Web design',
+        category: '6421ed8ed991d46a84721df4',
+        totalOffers: 9
+      }
+    ],
+    tutor: []
+  },
+  photo: '../src/John_Shepard_29.jpeg',
+  professionalSummary:
+    'Some amount of text regarding the proficiency.Some amount of text rega'
+}
+
 describe('ProfileInfo test in my profile', () => {
   beforeEach(() => {
     vi.mock('react-router-dom', async () => {
@@ -25,7 +59,7 @@ describe('ProfileInfo test in my profile', () => {
     })
     renderWithProviders(
       <SnackBarProvider>
-        <ProfileInfo />
+        <ProfileInfo userData={userData} />
       </SnackBarProvider>
     )
   })
@@ -39,7 +73,9 @@ describe('ProfileInfo test in my profile', () => {
   })
 
   it('should render send message button', () => {
-    const sendMessageBtn = screen.getByText(/tutorProfilePage.profileInfo.sendMessage/i)
+    const sendMessageBtn = screen.getByText(
+      /tutorProfilePage.profileInfo.sendMessage/i
+    )
 
     expect(sendMessageBtn).toBeInTheDocument()
   })
