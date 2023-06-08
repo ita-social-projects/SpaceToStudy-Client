a194824b41c0e14274f63fe8caf2edca86de7ad8
Denys Lopushanskyi <89192246+DLmar@users.noreply.github.com>
2023-06-09 00:29:03 +0300

    (SP: 1) Write unit test for tab (#810)
    * unit test for Tab
    
    * fixed test
    
    * updated test
    
    * update2 test
    
    * updated test
    
    * fixed, variables outside

---
+++ tests/unit/components/tab/Tab.spec.jsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { vi } from 'vitest'
+
+import Tab from '~/components/tab/Tab'
+
+let label = 'text'
+let onClick = vi.fn()
+describe('Tab', () => {
+  beforeEach(() => {
+    render(<Tab activeTab label={label} onClick={onClick} />)
+  })
+
+  it('renders the label', () => {
+    const tabLabel = screen.getByText(label)
+
+    expect(tabLabel).toBeInTheDocument()
+  })
+
+  it('applies activeTab if it is true', () => {
+    const tabButton = screen.getByRole('button')
+
+    if (tabButton.classList.contains('activeTab')) {
+      expect(tabButton).toHaveClass('activeTab')
+    }
+  })
+
+  it('does not apply activeTab if it is false', () => {
+    const tabButton = screen.getByRole('button')
+
+    expect(tabButton).not.toHaveClass('activeTab')
+  })
+
+  it('calls onClick function when clicked', () => {
+    const tabButton = screen.getByRole('button')
+
+    fireEvent.click(tabButton)
+
+    expect(onClick).toHaveBeenCalled()
+  })
+})
