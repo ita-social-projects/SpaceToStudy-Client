070e95bcac379ee6696c7de0899d79b7179eddbe
Pavlo Diakun <rubber883@gmail.com>
2023-06-09 13:51:52 +0300

    Styles were rewritten and added spaces to use-user-info

--- src/hooks/use-user-info.jsx
+++ src/hooks/use-user-info.jsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react'
 import { userService } from '~/services/user-service'
+
 import useAxios from '~/hooks/use-axios'
 import { defaultResponses } from '~/constants'
 
