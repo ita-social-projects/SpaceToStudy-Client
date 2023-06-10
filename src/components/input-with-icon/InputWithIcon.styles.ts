25954a46f240002f52faa3e012650aca42a88dc4
DLmar <dlmar2code@gmail.com>
2023-06-10 11:55:06 +0300

    fixed px for mobile

--- src/components/input-with-icon/InputWithIcon.styles.ts
+++ src/components/input-with-icon/InputWithIcon.styles.ts
@@ -2,7 +2,7 @@ export const styles = {
   root: {
     display: 'flex',
     alignItems: 'center',
-    px: '14px',
+    px: { xs: '8px', sm: '14px' },
     py: { xs: '7px', sm: 0 },
     gap: '12px'
   },
