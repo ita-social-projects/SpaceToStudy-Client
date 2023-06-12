6d33c832396167236e35b6908d9b13ca2f0c2f61
Pavlo Diakun <rubber883@gmail.com>
2023-06-12 21:43:34 +0300

    All data fetched

---
+++ src/containers/tutor-profile/profile-info/ProfileInfo.constants.jsx
@@ -0,0 +1,40 @@
+import Typography from '@mui/material/Typography'
+import StarIcon from '@mui/icons-material/Star'
+import HashLink from '~/components/hash-link/HashLink'
+
+import { styles } from '~/containers/tutor-profile/profile-info/ProfileInfo.styles'
+
+const accountRating = (
+  <>
+    <StarIcon sx={styles.ratingIcon} />
+    {'5'}
+  </>
+)
+
+const linkToReviews = (
+  <Typography component={HashLink} to={'#'} variant='overline'>
+    {'23 reviews'}
+  </Typography>
+)
+
+export const accountInfoMock = [
+  {
+    title: '3 years',
+    description: 'at space2study'
+  },
+  {
+    title: accountRating,
+    description: linkToReviews
+  },
+  {
+    title: '75 UAH',
+    description: 'hourly rate'
+  }
+]
+
+export const doneItemsMock = [
+  { title: '251', description: 'lessons' },
+  { title: 'Native: ', description: 'English' },
+  { title: 'Responds in', description: '5 hours' },
+  { title: 'Based in', description: 'Lviv, Ukraine' }
+]
