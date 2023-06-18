cfc068403bfcf165f2a123b646a68ad93dbeadc9
Yura Didenko <didenkoyura225@gmail.com>
2023-06-18 11:19:52 +0300

    Fixed warning with ref

--- src/components/page-wrapper/PageWrapper.tsx
+++ src/components/page-wrapper/PageWrapper.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react'
+import { FC, forwardRef, useEffect } from 'react'
 
 import Container, { ContainerProps } from '@mui/material/Container'
 
@@ -6,18 +6,27 @@ import { spliceSx } from '~/utils/helper-functions'
 import { styles } from '~/components/page-wrapper/PageWrapper.styles'
 import { useModalContext } from '~/context/modal-context'
 
-const PageWrapper: FC<ContainerProps> = ({ children, sx, ...rest }) => {
-  const { closeModal } = useModalContext()
+const PageWrapper: FC<ContainerProps> = forwardRef(
+  ({ children, sx, ...rest }, ref) => {
+    const { closeModal } = useModalContext()
 
-  useEffect(() => {
-    return () => closeModal()
-  }, [closeModal])
+    useEffect(() => {
+      return () => closeModal()
+    }, [closeModal])
 
-  return (
-    <Container maxWidth='xl' sx={spliceSx(styles.container, sx)} {...rest}>
-      {children}
-    </Container>
-  )
-}
+    return (
+      <Container
+        maxWidth='xl'
+        ref={ref}
+        sx={spliceSx(styles.container, sx)}
+        {...rest}
+      >
+        {children}
+      </Container>
+    )
+  }
+)
+
+PageWrapper.displayName = 'PageWrapper'
 
 export default PageWrapper
