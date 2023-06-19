102b972af642d6c95ff835a4d73cee553cfbc720
Taras Chaika <45912519+TSlashDreamy@users.noreply.github.com>
2023-06-20 00:01:27 +0300

    added popular categories (#871)

--- src/pages/tutor-home/TutorHome.tsx
+++ src/pages/tutor-home/TutorHome.tsx
@@ -1,16 +1,19 @@
 import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { useAppSelector } from '~/hooks/use-redux'
 import { useModalContext } from '~/context/modal-context'
 
 import UserStepsWrapper from '~/components/user-steps-wrapper/UserStepsWrapper'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import PopularCategories from '~/components/popular-categories/PopularCategories'
 import FindBlock from '~/components/find-block/FindBlock'
 
 import { styles } from '~/pages/tutor-home/TutorHome.styles'
 import { translationKey } from '~/components/find-block/find-student-constants'
 
 const TutorHome = () => {
+  const { t } = useTranslation()
   const { openModal } = useModalContext()
   const { isFirstLogin, userRole } = useAppSelector((state) => state.appMain)
 
@@ -28,6 +31,7 @@ const TutorHome = () => {
   return (
     <PageWrapper data-testid='tutorHome'>
       <FindBlock translationKey={translationKey} />
+      <PopularCategories title={t('common.popularCategories')} />
     </PageWrapper>
   )
 }
