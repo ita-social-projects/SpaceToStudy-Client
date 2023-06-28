2f4c7b956dd5eaee8e537d5a236c4db7a8ce21b5
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-06-28 18:33:57 +0300

    commit

--- src/components/input-with-icon/InputWithIcon.styles.ts
+++ src/components/input-with-icon/InputWithIcon.styles.ts
@@ -4,7 +4,8 @@ export const styles = {
     alignItems: 'center',
     px: { xs: '8px', sm: '14px' },
     py: { xs: '7px', sm: 0 },
-    gap: '12px'
+    gap: '12px',
+    minHeight: { xs: '40px', sm: 0 }
   },
   input: {
     flex: 1
