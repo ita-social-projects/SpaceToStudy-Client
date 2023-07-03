d721c6913dc762d11dc8ac5a17ed8687c21fc6ba
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-07-03 19:41:11 +0300

    Change container size  (#933)
    * commit
    
    * Change container

--- src/components/input-with-icon/InputWithIcon.styles.ts
+++ src/components/input-with-icon/InputWithIcon.styles.ts
@@ -3,8 +3,9 @@ export const styles = {
     display: 'flex',
     alignItems: 'center',
     px: { xs: '8px', sm: '14px' },
-    py: { xs: '7px', sm: 0 },
-    gap: '12px'
+    py: { xs: '4px', sm: 0 },
+    gap: '12px',
+    minHeight: { xs: '40px' }
   },
   input: {
     flex: 1
