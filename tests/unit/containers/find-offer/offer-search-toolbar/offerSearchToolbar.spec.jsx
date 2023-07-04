d6f19b8cffd391747dd59af4c0f878081e510804
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-07-04 11:53:04 +0300

    main commit

---
+++ tests/unit/containers/find-offer/offer-search-toolbar/offerSearchToolbar.spec.jsx
@@ -0,0 +1,61 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { vi } from 'vitest'
+import OfferSearchToolbar from '~/containers/find-offer/offer-search-toolbar/OfferSearchToolbar'
+import useBreakpoints from '~/hooks/use-breakpoints'
+import { beforeEach } from 'vitest'
+import { defaultFilters } from '~/pages/find-offers/FindOffers.constants'
+
+vi.mock('~/hooks/use-breakpoints')
+const filterActions = {
+  updateFilterInQuery: vi.fn()
+}
+const filters = defaultFilters('student')
+const resetPage = vi.fn()
+
+useBreakpoints.mockImplementation(() => ({ isLaptopAndAbove: true }))
+
+describe('OfferSearchToolbar', () => {
+  beforeEach(() => {
+    render(
+      <OfferSearchToolbar
+        filterActions={filterActions}
+        filters={filters}
+        resetPage={resetPage}
+      />
+    )
+  })
+
+  it('should find right category value', () => {
+    const testValue = 'Music'
+    const categories = screen.getByLabelText('breadCrumbs.categories')
+
+    fireEvent.click(categories)
+    fireEvent.change(categories, { target: { value: testValue } })
+    fireEvent.keyDown(categories, { key: 'Enter' })
+
+    expect(categories.value).toBe(testValue)
+  })
+
+  it('should find right subject value', () => {
+    const testValue = 'Web design'
+    const subjects = screen.getByLabelText('breadCrumbs.subjects')
+
+    fireEvent.click(subjects)
+    fireEvent.change(subjects, { target: { value: testValue } })
+    fireEvent.keyDown(subjects, { key: 'Enter' })
+
+    expect(subjects.value).toBe(testValue)
+  })
+
+  it('should test search input with button', () => {
+    const testValue = 'music'
+    const search = screen.getByRole('textbox')
+    const searchBtn = screen.getByRole('button')
+
+    fireEvent.click(search)
+    fireEvent.change(search, { target: { value: testValue } })
+    fireEvent.click(searchBtn)
+
+    expect(search.value).toBe(testValue)
+  })
+})
