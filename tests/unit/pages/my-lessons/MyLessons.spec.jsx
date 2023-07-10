7213f40043c1b1bd53d36a91366962e88fee5e52
DLmar <dlmar2code@gmail.com>
2023-07-10 17:03:51 +0300

    added test

---
+++ tests/unit/pages/my-lessons/MyLessons.spec.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from '~tests/test-utils'
+
+import MyLessons from '~/pages/my-lessons/MyLessons'
+
+describe('MyLessons', () => {
+  beforeEach(() => {
+    renderWithProviders(<MyLessons />)
+  })
+
+  it('renders the component with tabs', () => {
+    const tab1 = screen.getByText('All lessons')
+    const tab2 = screen.getByText('Grouped lessons')
+
+    expect(tab1).toBeInTheDocument()
+    expect(tab2).toBeInTheDocument()
+  })
+})
