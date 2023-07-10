a08f99a6d09c6d13f78685b96eda1cf8dec320cb
DLmar <dlmar2code@gmail.com>
2023-07-10 13:00:07 +0300

    created MyLessons.constants

--- src/pages/my-lessons/MyLessons.tsx
+++ src/pages/my-lessons/MyLessons.tsx
@@ -4,28 +4,9 @@ import Box from '@mui/material/Box'
 import Tab from '~/components/tab/Tab'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 
-import { styles } from '~/pages/my-lessons/MyLessons.styles'
+import { listDataMock } from './MyLessons.constants'
 
-const listDataMock = [
-  {
-    id: 0,
-    title: 'All lessons',
-    items: [
-      { id: 1, title: 'Item-1' },
-      { id: 2, title: 'Item-2' },
-      { id: 3, title: 'Item-3' }
-    ]
-  },
-  {
-    id: 1,
-    title: 'Grouped lessons',
-    items: [
-      { id: 1, title: 'Item-11' },
-      { id: 2, title: 'Item-22' },
-      { id: 3, title: 'Item-33' }
-    ]
-  }
-]
+import { styles } from '~/pages/my-lessons/MyLessons.styles'
 
 const MyLessons = () => {
   const [value, setValue] = useState<number>(0)
