b4897f24df9ed69adaa636a09bab16bbfb228a1f
Yura Didenko <didenkoyura225@gmail.com>
2023-08-07 17:43:23 +0300

    Implemented unit test for FindOffers component

---
+++ tests/unit/pages/find-offers/FindOffers.constants.js
@@ -0,0 +1,323 @@
+export const offersMock = {
+  items: [
+    {
+      _id: '64cc01437b124cfa8bb57e0d',
+      price: 250,
+      proficiencyLevel: ['Intermediate'],
+      title: 'Piano teaching ',
+      description: 'Piano training is needed ',
+      languages: ['Ukrainian', 'English'],
+      authorRole: 'student',
+      author: {
+        _id: '64241ac11920ac9e56b79aef',
+        firstName: 'Khrystyna',
+        lastName: 'Pavlikovska',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '648850ddfdc2d1a130c24af3', name: 'Piano' },
+      category: {
+        _id: '64884f21fdc2d1a130c24ac0',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-08-03T19:34:27.647Z',
+      updatedAt: '2023-08-03T19:34:27.647Z'
+    },
+    {
+      _id: '64c6430055ca02e87a340bbf',
+      price: 1,
+      proficiencyLevel: ['Beginner'],
+      title: '11',
+      description: 'test tes tes tes tes test',
+      languages: ['English'],
+      authorRole: 'student',
+      author: {
+        _id: '64c39dedd488960d0caa3cd0',
+        firstName: 'Ілля',
+        lastName: 'Наконечний',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 },
+        nativeLanguage: 'Ukrainian',
+        photo: '',
+        professionalSummary: ''
+      },
+      subject: { _id: '64885108fdc2d1a130c24af9', name: 'Cybersecurity' },
+      category: {
+        _id: '64884f33fdc2d1a130c24ac2',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-07-30T11:01:20.785Z',
+      updatedAt: '2023-07-31T14:10:14.867Z'
+    },
+    {
+      _id: '64bc01f29bbe3a53a746be3e',
+      price: 300,
+      proficiencyLevel: ['Intermediate', 'Advanced'],
+      title:
+        'I desire to get a degree of proffessional specialist of cybersecurity',
+      description:
+        'I look for senior in cybersecurity who has a flexible working hours',
+      languages: ['Ukrainian'],
+      authorRole: 'student',
+      author: {
+        _id: '64b79d41d8ee5eae0897fb93',
+        firstName: 'Anastasiia',
+        lastName: 'Mashchenko',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '64885108fdc2d1a130c24af9', name: 'Cybersecurity' },
+      category: {
+        _id: '64884f33fdc2d1a130c24ac2',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-07-22T16:21:06.023Z',
+      updatedAt: '2023-07-22T16:21:06.023Z'
+    },
+    {
+      _id: '64bb9c779bbe36a53a746bc88',
+      price: 200,
+      proficiencyLevel: ['Beginner'],
+      title:
+        'I would like to become a famous guitar player and be the same stage with Slash',
+      description: "I'd like to be aware of everything",
+      languages: ['English', 'Ukrainian'],
+      authorRole: 'student',
+      author: {
+        _id: '64b79d41d8ee5e6ae0897fb93',
+        firstName: 'Anastasiia',
+        lastName: 'Lopez',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '648850c4fdc2d1a130c24aea', name: 'Guitar' },
+      category: {
+        _id: '64884f21fdc2d1a130c24ac0',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-07-22T09:08:07.413Z',
+      updatedAt: '2023-07-22T09:08:07.413Z'
+    },
+    {
+      _id: '64ac054c1307130e45017f841',
+      price: 3232,
+      proficiencyLevel: ['Beginner', 'Intermediate', 'Advanced'],
+      title: '121212',
+      description: 'wewewewewfdfdfdfdfdfdfdfdfd',
+      languages: ['English'],
+      authorRole: 'student',
+      author: {
+        _id: '64944409a3a3698915f9663f9',
+        firstName: 'Анатолій',
+        lastName: 'Сарахман',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '648850c4fdc2d1a130c24aea', name: 'Guitar' },
+      category: {
+        _id: '64884f21fdc2d1a130c24ac0',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [
+        { question: '12121', answer: '211', _id: '64ac054c137130e45017f842' }
+      ],
+      createdAt: '2023-07-10T13:19:08.934Z',
+      updatedAt: '2023-07-18T12:31:24.088Z'
+    },
+    {
+      _id: '64944a54e8764088383fdfb84',
+      price: 505,
+      proficiencyLevel: ['Professional'],
+      title: 'Cybersecurity proffesional',
+      description:
+        "Please describe any additional information about the tutoring you need, such as specific topics or areas you'd like to focus on, your preferred teaching style, or any particular goals you're hoping to achieve.1",
+      languages: ['German', 'Polish'],
+      authorRole: 'student',
+      author: {
+        _id: '64944409a3a8368915f9663f9',
+        firstName: 'Анатолій',
+        lastName: 'Сарахман',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '64885108fdc2d1a130c24af9', name: 'Cybersecurity' },
+      category: {
+        _id: '64884f33fdc2d1a130c24ac2',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-06-22T13:19:16.296Z',
+      updatedAt: '2023-07-18T12:49:31.901Z'
+    },
+    {
+      _id: '64cc01437b1240cfa8bb57e0d',
+      price: 250,
+      proficiencyLevel: ['Intermediate'],
+      title: 'Piano teaching ',
+      description: 'Piano training is needed ',
+      languages: ['Ukrainian', 'English'],
+      authorRole: 'student',
+      author: {
+        _id: '64241ac11920ac09e56b79aef',
+        firstName: 'Khrystyna',
+        lastName: 'Pavlikovska',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '648850ddfdc2d1a130c24af3', name: 'Piano' },
+      category: {
+        _id: '64884f21fdc2d1a130c24ac0',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-08-03T19:34:27.647Z',
+      updatedAt: '2023-08-03T19:34:27.647Z'
+    },
+    {
+      _id: '64c6430055ca027e87a340bbf',
+      price: 1,
+      proficiencyLevel: ['Beginner'],
+      title: '11',
+      description: 'test tes tes tes tes test',
+      languages: ['English'],
+      authorRole: 'student',
+      author: {
+        _id: '64c39dedd4889760d0caa3cd0',
+        firstName: 'Ілля',
+        lastName: 'Наконечний',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 },
+        nativeLanguage: 'Ukrainian',
+        photo: '',
+        professionalSummary: ''
+      },
+      subject: { _id: '64885108fdc2d1a130c24af9', name: 'Cybersecurity' },
+      category: {
+        _id: '64884f33fdc2d1a130c24ac2',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-07-30T11:01:20.785Z',
+      updatedAt: '2023-07-31T14:10:14.867Z'
+    },
+    {
+      _id: '64bc01f299bbe3a53a746be3e',
+      price: 300,
+      proficiencyLevel: ['Intermediate', 'Advanced'],
+      title:
+        'I desire to get a degree of proffessional specialist of cybersecurity',
+      description:
+        'I look for senior in cybersecurity who has a flexible working hours',
+      languages: ['Ukrainian'],
+      authorRole: 'student',
+      author: {
+        _id: '64b79d41d8ee5e9ae0897fb93',
+        firstName: 'Anastasiia',
+        lastName: 'Jenkins',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '64885108fdc2d1a130c24af9', name: 'Cybersecurity' },
+      category: {
+        _id: '64884f33fdc2d1a130c24ac2',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-07-22T16:21:06.023Z',
+      updatedAt: '2023-07-22T16:21:06.023Z'
+    },
+    {
+      _id: '64bb9c779bbe93a53a746bc88',
+      price: 200,
+      proficiencyLevel: ['Beginner'],
+      title:
+        'I would like to become a famous guitar player and be the same stage with Slash',
+      description: "I'd like to be aware of everything",
+      languages: ['English', 'Ukrainian'],
+      authorRole: 'student',
+      author: {
+        _id: '64b79d41d8ee5eae90897fb93',
+        firstName: 'Anastasiia',
+        lastName: 'Poverly',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '648850c4fdc2d1a130c24aea', name: 'Guitar' },
+      category: {
+        _id: '64884f21fdc2d1a130c24ac0',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-07-22T09:08:07.413Z',
+      updatedAt: '2023-07-22T09:08:07.413Z'
+    },
+    {
+      _id: '64ac0548c137130e45017f841',
+      price: 3232,
+      proficiencyLevel: ['Beginner', 'Intermediate', 'Advanced'],
+      title: '121212',
+      description: 'wewewewewfdfdfdfdfdfdfdfdfd',
+      languages: ['English'],
+      authorRole: 'student',
+      author: {
+        _id: '64944409a3a3868915f9663f9',
+        firstName: 'Анатолій',
+        lastName: 'Сарахман',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '648850c4fdc2d1a130c24aea', name: 'Guitar' },
+      category: {
+        _id: '64884f21fdc2d1a130c24ac0',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [
+        { question: '12121', answer: '211', _id: '64ac054c137130e45017f842' }
+      ],
+      createdAt: '2023-07-10T13:19:08.934Z',
+      updatedAt: '2023-07-18T12:31:24.088Z'
+    },
+    {
+      _id: '64944a54e8740883835fdfb84',
+      price: 505,
+      proficiencyLevel: ['Professional'],
+      title: 'Cybersecurity proffesional',
+      description:
+        "Please describe any additional information about the tutoring you need, such as specific topics or areas you'd like to focus on, your preferred teaching style, or any particular goals you're hoping to achieve.1",
+      languages: ['German', 'Polish'],
+      authorRole: 'student',
+      author: {
+        _id: '64944409a3a3689155f9663f9',
+        firstName: 'Анатолій',
+        lastName: 'Сарахман',
+        totalReviews: { student: 0, tutor: 0 },
+        averageRating: { student: 0, tutor: 0 }
+      },
+      subject: { _id: '64885108fdc2d1a130c24af9', name: 'Cybersecurity' },
+      category: {
+        _id: '64884f33fdc2d1a130c24ac2',
+        appearance: { icon: 'mocked-path-to-icon', color: '#66C42C' }
+      },
+      status: 'active',
+      FAQ: [],
+      createdAt: '2023-06-22T13:19:16.296Z',
+      updatedAt: '2023-07-18T12:49:31.901Z'
+    }
+  ],
+  count: 12
+}
