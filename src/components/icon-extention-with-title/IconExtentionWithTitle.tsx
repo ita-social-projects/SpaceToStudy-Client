0528bb50b0a0e1a2a9f9c52c1becfe19ca34aa5c
Artur Bekh <102412173+ArturBekhDEV@users.noreply.github.com>
2023-08-08 15:49:45 +0300

    (SP:1) Implement Icon with extention and title component (#1017)
    * add iconWithExtentionComponent
    
    * fix typo
    
    * fix comment
    
    * fixed sonar bug
    
    * updated test

---
+++ src/components/icon-extention-with-title/IconExtentionWithTitle.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import Box from '@mui/material/Box'
+import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
+import { styles } from '~/components/icon-extention-with-title/IconExtentionWithTitle.styles'
+
+interface IconExtentionWithTitleProps {
+  title: string
+  description: number
+}
+
+const IconExtentionWithTitle: FC<IconExtentionWithTitleProps> = ({
+  title,
+  description
+}) => {
+  const { t } = useTranslation()
+  const fileExtension = title.slice(title.lastIndexOf('.') + 1)
+
+  return (
+    <Box sx={styles.container}>
+      <Box sx={styles.iconBox}>{fileExtension}</Box>
+      <TitleWithDescription
+        description={
+          description > 0 && `${description} ${t('common.megabytes')}`
+        }
+        style={styles.titleWithDescription}
+        title={title}
+      />
+    </Box>
+  )
+}
+
+export default IconExtentionWithTitle
---
+++ tests/unit/components/icon-extention-with-title/IconExtentionWithTitle.spec.jsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from '~tests/test-utils'
+import IconExtentionWithTitle from '~/components/icon-extention-with-title/IconExtentionWithTitle'
+
+describe('IconExtentionWithTitle test', () => {
+  beforeEach(() => {
+    renderWithProviders(
+      <IconExtentionWithTitle description='13' title='NameOfAttachment.doc' />
+    )
+  })
+
+  it('Should render title', () => {
+    const title = screen.getByText('NameOfAttachment.doc')
+
+    expect(title).toBeInTheDocument()
+  })
+
+  it('Should render icon text', () => {
+    const iconText = screen.getByText('doc')
+
+    expect(iconText).toBeInTheDocument()
+  })
+
+  it('Should render description', () => {
+    const description = screen.getByText('13 common.megabytes')
+
+    expect(description).toBeInTheDocument()
+  })
+})
