3e63402824ed4c56229ce5133e20fc215ed2aa97
mxrcury <demetrgonchar@gmail.com>
2023-08-09 13:12:11 +0300

    fix

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
 
-const fakeRequest = () => new Promise((r) => setTimeout(r, 5000))
+const fakeRequest = () =>
+  new Promise((r) =>
+    setTimeout(() => {
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 5; j++) {
+          if (Math.round((i + j) % 2) == 0) {
+            console.log('i: %d, j: %d', i, j)
+          }
+        }
+      }
+      r('ok')
+    }, 5000)
+  )
 const TestComponent = () => {
   const items = [1, 2, 3, 4, 5]
 
   const makeFakeRequest = () => fakeRequest()
 
   React.useEffect(() => {
-    makeFakeRequest()
+    void makeFakeRequest()
   })
 
   return (
     <React.Fragment>
       {items.forEach((item) => (
         <p>{item}</p>
-      ))
-      }
+      ))}
     </React.Fragment>
   )
 }
