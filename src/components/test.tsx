3da257dc5912e8e52292dec5f7e207ca09c2d92f
mxrcury <demetrgonchar@gmail.com>
2023-08-09 13:06:03 +0300

    some test

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const fakeRequest = () => new Promise((r) => setTimeout(r, 5000))
 const TestComponent = () => {
-  const items = [1,2,3,4,5]
+  const items = [1, 2, 3, 4, 5]
 
   const makeFakeRequest = () => fakeRequest()
 
@@ -10,9 +10,14 @@ const TestComponent = () => {
     makeFakeRequest()
   })
 
-  return (<React.Fragment>
-    {items.forEach( item => <p>{item}</p>)}
-  </React.Fragment>)
+  return (
+    <React.Fragment>
+      {items.forEach((item) => (
+        <p>{item}</p>
+      ))
+      }
+    </React.Fragment>
+  )
 }
 
 export default TestComponent
