7638b0aca72e0a858b83c16e21f533a388892a50
mxrcury <demetrgonchar@gmail.com>
2023-08-09 13:26:58 +0300

    test

---
+++ tests/unit/components/test.spec.jsx
@@ -0,0 +1,8 @@
+import TestComponet from '~/components/test'
+import { render } from '@testing-library/react'
+
+describe('TEst compone', () => {
+  it('some aciton', () => {
+    render(<TestComponet />)
+  })
+})
