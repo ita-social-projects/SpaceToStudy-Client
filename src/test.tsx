9b4ea9f502e0abc31d4ffa4b2d389723581d9268
mxrcury <demetrgonchar@gmail.com>
2023-08-09 11:19:17 +0300

    added file

---
+++ src/test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const fakeRequest = () => new Promise((r) => setTimeout(r, 5000))
+const TestComponent = () => {
+  const items = [1,2,3,4,5]
+
+  const makeFakeRequest = () => fakeRequest()
+
+  React.useEffect(() => {
+    makeFakeRequest()
+  })
+
+  return (<React.Fragment>
+    {items.forEach( item => <p>{item}</p>)}
+  </React.Fragment>)
+}
+
+export default TestComponent
