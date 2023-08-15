3d7378fc191fc5191f742620e627e8d720cb27d6
Roman Peretiatko <romansperetiatko@gmail.com>
2023-08-15 12:09:15 +0300

    Moved imports

--- src/containers/add-attachments/AddAttachments.tsx
+++ src/containers/add-attachments/AddAttachments.tsx
@@ -1,6 +1,5 @@
 import { ChangeEvent, FC, useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import SearchIcon from '@mui/icons-material/Search'
