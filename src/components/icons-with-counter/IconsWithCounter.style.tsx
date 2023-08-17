799a9c8f9d3e6c65ee5b9a1e54d9b0b2a844aa85
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-08-17 15:06:54 +0300

    fixed mistake

--- src/components/icons-with-counter/IconsWithCounter.style.tsx
+++ src/components/icons-with-counter/IconsWithCounter.style.tsx
@@ -2,7 +2,7 @@ export const styles = {
   iconBox: {
     width: 'max-content',
     display: 'flex',
-    alignItems: 'centre'
+    alignItems: 'center'
   },
   typography: {
     minWidth: 'max-content',
