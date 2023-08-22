9338acb2a7e12b790708409af45d3e86e26db47b
Anatoliy Sarakhman <anatolej1111@gmail.com>
2023-08-22 15:40:09 +0300

    fixed imports

--- src/components/file-editor/FileEditor.tsx
+++ src/components/file-editor/FileEditor.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
 import { Editor } from '@tinymce/tinymce-react'
+
 import { initialFileValue } from '~/components/file-editor/FileEditor.constants'
 
 const FileEditor = () => {
