f3980d23a6c7b321d5fdefe134c2657f9f3ed6b4
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-08-22 19:12:46 +0300

    Search by message (#1025)
    * fixed troubles
    
    * main commit
    
    * added IconsWithCounter
    
    * fixed mistakes
    
    * fixed mistakes
    
    * fixed mistake
    
    * fixed issues
    
    * return test back
    
    * fixed imports
    
    * added tests
    
    * fixed issues

---
+++ src/components/icons-with-counter/IconsWithCounter.style.ts
@@ -0,0 +1,14 @@
+export const styles = {
+  iconBox: {
+    width: 'max-content',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  typography: {
+    minWidth: 'max-content',
+    my: '10px',
+    width: '40px',
+    height: '24px',
+    padding: '3px'
+  }
+}
