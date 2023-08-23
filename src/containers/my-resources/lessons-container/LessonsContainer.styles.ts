6f8d04b2d6164b6c7b180f8963785d1fa0e11f0f
Sarakhman Anatoliy <63456632+Tolik170@users.noreply.github.com>
2023-08-23 18:52:34 +0300

    implemented search for table of quizzes (#1034)
    * implemented search for table of quizzes
    
    * refactor AddResource btn
    
    * changed test
    
    * fix

--- src/containers/my-resources/lessons-container/LessonsContainer.styles.ts
+++ src/containers/my-resources/lessons-container/LessonsContainer.styles.ts
@@ -8,11 +8,6 @@ const captionTitle = {
 
 export const styles = {
   table: roundedBorderTable,
-  topContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    mb: '24px'
-  },
   lessonTitleContainer: {
     display: 'flex',
     alignItems: 'center',
@@ -26,21 +21,5 @@ export const styles = {
     color: 'primary.900'
   },
   attachmentsTitle: captionTitle,
-  dateTitle: captionTitle,
-  addLessonBtn: {
-    py: '19px',
-    px: '40px'
-  },
-  searchIcon: { color: 'primary.700' },
-  input: {
-    flex: 1,
-    maxWidth: '285px',
-    border: '1px solid',
-    borderColor: 'primary.500',
-    borderRadius: '6px'
-  },
-  icon: {
-    width: { xs: '18px', sm: '22px' },
-    color: 'primary.100'
-  }
+  dateTitle: captionTitle
 }
