790b1786d4df0f602bdbec585485800756ca648d
Tetiana Deresh <tetiana.deresh.knm.2020@lpnu.ua>
2023-08-29 16:56:39 +0300

    Refactored code

--- tests/unit/containers/tutor-profile/profile-info/ProfileContainerMobile.spec.jsx
+++ tests/unit/containers/tutor-profile/profile-info/ProfileContainerMobile.spec.jsx
@@ -1,55 +1,59 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ProfileContainerMobile from '~/containers/tutor-profile/profile-info/ProfileContainerMobile'
 
+const mockProps = {
+  userData: {
+    firstName: 'John',
+    lastName: 'Doe',
+    professionalSummary: 'Experienced tutor',
+    photo: 'john-doe.jpg'
+  },
+  actionIcon: <button>Edit</button>,
+  accInfo: <div>Account info</div>,
+  buttonGroup: <div>Button group</div>,
+  defaultQuantity: 2,
+  doneItems: [
+    { title: 'Item 1', description: 'Description 1' },
+    { title: 'Item 2', description: 'Description 2' }
+  ],
+  chipItems: ['Subject 1', 'Subject 2']
+}
+
 describe('ProfileContainerMobile', () => {
-  const mockProps = {
-    userData: {
-      firstName: 'John',
-      lastName: 'Doe',
-      professionalSummary: 'Experienced tutor',
-      photo: 'john-doe.jpg'
-    },
-    actionIcon: <button>Edit</button>,
-    accInfo: <div>Account info</div>,
-    buttonGroup: <div>Button group</div>,
-    defaultQuantity: 2,
-    doneItems: [
-      { title: 'Item 1', description: 'Description 1' },
-      { title: 'Item 2', description: 'Description 2' }
-    ],
-    chipItems: ['Subject 1', 'Subject 2']
-  }
+  beforeEach(() => {
+    render(<ProfileContainerMobile {...mockProps} />)
+  })
 
   it('renders user information', () => {
-    const { getByText } = render(<ProfileContainerMobile {...mockProps} />)
+    expect(screen.getByText('John Doe')).toBeInTheDocument()
 
-    expect(getByText('John Doe')).toBeInTheDocument()
-    expect(getByText('Experienced tutor')).toBeInTheDocument()
+    expect(screen.getByText('Experienced tutor')).toBeInTheDocument()
   })
 
   it('renders action icon', () => {
-    const { getByText } = render(<ProfileContainerMobile {...mockProps} />)
-    expect(getByText('Edit')).toBeInTheDocument()
+    expect(screen.getByText('Edit')).toBeInTheDocument()
   })
 
   it('renders chip items', () => {
-    const { getByText } = render(<ProfileContainerMobile {...mockProps} />)
-    expect(getByText('Subject 1')).toBeInTheDocument()
-    expect(getByText('Subject 2')).toBeInTheDocument()
+    expect(screen.getByText('Subject 1')).toBeInTheDocument()
+
+    expect(screen.getByText('Subject 2')).toBeInTheDocument()
   })
 
   it('renders done items', () => {
-    const { getByText } = render(<ProfileContainerMobile {...mockProps} />)
-    expect(getByText('Item 1')).toBeInTheDocument()
-    expect(getByText('Description 1')).toBeInTheDocument()
-    expect(getByText('Item 2')).toBeInTheDocument()
-    expect(getByText('Description 2')).toBeInTheDocument()
+    expect(screen.getByText('Item 1')).toBeInTheDocument()
+
+    expect(screen.getByText('Description 1')).toBeInTheDocument()
+
+    expect(screen.getByText('Item 2')).toBeInTheDocument()
+
+    expect(screen.getByText('Description 2')).toBeInTheDocument()
   })
 
   it('renders account info and button group', () => {
-    const { getByText } = render(<ProfileContainerMobile {...mockProps} />)
-    expect(getByText('Account info')).toBeInTheDocument()
-    expect(getByText('Button group')).toBeInTheDocument()
+    expect(screen.getByText('Account info')).toBeInTheDocument()
+
+    expect(screen.getByText('Button group')).toBeInTheDocument()
   })
 })
