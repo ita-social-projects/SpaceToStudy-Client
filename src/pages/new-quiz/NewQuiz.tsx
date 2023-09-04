4ab9fed46bd19a9e9d72aee8ec8b5cd866cc1cdd
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-09-04 23:47:34 +0300

    impelement new-quiz rote

---
+++ src/pages/new-quiz/NewQuiz.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from '~/components/page-wrapper/PageWrapper'
+
+const NewQuiz = () => {
+  return (
+    <PageWrapper>
+      <h3>New Quiz</h3>
+    </PageWrapper>
+  )
+}
+
+export default NewQuiz
