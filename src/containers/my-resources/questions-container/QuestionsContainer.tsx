de9114a6ad1d43fd2c74b7c3ce1199591a1b39a9
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-09-04 23:57:37 +0300

    Question page (#1084)
    * fixed troubles
    
    * added questions page
    
    * fixed mistakes n added test

---
+++ src/containers/my-resources/questions-container/QuestionsContainer.tsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box'
+
+const QuestionsContainer = () => {
+  return (
+    <Box>
+      <h3>Questions</h3>
+    </Box>
+  )
+}
+
+export default QuestionsContainer
--- src/pages/my-resources/MyResources.constants.tsx
+++ src/pages/my-resources/MyResources.constants.tsx
@@ -3,10 +3,12 @@ import { ReactElement } from 'react'
 import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined'
 import AttachFileIcon from '@mui/icons-material/AttachFile'
 import NoteAltOutlinedIcon from '@mui/icons-material/NoteAltOutlined'
+import QuizOutlinedIcon from '@mui/icons-material/QuizOutlined'
 
 import AttachmentsContainer from '~/containers/my-resources/attachments-container/AttachmentsContainer'
 import QuizzesContainer from '~/containers/my-quizzes/QuizzesContainer'
 import LessonsContainer from '~/containers/my-resources/lessons-container/LessonsContainer'
+import QuestionsContainer from '~/containers/my-resources/questions-container/QuestionsContainer'
 
 interface TabsData {
   [key: string]: {
@@ -25,6 +27,11 @@ export const tabsData: TabsData = {
     title: 'myResourcesPage.tabs.quizzes',
     content: <QuizzesContainer />,
     icon: <NoteAltOutlinedIcon />
+  },
+  questions: {
+    title: 'myResourcesPage.tabs.questions',
+    content: <QuestionsContainer />,
+    icon: <QuizOutlinedIcon />
   },
   attachments: {
     title: 'myResourcesPage.tabs.attachments',
