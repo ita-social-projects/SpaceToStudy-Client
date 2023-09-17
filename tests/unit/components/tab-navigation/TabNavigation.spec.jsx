3c48b8d8c64d570972cea0401c6ffb55d4ab3761
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-09-17 19:21:52 +0300

    removed redundant import

--- tests/unit/components/tab-navigation/TabNavigation.spec.jsx
+++ tests/unit/components/tab-navigation/TabNavigation.spec.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect' // For expect(...).toBeInTheDocument()
 import TabNavigation from '~/components/tab-navigation/TabNavigation'
 
 describe('TabNavigation', () => {
