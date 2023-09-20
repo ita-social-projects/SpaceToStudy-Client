afb891f876bfaafb51ddb3dd303db3c02386bc5d
Dmytro Honchar <90138904+mxrcury@users.noreply.github.com>
2023-09-20 20:17:30 +0300

    Implemented question component (#1129)
    * Inplemented uploading files
    
    * Small fix
    
    * Added type
    
    * Move uploadin to modal window
    
    * Small fix
    
    * Fixed buttons
    
    * Fixed buttons
    
    * Fixed comments
    
    * Fixed comment
    
    * Added sorting to quizzes tab (#1043)
    
    * added sorting to quizzes tab
    
    * removed redundant destructured variable
    
    * renamed variable
    
    * fixed code smell
    
    * Add sort to attachments on myResources (#1059)
    
    * fix conflicts
    
    * fix conflicts 3
    
    * init
    
    * finished
    
    * fix
    
    * fix
    
    * fix
    
    * fixed conflicts
    
    * added question component
    
    * fix eslint
    
    * fix
    
    * fix eslint
    
    * import fix
    
    * small fixes, added unit test
    
    * fix
    
    * fixed styles
    
    ---------
    
    Co-authored-by: Yura Didenko <didenkoyura225@gmail.com>
    Co-authored-by: Taras Chaika <45912519+TSlashDreamy@users.noreply.github.com>
    Co-authored-by: Artur Bekh <102412173+ArturBekhDEV@users.noreply.github.com>

--- src/types/questions/interfaces/questions.interface.ts
+++ src/types/questions/interfaces/questions.interface.ts
@@ -1,6 +1,6 @@
 import { CommonEntityFields, UserResponse } from '~/types'
 export interface Answer {
-  id: number
+  id: string
   text: string
   isCorrect: boolean
 }
@@ -9,4 +9,8 @@ export interface Question extends CommonEntityFields {
   title: string
   items: Omit<Answer, 'id'>[]
   author: Pick<UserResponse, '_id'>
+}
+export interface QuestionCategory {
+  name: string
+  _id: string
 }
