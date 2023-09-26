262f0aaa44117127508024ca8c015173ff500daa
Sarakhman Anatoliy <63456632+Tolik170@users.noreply.github.com>
2023-09-26 12:35:32 +0300

    imlemented emoji picker for chat (#1143)
    * imlemented emoji picker for chat
    
    * added coverage

--- src/containers/chat/chat-text-area/ChatTextArea.tsx
+++ src/containers/chat/chat-text-area/ChatTextArea.tsx
@@ -1,10 +1,22 @@
-import { FC, ChangeEvent } from 'react'
+import {
+  FC,
+  ChangeEvent,
+  useState,
+  useRef,
+  Dispatch,
+  SetStateAction
+} from 'react'
+import Picker from '@emoji-mart/react'
+import data from '@emoji-mart/data'
+import { SxProps } from '@mui/material'
 import { TextFieldProps } from '@mui/material/TextField'
 import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
 import SendIcon from '@mui/icons-material/Send'
-import { SxProps } from '@mui/material'
+import MoodIcon from '@mui/icons-material/Mood'
+import AttachFileIcon from '@mui/icons-material/AttachFile'
 
+import useBreakpoints from '~/hooks/use-breakpoints'
 import AppTextArea from '~/components/app-text-area/AppTextArea'
 
 import { styles } from '~/containers/chat/chat-text-area/ChatTextArea.styles'
@@ -13,43 +25,105 @@ import { spliceSx } from '~/utils/helper-functions'
 
 interface ChatTextAreaProps extends Omit<TextFieldProps, 'onChange' | 'sx'> {
   value: string
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  setValue: Dispatch<SetStateAction<string>>
   onClick: () => void
   sx?: {
     textAreaWrapper?: SxProps
     container?: SxProps
     icon?: SxProps
   }
+  emojiPickerProps?: { perLine: number }
 }
 
 const ChatTextArea: FC<ChatTextAreaProps> = ({
   value,
-  onChange,
+  setValue,
   onClick,
   maxRows = 6,
   minRows = 1,
   sx = {},
+  emojiPickerProps,
   ...props
 }) => {
+  const { isMobile } = useBreakpoints()
+  const [isEmojiPickerOpen, setIsEmojiPickerOpen] = useState<boolean>(false)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value)
+
+  const onEmojiSelect = ({ native: emoji }: { native: string }) => {
+    if (inputRef.current) {
+      const input = inputRef.current
+      const start = input.selectionStart ?? 0
+
+      setValue((prev) => {
+        const updatedValue = prev.split('')
+        updatedValue.splice(start, 0, emoji)
+        return updatedValue.join('')
+      })
+
+      const newCursorPosition = start + emoji.length
+
+      input.setSelectionRange(newCursorPosition, newCursorPosition)
+      input.focus()
+    }
+  }
+
+  const onOpenPicker = () => setIsEmojiPickerOpen(true)
+  const onClosePicker = () => setIsEmojiPickerOpen(false)
+
+  const endAdornment = (
+    <>
+      <IconButton onMouseEnter={onOpenPicker}>
+        <MoodIcon />
+      </IconButton>
+      <IconButton disabled>
+        <AttachFileIcon />
+      </IconButton>
+    </>
+  )
+
   return (
     <Box sx={spliceSx(styles.container, sx.container)}>
-      <AppTextArea
-        InputLabelProps={{
-          style: styles.textAreaLabel(value),
-          shrink: false
-        }}
-        InputProps={{ disableUnderline: true }}
-        fullWidth
-        maxRows={maxRows}
-        minRows={minRows}
-        onChange={onChange}
-        sx={spliceSx(styles.textAreaWrapper, sx.textAreaWrapper)}
-        textFieldStyles={styles.textArea}
-        value={value}
-        variant={TextFieldVariantEnum.Standard}
-        withHelperText={false}
-        {...props}
-      />
+      <Box sx={styles.testAreaWithPicker}>
+        {isEmojiPickerOpen && (
+          <Box data-testid='emoji-picker' sx={styles.emojiPicker}>
+            <Picker
+              data={data}
+              emojiSize={20}
+              maxFrequentRows={2}
+              onClickOutside={onClosePicker}
+              onEmojiSelect={onEmojiSelect}
+              perLine={isMobile ? 6 : 8}
+              previewPosition='none'
+              searchPosition='none'
+              skinTonePosition='none'
+              {...emojiPickerProps}
+            />
+          </Box>
+        )}
+
+        <AppTextArea
+          InputLabelProps={styles.textAreaLabel(value)}
+          InputProps={{
+            disableUnderline: true,
+            endAdornment: endAdornment
+          }}
+          fullWidth
+          inputRef={inputRef}
+          maxRows={maxRows}
+          minRows={minRows}
+          onChange={onChange}
+          sx={spliceSx(styles.textAreaWrapper, sx.textAreaWrapper)}
+          textFieldStyles={styles.textArea}
+          value={value}
+          variant={TextFieldVariantEnum.Standard}
+          withHelperText={false}
+          {...props}
+        />
+      </Box>
+
       <IconButton data-testid='send-btn' onClick={onClick}>
         <SendIcon sx={spliceSx(styles.icon, sx.icon)} />
       </IconButton>
---
+++ tests/unit/pages/chat/chat-text-area/ChatTextArea.spec.jsx
@@ -0,0 +1,85 @@
+import { fireEvent, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import useBreakpoints from '~/hooks/use-breakpoints'
+import ChatTextArea from '~/containers/chat/chat-text-area/ChatTextArea'
+
+import { renderWithProviders } from '~tests/test-utils'
+
+vi.mock('~/hooks/use-breakpoints')
+
+global.window.matchMedia = vi.fn().mockImplementation((query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: vi.fn(),
+  removeListener: vi.fn()
+}))
+
+global.IntersectionObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn()
+}))
+
+const mockRef = {
+  current: { focus: vi.fn(), setSelectionRange: vi.fn(), selectionStart: 5 }
+}
+const mockSetValue = vi.fn()
+const mockOnClick = vi.fn()
+
+vi.mock('react', async () => {
+  const actual = await vi.importActual('react')
+  return {
+    ...actual,
+    useRef: () => vi.fn().mockReturnValue(mockRef)
+  }
+})
+
+const props = {
+  value: 'new message',
+  label: 'Enter some text',
+  setValue: mockSetValue,
+  onClick: mockOnClick
+}
+
+describe('ChatTextArea component test', () => {
+  const breakpointsData = { isMobile: false }
+  beforeEach(() => {
+    useBreakpoints.mockImplementation(() => breakpointsData)
+
+    renderWithProviders(<ChatTextArea {...props} />)
+  })
+
+  it('should render input with emoji icon', async () => {
+    const input = screen.getByText('Enter some text')
+    const emojiIcon = screen.getByTestId('MoodIcon')
+
+    expect(input).toBeInTheDocument()
+    expect(emojiIcon).toBeInTheDocument()
+  })
+
+  it('should open emoji picker', async () => {
+    const emojiIcon = screen.getByTestId('MoodIcon')
+
+    fireEvent.mouseEnter(emojiIcon)
+
+    const picker = screen.getByTestId('emoji-picker')
+
+    expect(picker).toBeInTheDocument()
+  })
+
+  it('should send new message and clear input', async () => {
+    const input = screen.getByLabelText('Enter some text')
+
+    userEvent.type(input, 'new message')
+
+    expect(input.value).toBe('new message')
+
+    const sendBtn = screen.getByTestId('SendIcon')
+
+    fireEvent.click(sendBtn)
+
+    expect(mockOnClick).toHaveBeenCalled()
+  })
+})
