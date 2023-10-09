115ef96084478cf84d45102426bb6199e40fbdeb
dmtrth25 <vodolazhchenko25@gmail.com>
2023-10-09 15:21:10 +0300

    added tests for CreateOrEditLesson

--- tests/unit/pages/create-or-edit-lesson/CreateOrEditLesson.spec.jsx
+++ tests/unit/pages/create-or-edit-lesson/CreateOrEditLesson.spec.jsx
@@ -1,10 +1,17 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithProviders } from '~tests/test-utils'
 
 import CreateOrEditLesson from '~/pages/create-or-edit-lesson/CreateOrEditLesson'
 
+const mockFetchData = vi.fn()
+
 describe('CreateOrEditLesson', () => {
   beforeEach(() => {
+    vi.mock('~/services/resource-service', () => ({
+      ResourceService: {
+        addLesson: () => mockFetchData()
+      }
+    }))
     renderWithProviders(<CreateOrEditLesson />)
   })
 
@@ -14,5 +21,20 @@ describe('CreateOrEditLesson', () => {
 
     expect(titleInput).toBeInTheDocument()
     expect(descriptionInput).toBeInTheDocument()
+  })
+
+  it('should add attachments', () => {
+    const addedAttachment = screen.getByText('lesson.labels.attachments')
+    expect(addedAttachment).toBeInTheDocument()
+
+    fireEvent.click(screen.getByText('lesson.labels.attachments'))
+  })
+
+  it('display validation error if title or description is empty', () => {
+    const titleInput = screen.getByLabelText('lesson.labels.title')
+    fireEvent.change(titleInput, { target: { value: '' } })
+
+    const descriptionInput = screen.getByLabelText('lesson.labels.description')
+    fireEvent.change(descriptionInput, { target: { value: '' } })
   })
 })
