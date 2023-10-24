4b7e9f5e54b3fa118387dd313a5cb4fbcbba28e4
dmtrth25 <vodolazhchenko25@gmail.com>
2023-10-24 10:42:13 +0300

    added buttons for new page

--- src/pages/create-course/CreateCourse.tsx
+++ src/pages/create-course/CreateCourse.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+
+import Box from '@mui/material/Box'
+
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import AppButton from '~/components/app-button/AppButton'
+
+import { authRoutes } from '~/router/constants/authRoutes'
+import { ButtonTypeEnum, ButtonVariantEnum } from '~/types'
+
+import { styles } from '~/pages/create-course/CreateCourse.styles'
 
 const CreateCourse = () => {
+  const { t } = useTranslation()
+  const navigate = useNavigate()
   return (
-    <div>
-      <PageWrapper>
-        <div>New course</div>
-      </PageWrapper>
-    </div>
+    <PageWrapper>
+      <Box sx={styles.buttons}>
+        <AppButton
+          onClick={() => navigate(authRoutes.myCourses.root.path)}
+          variant={ButtonVariantEnum.Tonal}
+        >
+          {t('common.cancel')}
+        </AppButton>
+        <AppButton disabled type={ButtonTypeEnum.Submit}>
+          {t('common.save')}
+        </AppButton>
+      </Box>
+    </PageWrapper>
   )
 }
 
--- tests/unit/pages/create-course/CreateCourse.spec.jsx
+++ tests/unit/pages/create-course/CreateCourse.spec.jsx
@@ -1,14 +1,33 @@
-import CreateCourse from '~/pages/create-course/CreateCourse'
 import { renderWithProviders } from '~tests/test-utils'
-import { screen } from '@testing-library/react'
+import { screen, fireEvent } from '@testing-library/react'
+
+import CreateCourse from '~/pages/create-course/CreateCourse'
+
+const mockedNavigate = vi.fn()
+
+vi.mock('react-router-dom', async () => ({
+  ...(await vi.importActual('react-router-dom')),
+  useNavigate: () => mockedNavigate
+}))
 
 describe('CreateCourse', () => {
   beforeEach(() => {
     renderWithProviders(<CreateCourse />)
   })
 
-  it('should render page and find "New course" title', () => {
-    const newCourseBreadcrumb = screen.getByText('New course')
-    expect(newCourseBreadcrumb).toBeInTheDocument()
+  it('should render cancel and save buttons', () => {
+    const cancelButton = screen.getByText('common.cancel')
+    expect(cancelButton).toBeInTheDocument()
+
+    const saveButton = screen.getByText('common.save')
+    expect(saveButton).toBeInTheDocument()
+  })
+
+  it('redirect by clicking cancel button', () => {
+    const cancelButton = screen.getByText('common.cancel')
+
+    fireEvent.click(cancelButton)
+
+    expect(mockedNavigate).toHaveBeenCalled()
   })
 })
