a361200274baae6bdff4f8f6b4eb5c32d2c9a6f7
Anatoliy Sarakhman <anatolej1111@gmail.com>
2023-11-07 17:31:42 +0200

    fetch resources categories and add create new category modal for new question page

--- tests/unit/pages/create-or-edit-question/CreateOrEditQuestion.spec.jsx
+++ tests/unit/pages/create-or-edit-question/CreateOrEditQuestion.spec.jsx
@@ -1,21 +1,43 @@
-import { screen } from '@testing-library/react'
-import { renderWithProviders } from '~tests/test-utils'
 import { describe } from 'vitest'
+import { screen, fireEvent } from '@testing-library/react'
+
+import { renderWithProviders, mockAxiosClient } from '~tests/test-utils'
 import CreateOrEditQuestion from '~/pages/create-or-edit-question/CreateOrEditQuestion'
+import { URLs } from '~/constants/request'
 
-const mockFetchData = vi.fn()
-
-vi.mock('~/services/resource-service', () => ({
-  ResourseService: {
-    createQuestion: () => mockFetchData()
-  }
-}))
+const categoriesNamesMock = [
+  { _id: '650c27618a9fbf234b8bb4cf', name: 'New category in resources!' },
+  { _id: '650c27618a9fbf234b8bb4cd', name: 'Category 1' }
+]
 
 describe('CreateOrEditQuestion component test', () => {
-  it('should display CreateQuestion form', () => {
-    renderWithProviders(<CreateOrEditQuestion />)
+  mockAxiosClient
+    .onGet(URLs.resources.resourcesCategories.getNames)
+    .reply(200, categoriesNamesMock)
 
+  beforeEach(() => {
+    renderWithProviders(<CreateOrEditQuestion />)
+  })
+
+  it('should display CreateQuestion form', () => {
     const title = screen.getByLabelText('questionPage.untitled')
+
     expect(title).toBeInTheDocument()
+  })
+
+  it('should choose category', () => {
+    const autocomplete = screen.getByRole('combobox')
+
+    expect(autocomplete).toBeInTheDocument()
+    expect(autocomplete.value).toBe('')
+
+    fireEvent.click(autocomplete)
+    fireEvent.change(autocomplete, {
+      target: { value: categoriesNamesMock[1].name }
+    })
+    fireEvent.keyDown(autocomplete, { key: 'ArrowDown' })
+    fireEvent.keyDown(autocomplete, { key: 'Enter' })
+
+    expect(autocomplete.value).toBe(categoriesNamesMock[1].name)
   })
 })
