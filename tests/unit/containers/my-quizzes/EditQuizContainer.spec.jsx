f6fd097b5be157549b0ded056e36bf36053fd354
Vitalii <bossi@ukr.net>
2023-11-11 21:12:35 +0200

    fixed_comments_and_added_test

---
+++ tests/unit/containers/my-quizzes/EditQuizContainer.spec.jsx
@@ -0,0 +1,70 @@
+import { vi } from 'vitest'
+import EditQuizContainer from '~/containers/my-quizzes/edit-quiz-container/EditQuizContainer'
+import { fireEvent, screen } from '@testing-library/react'
+import { renderWithProviders } from '~tests/test-utils'
+
+vi.mock('~/hooks/use-categories-names', () => ({
+  __esModule: true,
+  default: () => ({
+    loading: false,
+    response: [
+      { _id: '123', name: 'Category 1' },
+      { _id: '456', name: 'Category 2' },
+      { _id: '789', name: '' }
+    ]
+  })
+}))
+
+const route = '/categories/subjects?categoryId=123'
+
+const mockState = {
+  appMain: { userRole: 'tutor' }
+}
+
+describe('EditQuizContainer', () => {
+  beforeEach(() => {
+    renderWithProviders(
+      <EditQuizContainer questions={[]} setQuestions={() => {}} />,
+      {
+        initialEntries: route,
+        preloadedState: mockState
+      }
+    )
+  })
+
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should render correctly', () => {
+    expect(
+      screen.getByLabelText('myResourcesPage.quizzes.categoryDropdown')
+    ).toBeInTheDocument()
+  })
+
+  it('should change autocomplete', () => {
+    const autocomplete = screen.getByLabelText(
+      'myResourcesPage.quizzes.categoryDropdown'
+    )
+
+    fireEvent.click(autocomplete)
+    fireEvent.change(autocomplete, { target: { value: 'Category 2' } })
+    fireEvent.keyDown(autocomplete, { key: 'ArrowDown' })
+    fireEvent.keyDown(autocomplete, { key: 'Enter' })
+
+    expect(autocomplete.value).toBe('Category 2')
+  })
+
+  it('should clear autocomplete', () => {
+    const autocomplete = screen.getByLabelText(
+      'myResourcesPage.quizzes.categoryDropdown'
+    )
+
+    fireEvent.click(autocomplete)
+    fireEvent.change(autocomplete, { target: { value: '' } })
+    fireEvent.keyDown(autocomplete, { key: 'ArrowDown' })
+    fireEvent.keyDown(autocomplete, { key: 'Enter' })
+
+    expect(autocomplete.value).toBe('')
+  })
+})
