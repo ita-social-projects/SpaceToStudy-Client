f1604b26a3e6a6f0e65d290da8d861093e78bb56
Sarakhman Anatoliy <63456632+Tolik170@users.noreply.github.com>
2023-11-15 16:18:11 +0200

    fetched resource categories and add create new category modal for new quiz page (#1374)
    * fetch resources categories and add create new category modal for new question page
    
    * fixed test
    
    * added coverage

--- src/pages/create-or-edit-question/CreateOrEditQuestion.tsx
+++ src/pages/create-or-edit-question/CreateOrEditQuestion.tsx
@@ -1,8 +1,6 @@
-import { HTMLAttributes, useCallback } from 'react'
+import { HTMLAttributes, SyntheticEvent, useCallback, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-import Autocomplete from '@mui/material/Autocomplete'
-import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
@@ -12,18 +10,24 @@ import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import QuestionEditor from '~/components/question-editor/QuestionEditor'
 import AppButton from '~/components/app-button/AppButton'
 import AppTextField from '~/components/app-text-field/AppTextField'
+import AddCategoriesModal from '~/containers/my-resources/add-categories-modal/AddCategoriesModal'
+import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete'
 import useForm from '~/hooks/use-form'
 import useAxios from '~/hooks/use-axios'
+import { ResourceService } from '~/services/resource-service'
+import { useModalContext } from '~/context/modal-context'
 import { useSnackBarContext } from '~/context/snackbar-context'
 
-import { ResourceService } from '~/services/resource-service'
 import { authRoutes } from '~/router/constants/authRoutes'
 import { defaultResponses, snackbarVariants } from '~/constants'
 import {
   ButtonTypeEnum,
   ButtonVariantEnum,
+  Categories,
+  CategoryNameInterface,
   ComponentEnum,
   ErrorResponse,
+  CreateCategoriesParams,
   QuestionForm,
   SizeEnum,
   TextFieldVariantEnum,
@@ -39,13 +43,33 @@ const CreateOrEditQuestion = () => {
   const { t } = useTranslation()
   const navigate = useNavigate()
   const { setAlert } = useSnackBarContext()
+  const { openModal, closeModal } = useModalContext()
+  const [isFetched, setIsFetched] = useState<boolean>(false)
 
   const createQuestion = useCallback(
     (data?: QuestionForm) => ResourceService.createQuestion(data),
     []
   )
 
-  const handleResponse = () => {
+  const createCategory = useCallback(
+    (params?: CreateCategoriesParams) =>
+      ResourceService.createResourceCategory(params),
+    []
+  )
+
+  const getCategories = useCallback(() => {
+    setIsFetched(true)
+    return ResourceService.getResourcesCategoriesNames()
+  }, [])
+
+  const onCategoryChange = (
+    _: SyntheticEvent,
+    value: CategoryNameInterface | null
+  ) => {
+    handleNonInputValueChange('category', value?._id ?? null)
+  }
+
+  const onResponse = () => {
     setAlert({
       severity: snackbarVariants.success,
       message: 'categoriesPage.newSubject.successMessage'
@@ -53,7 +77,23 @@ const CreateOrEditQuestion = () => {
     navigate(authRoutes.myResources.root.path)
   }
 
-  const handleResponseError = (error: ErrorResponse) => {
+  const onResponseCategory = useCallback(
+    (response: Categories | null) => {
+      const categoryName = response ? response.name : ''
+
+      setAlert({
+        severity: snackbarVariants.success,
+        message: t('myResourcesPage.categories.successCreation', {
+          category: categoryName
+        })
+      })
+
+      setIsFetched(false)
+    },
+    [setAlert, t]
+  )
+
+  const onResponseError = (error: ErrorResponse) => {
     setAlert({
       severity: snackbarVariants.error,
       message: error ? `errors.${error.message}` : ''
@@ -64,8 +104,16 @@ const CreateOrEditQuestion = () => {
     service: createQuestion,
     defaultResponse: defaultResponses.object,
     fetchOnMount: false,
-    onResponse: handleResponse,
-    onResponseError: handleResponseError
+    onResponse,
+    onResponseError
+  })
+
+  const { fetchData: handleCreateCategory } = useAxios({
+    service: createCategory,
+    defaultResponse: null,
+    fetchOnMount: false,
+    onResponse: onResponseCategory,
+    onResponseError
   })
 
   const { data, handleInputChange, handleNonInputValueChange, handleSubmit } =
@@ -83,8 +131,19 @@ const CreateOrEditQuestion = () => {
       }
     })
 
-  const { type, title, text, answers, openAnswer } = data
+  const { type, title, text, answers, openAnswer, category } = data
   const { isOpenAnswer } = questionType(type)
+
+  const onCreateCategory = () => {
+    openModal({
+      component: (
+        <AddCategoriesModal
+          closeModal={closeModal}
+          createCategories={handleCreateCategory}
+        />
+      )
+    })
+  }
 
   const isButtonsVisible = isOpenAnswer
     ? Boolean(title && text && openAnswer)
@@ -113,12 +172,13 @@ const CreateOrEditQuestion = () => {
     option: string,
     index: number
   ) => (
-    <Box>
+    <Box key={index}>
       {index === 0 && (
         <Box>
           <AppButton
             disableRipple
             fullWidth
+            onClick={onCreateCategory}
             size={SizeEnum.Medium}
             sx={styles.addButton}
             variant={ButtonVariantEnum.Text}
@@ -153,13 +213,17 @@ const CreateOrEditQuestion = () => {
           <Typography variant={TypographyVariantEnum.Body2}>
             {t('questionPage.chooseCategory')}
           </Typography>
-          <Autocomplete
-            disablePortal
-            options={['English', 'Music']}
-            renderInput={(params) => <TextField {...params} label='Category' />}
+          <AsyncAutocomplete<CategoryNameInterface>
+            fetchCondition={!isFetched}
+            fetchOnFocus
+            labelField='name'
+            onChange={onCategoryChange}
             renderOption={(props, option, state) =>
-              optionsList(props, option, state.index)
+              optionsList(props, option.name, state.index)
             }
+            service={getCategories}
+            value={category}
+            valueField='_id'
           />
         </Box>
         <Divider sx={styles.mainDivider} />
--- tests/unit/pages/create-or-edit-question/CreateOrEditQuestion.spec.jsx
+++ tests/unit/pages/create-or-edit-question/CreateOrEditQuestion.spec.jsx
@@ -1,21 +1,74 @@
-import { screen } from '@testing-library/react'
-import { renderWithProviders } from '~tests/test-utils'
 import { describe } from 'vitest'
+import { screen, fireEvent, waitFor } from '@testing-library/react'
+
+import { renderWithProviders, mockAxiosClient } from '~tests/test-utils'
 import CreateOrEditQuestion from '~/pages/create-or-edit-question/CreateOrEditQuestion'
+import { URLs } from '~/constants/request'
 
-const mockFetchData = vi.fn()
-
-vi.mock('~/services/resource-service', () => ({
-  ResourseService: {
-    createQuestion: () => mockFetchData()
-  }
-}))
+const categoriesNamesMock = [
+  { _id: '650c27618a9fbf234b8bb4cf', name: 'New category in resources!' },
+  { _id: '650c27618a9fbf234b8bb4cd', name: 'Category 1' }
+]
 
 describe('CreateOrEditQuestion component test', () => {
-  it('should display CreateQuestion form', () => {
-    renderWithProviders(<CreateOrEditQuestion />)
+  mockAxiosClient
+    .onGet(URLs.resources.resourcesCategories.getNames)
+    .reply(200, categoriesNamesMock)
 
+  beforeEach(() => {
+    renderWithProviders(<CreateOrEditQuestion />)
+  })
+
+  it('should display CreateQuestion form', () => {
     const title = screen.getByLabelText('questionPage.untitled')
+
     expect(title).toBeInTheDocument()
+  })
+
+  it('should choose the category from options list', async () => {
+    const autocomplete = screen.getByRole('combobox')
+
+    expect(autocomplete).toBeInTheDocument()
+    expect(autocomplete.value).toBe('')
+
+    fireEvent.click(autocomplete)
+    fireEvent.focus(autocomplete)
+
+    fireEvent.change(autocomplete, {
+      target: { value: categoriesNamesMock[1].name }
+    })
+
+    fireEvent.keyDown(autocomplete, { key: 'ArrowDown' })
+    fireEvent.keyDown(autocomplete, { key: 'Enter' })
+
+    await waitFor(() => {
+      expect(autocomplete.value).toBe(categoriesNamesMock[1].name)
+    })
+
+    fireEvent.keyDown(autocomplete, { key: 'ArrowDown' })
+    fireEvent.keyDown(autocomplete, { key: 'Enter' })
+
+    expect(autocomplete.value).toBe(categoriesNamesMock[1].name)
+  })
+
+  it('should click on "add button" in options list', async () => {
+    const autocomplete = screen.getByRole('combobox')
+
+    fireEvent.click(autocomplete)
+    fireEvent.focus(autocomplete)
+
+    fireEvent.keyDown(autocomplete, { key: 'ArrowDown' })
+
+    await waitFor(() => {
+      const addButton = screen.queryByText('myResourcesPage.categories.addBtn')
+
+      fireEvent.click(addButton)
+    })
+
+    await waitFor(() => {
+      const newCategory = screen.getByText('myResourcesPage.categories.name')
+
+      expect(newCategory).toBeInTheDocument()
+    })
   })
 })
