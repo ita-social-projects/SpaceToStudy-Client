c32c67e80f00e83bd5713985aa6f2e7a282b9a0c
Anatoliy Sarakhman <anatolej1111@gmail.com>
2023-11-16 18:11:31 +0200

    implemented navBar find offers modal for tutor

--- src/containers/layout/navbar/NavBar.tsx
+++ src/containers/layout/navbar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { Fragment, useMemo } from 'react'
-import { matchPath, useLocation } from 'react-router-dom'
+import { matchPath, useLocation, Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
 import Typography from '@mui/material/Typography'
@@ -7,16 +7,22 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
+import MenuItem from '@mui/material/MenuItem'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 import HashLink from '~/components/hash-link/HashLink'
 import Logo from '~/containers/logo/Logo'
 import Sidebar from '~/containers/layout/sidebar/Sidebar'
 import NavigationIcons from '~/containers/navigation-icons/NavigationIcons'
 import AppDrawer from '~/components/app-drawer/AppDrawer'
+import useMenu from '~/hooks/use-menu'
 import { useDrawer } from '~/hooks/use-drawer'
 import { useAppSelector } from '~/hooks/use-redux'
 import { guestRoutes } from '~/router/constants/guestRoutes'
-import { tutorRoutes } from '~/router/constants/tutorRoutes'
+import {
+  tutorRoutes,
+  findOffersChildRoutes
+} from '~/router/constants/tutorRoutes'
 import { studentRoutes } from '~/router/constants/studentRoutes'
 import { authRoutes } from '~/router/constants/authRoutes'
 
@@ -26,12 +32,17 @@ import { styles } from '~/containers/layout/navbar/NavBar.styles'
 const Navbar = () => {
   const { userRole } = useAppSelector((state) => state.appMain)
   const { openDrawer, closeDrawer, isOpen } = useDrawer()
+  const { openMenu, renderMenu, closeMenu, anchorEl } = useMenu()
   const { pathname } = useLocation()
   const { t } = useTranslation()
 
   const homePath = userRole
     ? guestRoutes[userRole].path
     : guestRoutes.welcome.path
+
+  const isChildRouteActive = findOffersChildRoutes.some((childRoute) =>
+    Boolean(matchPath(childRoute.path, pathname))
+  )
 
   const navigationItems = useMemo(() => {
     if (userRole === UserRoleEnum.Student) {
@@ -51,21 +62,42 @@ const Navbar = () => {
     openDrawer()
   }
 
+  const findOffersMenu = findOffersChildRoutes.map((childRoute) => (
+    <MenuItem
+      component={Link}
+      key={childRoute.route}
+      onClick={closeMenu}
+      sx={styles.findOfferMenuItem}
+      to={childRoute.path}
+    >
+      {t(`header.${childRoute.route}`)}
+    </MenuItem>
+  ))
+
   const navigationList = navigationItems.map((item, idx, array) => {
     const isLast = array.length - 1 === idx
     const isActive = Boolean(matchPath(item.path, pathname))
 
     return (
       <Fragment key={item.route}>
-        <ListItem>
-          <Typography
-            component={HashLink}
-            sx={styles.navItemText(isActive)}
-            to={item.path}
-          >
-            {t(`header.${item.route}`)}
-          </Typography>
-        </ListItem>
+        {item.route === 'findOffers' ? (
+          <ListItem onClick={openMenu} sx={styles.listItem}>
+            <Typography sx={styles.navItemText(isChildRouteActive)}>
+              {t(`header.${item.route}`)}
+            </Typography>
+            <KeyboardArrowDownIcon sx={styles.arrowIcon(Boolean(anchorEl))} />
+          </ListItem>
+        ) : (
+          <ListItem>
+            <Typography
+              component={HashLink}
+              sx={styles.navItemText(isActive)}
+              to={item.path}
+            >
+              {t(`header.${item.route}`)}
+            </Typography>
+          </ListItem>
+        )}
         {!isLast && <Typography sx={styles.divider}>{'/'}</Typography>}
       </Fragment>
     )
@@ -81,6 +113,7 @@ const Navbar = () => {
       >
         <Logo />
       </Button>
+      {renderMenu(findOffersMenu, { autoFocus: false })}
       <List sx={styles.navList}>{navigationList}</List>
       <NavigationIcons setSidebarOpen={handleOpenSidebar} />
       <AppDrawer onClose={closeDrawer} open={isOpen}>
