266543c95b524795ee210c0a063c97a24cd215b2
Olia Slisarchuk <korchanola@gmail.com>
2023-11-28 11:10:14 +0200

    only category data is passed to update attachment service

--- src/containers/my-resources/add-attachment-category-modal/AddAttachmentCategoryModal.constants.tsx
+++ src/containers/my-resources/add-attachment-category-modal/AddAttachmentCategoryModal.constants.tsx
@@ -1,15 +1,8 @@
 import { Attachment } from '~/types'
 
 export const getInitialValues = (attachment: Attachment) => {
-  const initialFileName = attachment.fileName.substring(
-    0,
-    attachment.fileName.lastIndexOf('.')
-  )
-
   return {
     id: attachment._id,
-    fileName: initialFileName ?? '',
-    category: null,
-    description: attachment.description ?? ''
+    category: null
   }
 }
