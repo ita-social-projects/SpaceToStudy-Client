dcee62e833fc036f8472c47f2636731d1dffae45
dmtrth25 <vodolazhchenko25@gmail.com>
2023-11-28 19:27:40 +0200

    fix

--- src/services/course-service.ts
+++ src/services/course-service.ts
@@ -2,13 +2,10 @@ import { AxiosResponse } from 'axios'
 import { URLs } from '~/constants/request'
 import { axiosClient } from '~/plugins/axiosClient'
 
-interface GetCoursesParams {
-  limit: number
-  skip: number
-}
+import { RequestParams } from '~/types/services/types/services.types'
 
 export const CourseService = {
-  getCourses: async (params?: GetCoursesParams): Promise<AxiosResponse> =>
+  getCourses: async (params?: Partial<RequestParams>): Promise<AxiosResponse> =>
     await axiosClient.get(URLs.courses.get, {
       params
     })
