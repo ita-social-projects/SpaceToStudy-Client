443651afd20bfe39e44849ae26b4cb8617a00051
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-11-29 13:29:17 +0200

    refactor code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,12 +9,6 @@ export const admin = 'admin'
 export const login = 'login'
 export const signup = 'signup'
 
-export const types = {
-  oneAnswer: 'oneAnswer',
-  openAnswer: 'openAnswer',
-  multipleChoice: 'multipleChoice'
-}
-
 export const snackbarVariants: { [key: string]: AlertColor } = {
   error: 'error',
   info: 'info',
--- src/utils/check-icons.tsx
+++ src/utils/check-icons.tsx
@@ -2,12 +2,16 @@ import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'
 import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined'
 import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined'
 
-import { types } from '~/constants'
+import { QuestionTypesEnum } from '~/types'
 
 export const CheckIcons = (type: string) => {
   return (
-    (type === types.oneAnswer && <CheckCircleOutlineIcon />) ||
-    (type === types.openAnswer && <DriveFileRenameOutlineOutlinedIcon />) ||
-    (type === types.multipleChoice && <FormatListBulletedOutlinedIcon />)
+    (type === QuestionTypesEnum.OneAnswer && <CheckCircleOutlineIcon />) ||
+    (type === QuestionTypesEnum.OpenAnswer && (
+      <DriveFileRenameOutlineOutlinedIcon />
+    )) ||
+    (type === QuestionTypesEnum.MultipleChoice && (
+      <FormatListBulletedOutlinedIcon />
+    ))
   )
 }
