0e5a2f1b90aa5cbcc13f33a267ab76e4d15bfec0
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2023-12-11 23:00:03 +0200

    fixed lint error

--- src/utils/check-icons.tsx
+++ src/utils/check-icons.tsx
@@ -2,16 +2,10 @@ import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'
 import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined'
 import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined'
 
-import { QuestionTypesEnum } from '~/types'
-
 export const CheckIcons = (type: string) => {
   return (
-    (type === QuestionTypesEnum.OneAnswer && <CheckCircleOutlineIcon />) ||
-    (type === QuestionTypesEnum.OpenAnswer && (
-      <DriveFileRenameOutlineOutlinedIcon />
-    )) ||
-    (type === QuestionTypesEnum.MultipleChoice && (
-      <FormatListBulletedOutlinedIcon />
-    ))
+    (type === 'oneAnswer' && <CheckCircleOutlineIcon />) ||
+    (type === 'openAnswer' && <DriveFileRenameOutlineOutlinedIcon />) ||
+    (type === 'multipleChoice' && <FormatListBulletedOutlinedIcon />)
   )
 }
