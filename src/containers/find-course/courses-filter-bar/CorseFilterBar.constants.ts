f6db607fb85e1420ee6a596772d633bf32df9659
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2024-01-16 18:34:13 +0200

    fixed comments

--- src/containers/find-course/courses-filter-bar/CorseFilterBar.constants.ts
+++ src/containers/find-course/courses-filter-bar/CorseFilterBar.constants.ts
@@ -1,8 +1,8 @@
 import { SortEnum } from '~/types'
 
 export const sortTranslationKeys = [
-  { title: 'myCoursesPage.sortTitles.newest', value: 'asc' },
-  { title: 'myCoursesPage.sortTitles.oldest', value: 'desc' }
+  { title: 'myCoursesPage.sortTitles.newest', value: SortEnum.Asc },
+  { title: 'myCoursesPage.sortTitles.oldest', value: SortEnum.Desc }
 ]
 
 export const initialSort = { order: SortEnum.Desc, orderBy: 'updatedAt' }
