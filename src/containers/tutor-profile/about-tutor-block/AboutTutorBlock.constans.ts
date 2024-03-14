d94a507b1f361879e6eec5117da32a8c3c32e093
dmtrth25 <vodolazhchenko25@gmail.com>
2024-03-14 12:24:21 +0200

    created about tutor block for my profile

---
+++ src/containers/tutor-profile/about-tutor-block/AboutTutorBlock.constans.ts
@@ -0,0 +1,22 @@
+export const accordionItems = [
+  {
+    title: 'tutorProfilePage.aboutTutor.education',
+    description: 'tutorProfilePage.aboutTutor.educationDescription'
+  },
+  {
+    title: 'tutorProfilePage.aboutTutor.workExperience',
+    description: 'tutorProfilePage.aboutTutor.workExperienceDescription'
+  },
+  {
+    title: 'tutorProfilePage.aboutTutor.scientificActivities',
+    description: 'tutorProfilePage.aboutTutor.scientificDescription'
+  },
+  {
+    title: 'tutorProfilePage.aboutTutor.awards',
+    description: 'tutorProfilePage.aboutTutor.awardsDescription'
+  },
+  {
+    title: 'tutorProfilePage.aboutTutor.socialMedia',
+    description: 'tutorProfilePage.aboutTutor.socialMediaDescription'
+  }
+]
