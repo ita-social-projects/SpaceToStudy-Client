b538b613bb6f5c50ec3598601db50b1da6375e3b
Mav-Ivan <110425368+Mav-Ivan@users.noreply.github.com>
2024-03-27 22:07:34 +0200

    fixed comments

---
+++ src/containers/edit-tutor-profile/NotificationContainer.constans.tsx
@@ -0,0 +1,18 @@
+export const titleWithSubtitle = [
+  {
+    title: 'editTutor.notificationTab.offerStatus',
+    subtitle: 'editTutor.notificationTab.offerSubtitle'
+  },
+  {
+    title: 'editTutor.notificationTab.chat',
+    subtitle: 'editTutor.notificationTab.chatSubtitle'
+  },
+  {
+    title: 'editTutor.notificationTab.similarOffers',
+    subtitle: 'editTutor.notificationTab.similarOffersSubtitle'
+  },
+  {
+    title: 'editTutor.notificationTab.email',
+    subtitle: 'editTutor.notificationTab.emailSubtitle'
+  }
+]
--- src/containers/edit-tutor-profile/NotificationContainer.tsx
+++ src/containers/edit-tutor-profile/NotificationContainer.tsx
@@ -1,52 +1,36 @@
-import Box from '@mui/system/Box'
-import Typography from '@mui/material/Typography'
-import Switch from '@mui/material/Switch'
-
 import { useTranslation } from 'react-i18next'
 
+import Box from '@mui/system/Box'
+import Switch from '@mui/material/Switch'
+
 import SettingItem from '~/components/setting-item/SettingItem'
+import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
+import { getRandomNumber } from '~/utils/helper-functions'
+import { titleWithSubtitle } from '~/containers/edit-tutor-profile/NotificationContainer.constans'
 import { styles } from '~/containers/edit-tutor-profile/NotificationContainer.style'
+
 export const NotificationContainer = () => {
   const { t } = useTranslation()
 
   return (
     <Box sx={styles.root}>
-      <Typography sx={styles.title}>
-        {t('editTutor.notificationTab.notifications')}
-      </Typography>
-      <Typography sx={styles.subtitle}>
-        {t('editTutor.notificationTab.mainHint')}
-      </Typography>
+      <TitleWithDescription
+        description={t('editTutor.notificationTab.mainHint')}
+        style={styles.titleWithDescription}
+        title={t('editTutor.notificationTab.notifications')}
+      />
 
       <Box sx={styles.optionsContainer}>
-        <SettingItem
-          style={styles.options}
-          subtitle={t('editTutor.notificationTab.offerSubtitle')}
-          title={t('editTutor.notificationTab.offerStatus')}
-        >
-          <Switch sx={styles.switch} />
-        </SettingItem>
-        <SettingItem
-          style={styles.options}
-          subtitle={t('editTutor.notificationTab.chatSubtitle')}
-          title={t('editTutor.notificationTab.chat')}
-        >
-          <Switch sx={styles.switch} />
-        </SettingItem>
-        <SettingItem
-          style={styles.options}
-          subtitle={t('editTutor.notificationTab.similarOffersSubtitle')}
-          title={t('editTutor.notificationTab.similarOffers')}
-        >
-          <Switch sx={styles.switch} />
-        </SettingItem>
-        <SettingItem
-          style={styles.options}
-          subtitle={t('editTutor.notificationTab.emailSubtitle')}
-          title={t('editTutor.notificationTab.email')}
-        >
-          <Switch sx={styles.switch} />
-        </SettingItem>
+        {titleWithSubtitle.map((item) => (
+          <SettingItem
+            key={getRandomNumber()}
+            style={styles.options}
+            subtitle={t(item.subtitle)}
+            title={t(item.title)}
+          >
+            <Switch sx={styles.switch} />
+          </SettingItem>
+        ))}
       </Box>
     </Box>
   )
