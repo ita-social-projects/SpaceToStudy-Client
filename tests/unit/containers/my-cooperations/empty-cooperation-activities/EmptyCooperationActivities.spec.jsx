508413dc572919e0370075c4d6eac4a448d2a32f
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-03-31 16:11:43 +0300

    fixing error with double quotes and adding tests to this component

--- tests/unit/containers/my-cooperations/empty-cooperation-activities/EmptyCooperationActivities.spec.jsx
+++ tests/unit/containers/my-cooperations/empty-cooperation-activities/EmptyCooperationActivities.spec.jsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { screen, fireEvent, act, waitFor } from '@testing-library/react'
 import EmptyCooperationActivities from '~/containers/my-cooperations/empty-cooperation-activities/EmptyCooperationActivities'
 import { renderWithProviders } from '~tests/test-utils'
 
@@ -9,5 +9,48 @@ describe('Cooperation activities', () => {
     const button = screen.getByText('cooperationsPage.button.create')
 
     expect(button).toBeInTheDocument()
+  })
+
+  it('should open menu course template after click on button and close menu after click on menu item', async () => {
+    renderWithProviders(<EmptyCooperationActivities />)
+
+    const button = screen.getByText('cooperationsPage.button.create')
+    act(() => {
+      fireEvent.click(button)
+    })
+    const menuTemplate = await screen.findByText(
+      'cooperationsPage.manyTypes.courseTemplate'
+    )
+    expect(menuTemplate).toBeInTheDocument()
+
+    act(() => {
+      fireEvent.click(menuTemplate)
+    })
+
+    await waitFor(() => {
+      expect(menuTemplate).not.toBeInTheDocument()
+    })
+  })
+
+  it('should open menu from scratch after click on button and close menu after click on menu item', async () => {
+    renderWithProviders(<EmptyCooperationActivities />)
+
+    const button = screen.getByText('cooperationsPage.button.create')
+    act(() => {
+      fireEvent.click(button)
+    })
+
+    const menuScratch = await screen.findByText(
+      'cooperationsPage.manyTypes.scratch'
+    )
+    expect(menuScratch).toBeInTheDocument()
+
+    act(() => {
+      fireEvent.click(menuScratch)
+    })
+
+    await waitFor(() => {
+      expect(menuScratch).not.toBeInTheDocument()
+    })
   })
 })
