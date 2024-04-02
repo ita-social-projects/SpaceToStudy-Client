14a70549a0c78993d0e55526dcbeb2b6b2110dc7
asya982 <bagin.nst@gmail.com>
2024-04-02 17:33:23 +0300

    separate logic for check & write test for files util

--- src/utils/validations/files.ts
+++ src/utils/validations/files.ts
@@ -3,30 +3,46 @@ import { AddDocuments } from '~/types'
 export const filesValidation = (
   files: File[],
   validationData: AddDocuments
-) => {
-  let error
-  if (files.some((file) => file.size > validationData.maxFileSize)) {
-    error = validationData.fileSizeError
-  }
-  if (
-    files.reduce((acc, file) => acc + file.size, 0) >
-    validationData.maxAllFilesSize
-  ) {
-    error = validationData.allFilesSizeError
-  }
-  if (
-    files.length > 0 &&
-    !files.every((file) =>
-      validationData.filesTypes.some((type) => file.type === type)
-    )
-  ) {
-    error = validationData.typeError
-  }
-  if (
-    files.some((file) => file.name.length > validationData.maxFileNameLength)
-  ) {
-    error = validationData.maxFileNameError
+): string | undefined => {
+  if (isFileNameValid(files, validationData.maxFileNameLength)) {
+    return validationData.maxFileNameError
   }
 
-  return error
+  if (isFileValid(files, validationData.filesTypes)) {
+    return validationData.typeError
+  }
+
+  if (isSizeValid(files, validationData.maxFileSize)) {
+    return validationData.fileSizeError
+  }
+
+  if (isSizeValid(files, validationData.maxAllFilesSize, true)) {
+    return validationData.allFilesSizeError
+  }
+}
+
+export const isFileNameValid = (files: File[], maxLength: number): boolean => {
+  return files.some((file) => file.name.length > maxLength)
+}
+
+export const isFileValid = (
+  files: File[],
+  acceptedTypes: string[]
+): boolean => {
+  return (
+    files.length > 0 &&
+    !files.every((file) => acceptedTypes.some((type) => file.type === type))
+  )
+}
+
+export const isSizeValid = (
+  files: File[],
+  maxSize: number,
+  countAll: boolean = false
+): boolean => {
+  if (countAll) {
+    return files.reduce((acc, file) => acc + file.size, 0) > maxSize
+  }
+
+  return files.some((file) => file.size > maxSize)
 }
