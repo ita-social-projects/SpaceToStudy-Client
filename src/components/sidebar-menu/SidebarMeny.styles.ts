1dcb5fddbf7170611e3978b3943ac03e74ab77f2
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-04-04 13:50:59 +0300

    sidebar button styles changes

--- src/components/sidebar-menu/SidebarMeny.styles.ts
+++ src/components/sidebar-menu/SidebarMeny.styles.ts
@@ -4,6 +4,19 @@ export const styles = {
   sidebarButton: {
     width: '30%',
     pr: { lg: '96px' },
+    pt: '0px',
+    '& .MuiListItem-root': {
+      pl: '0px',
+      '&:first-child': {
+        pt: '0px'
+      },
+      '&:not(:first-child)': {
+        pt: '18px'
+      }
+    },
+    '& .MuiListItemButton-root div': {
+      minWidth: '34px'
+    },
     '& .MuiButtonBase-root:hover': {
       backgroundColor: palette.basic.grey,
       '& .MuiTypography-body1': {
