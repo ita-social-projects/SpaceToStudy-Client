2992ca9722031371d4d8ba8346d3472924bf9ddf
Александр Мигаль <magalalecs2003@gmail.com>
2024-04-09 16:10:40 +0300

    fixed

--- src/pages/create-or-edit-lesson/CreateOrEditLesson.styles.ts
+++ src/pages/create-or-edit-lesson/CreateOrEditLesson.styles.ts
@@ -1,5 +1,18 @@
 import palette from '~/styles/app-theme/app.pallete'
-import { TypographyVariantEnum } from '~/types'
+import { VisibilityEnum } from '~/types'
+
+const inputFontSize = {
+  fontSize: '35px',
+  fontWeight: 500,
+  maxHeight: '35px',
+  marginTop: 0
+}
+
+const titleAndDescription = {
+  fontSize: '16px',
+  maxHeight: '16px',
+  marginTop: 0
+}
 
 export const styles = {
   root: {
@@ -8,46 +21,54 @@ export const styles = {
     gap: '24px',
     p: { sm: '40px 36px', md: '40px 72px' }
   },
+
   input: {
     style: {
       padding: 0
     }
   },
-  descriptionLabel: {
-    shrink: false,
-    sx: { typography: TypographyVariantEnum.Body1, top: -21 }
-  },
+
   divider: {
     color: 'primary.300'
   },
-  titleInput: (isDirty: boolean) => ({
-    disableUnderline: true,
-    style: {
-      fontSize: '35px',
-      fontWeight: 500,
-      marginTop: 0,
-      maxHeight: isDirty ? 'unset' : '35px'
-    }
-  }),
-  descriptionInput: (isDirty: boolean) => ({
-    disableUnderline: true,
-    style: {
-      fontSize: '16px',
-      padding: '0',
-      marginTop: 0,
-      maxHeight: isDirty ? 'unset' : '16px'
-    }
-  }),
-  titleLabel: {
+
+  titleLabel: (value: string) => ({
     shrink: false,
-    sx: { typography: TypographyVariantEnum.H4, top: -23 }
+    style: {
+      visibility: value ? VisibilityEnum.Hidden : VisibilityEnum.Visible,
+      color: palette.primary[300],
+      top: -23,
+      ...inputFontSize
+    }
+  }),
+
+  titleInput: {
+    style: inputFontSize,
+    disableUnderline: true
   },
+
+  descriptionLabel: (value: string) => ({
+    shrink: false,
+    style: {
+      visibility: value ? VisibilityEnum.Hidden : VisibilityEnum.Visible,
+      color: palette.primary[300],
+      top: -20,
+      ...titleAndDescription
+    }
+  }),
+
+  descriptionInput: {
+    style: titleAndDescription,
+    disableUnderline: true
+  },
+
   buttons: {
     alignSelf: { xs: 'center', sm: 'end' },
     display: 'flex',
     gap: { xs: '24px', sm: '30px' },
     justifyContent: 'space-between'
   },
+
   attachmentList: {
     container: {
       background: palette.basic.grey,
@@ -57,16 +78,19 @@ export const styles = {
       justifyContent: 'space-between'
     }
   },
+
   addIcon: { width: { xs: '18px', sm: '22px' }, ml: '5px' },
   addAttachments: {
     width: 'fit-content'
   },
+
   button: { width: '250px' },
   newAttachmentIcon: {
     fontSize: '25px',
     marginLeft: '13px',
     fontWeight: '400'
   },
+
   addAttachmentBtn: { width: 'fit-content' },
   addAttachmentIcon: { ml: '5px', width: { xs: '18px', sm: '22px' } }
 }
