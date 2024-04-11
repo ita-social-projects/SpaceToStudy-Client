da07bbed5070d08fdcd0b205a779aa62fadbf472
Trots Valentyna <valentinochkat16@gmail.com>
2024-04-11 18:31:14 +0300

    refactored test

--- tests/unit/containers/tutor-home-page/subjects-step/SubjectsStep.spec.jsx
+++ tests/unit/containers/tutor-home-page/subjects-step/SubjectsStep.spec.jsx
@@ -41,13 +41,11 @@ const btnsBox = (
   </div>
 )
 
-const chooseOption = async (name) => {
-  await waitFor(() => {
-    const firstOption = screen.getByRole('option', {
-      name
-    })
-    fireEvent.click(firstOption)
+const chooseOption = (name) => {
+  const firstOption = screen.getByRole('option', {
+    name
   })
+  fireEvent.click(firstOption)
 }
 
 const chooseCategoryOrSubject = async (inputField, value) => {
@@ -58,7 +56,7 @@ const chooseCategoryOrSubject = async (inputField, value) => {
     })
   })
 
-  chooseOption(new RegExp(value, 'i'))
+  chooseOption(value)
 }
 
 describe('SubjectsStep test with some data', () => {
@@ -79,126 +77,79 @@ describe('SubjectsStep test with some data', () => {
     vi.clearAllMocks()
   })
 
-  it('should add a new subject and delete a subject ', async () => {
-    const addSubject = screen.getByTestId('add-subject')
+  it('should render SubjectsStep title', () => {
+    const title = screen.getByText('becomeTutor.categories.title')
+    expect(title).toBeInTheDocument()
+  })
 
+  it('should add a new subject and delete a subject', async () => {
+    const addSubject = screen.getByTestId('add-subject')
     expect(addSubject).toBeInTheDocument()
 
     const firstField = screen.getByLabelText(
-      /becomeTutor.categories.mainSubjectsLabel/i
+      'becomeTutor.categories.mainSubjectsLabel'
     )
-
     expect(firstField.value).toBe('')
 
     await chooseCategoryOrSubject(firstField, 'Category 2')
-
     expect(firstField.value).toBe('Category 2')
 
     const secondField = screen.getByLabelText(
-      /becomeTutor.categories.subjectLabel/i
+      'becomeTutor.categories.subjectLabel'
     )
-
-    await waitFor(() => {
-      fireEvent.click(secondField)
-      fireEvent.change(secondField, {
-        target: { value: 'Subject 1' }
-      })
-    })
-
-    chooseOption(/Subject 1/i)
-
+    await chooseCategoryOrSubject(secondField, 'Subject 1')
     expect(secondField.value).toBe('Subject 1')
 
-    await waitFor(() => {
-      fireEvent.click(addSubject)
-    })
+    await chooseCategoryOrSubject(firstField, 'Category 1')
+    expect(secondField.value).toBe('')
+
+    await chooseCategoryOrSubject(secondField, 'Subject 1')
+
+    fireEvent.click(addSubject)
+
     const chip = screen.getByTestId('chip')
     expect(chip).toBeInTheDocument()
 
     const deleteChipButton = screen.getByTestId('CloseRoundedIcon')
-    await waitFor(() => {
-      fireEvent.click(deleteChipButton)
-    })
+    fireEvent.click(deleteChipButton)
     expect(chip).not.toBeInTheDocument()
   })
 
   it('should show an error message "All fields must be filled"', async () => {
     const addSubject = screen.getByTestId('add-subject')
-
     const firstField = screen.getByLabelText(
-      /becomeTutor.categories.mainSubjectsLabel/i
+      'becomeTutor.categories.mainSubjectsLabel'
     )
+    await chooseCategoryOrSubject(firstField, 'Category 3')
 
-    await waitFor(() => {
-      fireEvent.click(firstField)
-      fireEvent.change(firstField, {
-        target: { value: 'Category 3' }
-      })
-    })
-
-    chooseOption(/Category 3/i)
-
-    await waitFor(() => {
-      fireEvent.click(addSubject)
-    })
+    fireEvent.click(addSubject)
 
     const errorMessage = screen.getByText('becomeTutor.categories.emptyFields')
-
     expect(errorMessage).toBeInTheDocument()
   })
 
   it('should show an error message "You have the same subject"', async () => {
     const addSubject = screen.getByTestId('add-subject')
-
     const firstField = screen.getByLabelText(
-      /becomeTutor.categories.mainSubjectsLabel/i
+      'becomeTutor.categories.mainSubjectsLabel'
     )
-
     const secondField = screen.getByLabelText(
-      /becomeTutor.categories.subjectLabel/i
+      'becomeTutor.categories.subjectLabel'
     )
 
     for (let i = 0; i < 2; i++) {
-      await waitFor(() => {
-        fireEvent.click(firstField)
-        fireEvent.change(firstField, { target: { value: 'Category 2' } })
-      })
-
-      chooseOption(/Category 2/i)
-
-      await waitFor(() => {
-        fireEvent.click(secondField)
-        fireEvent.change(secondField, {
-          target: { value: 'Subject 2' }
-        })
-      })
-
-      chooseOption(/Subject 2/i)
-
-      await waitFor(() => {
-        fireEvent.click(addSubject)
-      })
+      await chooseCategoryOrSubject(firstField, 'Category 2')
+      await chooseCategoryOrSubject(secondField, 'Subject 2')
+      fireEvent.click(addSubject)
     }
 
     let errorMessage = screen.getByText('becomeTutor.categories.sameSubject')
-
     expect(errorMessage).toBeInTheDocument()
 
-    await waitFor(() => {
-      fireEvent.click(secondField)
-      fireEvent.change(secondField, {
-        target: { value: 'Subject 1' }
-      })
-    })
-
-    chooseOption(/Subject 1/i)
-
-    await waitFor(() => {
-      fireEvent.click(addSubject)
-    })
+    await chooseCategoryOrSubject(secondField, 'Subject 1')
+    fireEvent.click(addSubject)
 
     errorMessage = screen.queryByText('becomeTutor.categories.sameSubject')
-
     expect(errorMessage).toBeNull()
   })
 })
