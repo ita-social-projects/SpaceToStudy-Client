f782d3d57f145bb4dbc287fb8774b4f0eb93329d
Anastasiia Ivanchenko <iva21sta@gmail.com>
2024-04-11 11:54:33 +0300

    increase CooperationNotes coverage

--- tests/unit/containers/my-cooperations/cooperation-notes/CooperationNotes.spec.jsx
+++ tests/unit/containers/my-cooperations/cooperation-notes/CooperationNotes.spec.jsx
@@ -2,6 +2,9 @@ import { screen, fireEvent, act, waitFor } from '@testing-library/react'
 import CooperationNotes from '~/containers/my-cooperations/cooperation-notes/CooperationNotes'
 import { renderWithProviders } from '~tests/test-utils'
 import { CooperationNotesService } from '~/services/cooperation-service'
+import { ConfirmationDialogProvider } from '~/context/confirm-context'
+
+vi.mock('~/services/cooperation-service')
 
 const mockNotesData = [
   {
@@ -32,15 +35,21 @@ const mockNotesData = [
   }
 ]
 
-vi.mock('~/services/cooperation-service', () => ({
-  CooperationNotesService: {
-    getNotes: vi.fn()
-  }
-}))
+const userState = {
+  appMain: { userRole: 'tutor', userId: mockNotesData[0].author._id }
+}
 
 describe('CooperationNotes', () => {
-  beforeEach(async () => {
-    await waitFor(() => renderWithProviders(<CooperationNotes />))
+  beforeEach(() => {
+    CooperationNotesService.getNotes.mockResolvedValue({
+      data: mockNotesData
+    })
+    renderWithProviders(
+      <ConfirmationDialogProvider>
+        <CooperationNotes />
+      </ConfirmationDialogProvider>,
+      { preloadedState: userState }
+    )
   })
 
   it('should render CooperationNotes components', () => {
@@ -49,7 +58,7 @@ describe('CooperationNotes', () => {
     expect(notes).toBeInTheDocument()
   })
 
-  it('should open create note form', () => {
+  it('should open create note form and save the note', async () => {
     const addNoteBtn = screen.getByTestId('AddIcon')
 
     act(() => fireEvent.click(addNoteBtn))
@@ -59,10 +68,27 @@ describe('CooperationNotes', () => {
     )
 
     expect(noteFormSettings).toBeInTheDocument()
-  })
 
-  it('fetches data successfully', () => {
-    CooperationNotesService.getNotes.mockResolvedValue({ data: mockNotesData })
+    const noteTextInput = screen.getByLabelText(
+      'cooperationsPage.notes.noteText'
+    )
+    const saveButton = screen.getByRole('button', { name: 'common.save' })
+
+    act(() =>
+      fireEvent.change(noteTextInput, {
+        target: { value: 'Newly created note' }
+      })
+    )
+    act(() => fireEvent.click(saveButton))
+
+    CooperationNotesService.createNote.mockResolvedValue({
+      data: mockNotesData
+    })
+
+    await waitFor(() => {
+      const newNoteText = screen.getByText('Newly created note')
+      expect(newNoteText).toBeInTheDocument()
+    })
   })
 
   it('should close create note form', () => {
@@ -79,5 +105,82 @@ describe('CooperationNotes', () => {
     )
 
     expect(noteFormSettings).toBeNull()
+  })
+
+  it('should enable the edit mode and handle updates', () => {
+    const [menuButton] = screen.getAllByTestId('MoreVertIcon')
+
+    act(() => fireEvent.click(menuButton))
+
+    const editButton = screen.getByTestId('EditIcon')
+
+    act(() => fireEvent.click(editButton))
+
+    const [noteTextInput] = screen.getAllByDisplayValue(mockNotesData[0].text)
+    const saveButton = screen.getByRole('button', { name: 'common.save' })
+
+    act(() =>
+      fireEvent.change(noteTextInput, { target: { value: 'New note text' } })
+    )
+    act(() => fireEvent.click(saveButton))
+
+    CooperationNotesService.updateNote.mockResolvedValue({
+      data: mockNotesData
+    })
+
+    const newNoteText = screen.getByText('New note text')
+
+    expect(newNoteText).toBeInTheDocument()
+  })
+
+  it('should handle duplicate notes', async () => {
+    const [menuButton] = screen.getAllByTestId('MoreVertIcon')
+
+    act(() => fireEvent.click(menuButton))
+
+    const duplicateButton = screen.getByTestId('ContentCopyIcon')
+
+    act(() => fireEvent.click(duplicateButton))
+
+    await waitFor(() => {
+      expect(duplicateButton).not.toBeInTheDocument()
+    })
+  })
+
+  it('should handle delete notes', async () => {
+    const [menuButton] = screen.getAllByTestId('MoreVertIcon')
+
+    act(() => fireEvent.click(menuButton))
+
+    const deleteButton = screen.getByTestId('DeleteOutlineIcon')
+
+    act(() => fireEvent.click(deleteButton))
+
+    const confirmButton = screen.getByRole('button', { name: 'common.yes' })
+
+    act(() => fireEvent.click(confirmButton))
+
+    await waitFor(() => {
+      expect(confirmButton).not.toBeInTheDocument()
+    })
+  })
+})
+
+describe('CooperationNotes with error', () => {
+  const fakeError = {
+    message: 'Something went wrong'
+  }
+
+  beforeEach(() => {
+    CooperationNotesService.getNotes.mockRejectedValue({
+      response: { data: fakeError }
+    })
+    renderWithProviders(<CooperationNotes />, { preloadedState: userState })
+  })
+
+  it('should show the error message', () => {
+    const errorAlert = screen.getByText(`errors.${fakeError.message}`)
+
+    expect(errorAlert).toBeInTheDocument()
   })
 })
