2609b1b84c8f9406ca84936b1213c67aaccd9184
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-04-16 17:44:31 +0300

    Fixed course grid (#1785)
    * Fixed courses grid

--- src/containers/my-courses/my-courses-container/MyCorsesCardsList.styles.ts
+++ src/containers/my-courses/my-courses-container/MyCorsesCardsList.styles.ts
@@ -1,7 +1,11 @@
 export const styles = {
   wrapper: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: 'grid',
+    gridTemplateColumns: {
+      xs: 'minmax(0, 1fr)',
+      md: 'repeat(2, minmax(0, 1fr))',
+      lg: 'repeat(3, minmax(0, 1fr))'
+    },
     gap: '24px',
     mb: '24px'
   }
