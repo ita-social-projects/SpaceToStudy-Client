c72d8506a63c1e28d8bb872bbeb20785fcdad6d4
Александр Мигаль <magalalecs2003@gmail.com>
2024-04-18 15:29:55 +0300

    add ComponentEnum.Img

--- src/components/video-box/VideoBox.tsx
+++ src/components/video-box/VideoBox.tsx
@@ -4,17 +4,23 @@ import titleBar from '~/assets/img/guest-home-page/title-bar.png'
 import VideoPlayer from '~/components/video-player/VideoPlayer'
 import { styles } from '~/components/video-box/VideoBox.styles'
 import { VideoBoxType } from '~/types/video-box/videoBox.index'
+import { ComponentEnum } from '~/types'
 
 const VideoBox: FC<VideoBoxType> = ({ video, videoPreview, videoMock }) => {
   const videoBlockContent = videoPreview ? (
-    <Box alt='Video' component='img' src={videoMock} sx={styles.video} />
+    <Box
+      alt='Video'
+      component={ComponentEnum.Img}
+      src={videoMock}
+      sx={styles.video}
+    />
   ) : (
     <VideoPlayer video={video} />
   )
 
   return (
     <Box data-testid='videoBox' sx={styles.videoBox}>
-      <Box alt='Title bar' component='img' src={titleBar} />
+      <Box alt='Title bar' component={ComponentEnum.Img} src={titleBar} />
       <Box sx={styles.videoBg}>{videoBlockContent}</Box>
     </Box>
   )
