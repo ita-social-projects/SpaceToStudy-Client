a9c0ceedb3e95920376c62fa056bc5d729fcc1ff
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-04-29 19:39:16 +0300

    render tooltip title conditionaly

--- src/containers/my-quizzes/create-or-edit-quiz-container/CreateOrEditQuizContainer.tsx
+++ src/containers/my-quizzes/create-or-edit-quiz-container/CreateOrEditQuizContainer.tsx
@@ -199,26 +199,45 @@ const CreateOrEditQuizContainer = ({
         })
       : void addNewQuiz({ title, description, items: questions, category })
 
-  const CreateQuestionButton = isCreationOpen ? (
+  // const CreateQuestionButton = isCreationOpen ? (
+  //   <Tooltip
+  //     arrow
+  //     placement='top'
+  //     title={t('myResourcesPage.quizzes.savePreviousQuestion')}
+  //   >
+  //     <AppButton size={SizeEnum.ExtraLarge} variant={ButtonVariantEnum.Tonal}>
+  //       {t('myResourcesPage.quizzes.createNewQuestion')}
+  //       <EditIcon fontSize={SizeEnum.Small} />
+  //     </AppButton>
+  //   </Tooltip>
+  // ) : (
+  //   <AppButton
+  //     onClick={onOpenCreateQuestion}
+  //     size={SizeEnum.ExtraLarge}
+  //     variant={ButtonVariantEnum.Tonal}
+  //   >
+  //     {t('myResourcesPage.quizzes.createNewQuestion')}
+  //     <EditIcon fontSize={SizeEnum.Small} />
+  //   </AppButton>
+  // )
+
+  const CreateQuestionButton = (
     <Tooltip
       arrow
       placement='top'
-      title={t('myResourcesPage.quizzes.savePreviousQuestion')}
+      title={
+        isCreationOpen ? t('myResourcesPage.quizzes.savePreviousQuestion') : ''
+      }
     >
-      <AppButton size={SizeEnum.ExtraLarge} variant={ButtonVariantEnum.Tonal}>
+      <AppButton
+        onClick={onOpenCreateQuestion}
+        size={SizeEnum.ExtraLarge}
+        variant={ButtonVariantEnum.Tonal}
+      >
         {t('myResourcesPage.quizzes.createNewQuestion')}
         <EditIcon fontSize={SizeEnum.Small} />
       </AppButton>
     </Tooltip>
-  ) : (
-    <AppButton
-      onClick={onOpenCreateQuestion}
-      size={SizeEnum.ExtraLarge}
-      variant={ButtonVariantEnum.Tonal}
-    >
-      {t('myResourcesPage.quizzes.createNewQuestion')}
-      <EditIcon fontSize={SizeEnum.Small} />
-    </AppButton>
   )
 
   return (
