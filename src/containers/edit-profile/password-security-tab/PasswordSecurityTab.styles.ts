114554685c406f63e4ae2244607714e1c5438df1
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-06-01 19:36:54 +0300

    style changes

--- src/containers/edit-profile/password-security-tab/PasswordSecurityTab.styles.ts
+++ src/containers/edit-profile/password-security-tab/PasswordSecurityTab.styles.ts
@@ -22,15 +22,19 @@ export const styles = {
   passwordButtonsContainer: {
     m: '10px 0 20px',
     display: 'grid',
-    gridTemplateColumns: 'repeat(9, minmax(0, 1fr))',
+    gridTemplateColumns: {
+      sm: 'repeat(5, minmax(0, 1fr))',
+      md: 'repeat(7, minmax(0, 1fr))',
+      lg: 'repeat(10, minmax(0, 1fr))'
+    },
     gap: '10px'
   },
 
   saveButton: {
-    gridColumn: 'span 3'
+    gridColumn: { sm: 'span 3', md: 'span 3', lg: 'span 3' }
   },
   discardButton: {
-    gridColumn: 'span 2'
+    gridColumn: { sm: 'span 1', md: 'span 2', lg: 'span 2' }
   },
   deactivateButton: {
     mt: '20px',
