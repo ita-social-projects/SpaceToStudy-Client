ce3c2cef0bf5108191c970ee3f4cbcded3458560
Dinviz <eugene.hetsyanyn@gmail.com>
2024-06-05 18:36:38 +0300

    fixed styles for active tab

--- src/components/tab/Tab.styles.ts
+++ src/components/tab/Tab.styles.ts
@@ -3,16 +3,17 @@ import { fadeAnimation } from '~/styles/app-theme/custom-animations'
 const activeTab = {
   color: 'primary.600',
   borderBottom: '2px solid',
-  pb: '14px',
   ...fadeAnimation
 }
 
 export const styles = {
   tab: (isActive: boolean) => ({
     px: { xs: '32px', sm: '44px' },
+    pb: '14px',
     cursor: 'pointer',
     color: 'primary.300',
     typography: 'subtitle2',
+    borderBottom: '2px solid transparent',
     borderRadius: 0,
     ...(isActive && activeTab)
   })
