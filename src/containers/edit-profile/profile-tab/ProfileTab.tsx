1e8bfd4be72ca13614f4b1d667394bbea6e0049d
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-06-05 14:20:40 +0300

    removed void

--- src/containers/edit-profile/profile-tab/ProfileTab.tsx
+++ src/containers/edit-profile/profile-tab/ProfileTab.tsx
@@ -74,7 +74,7 @@ const ProfileTab: FC<ProfileTabProps> = ({ user }) => {
 
   const handleUpdateData = () => {
     const updatedData = getUserUpdatedData(data)
-    void handleSubmit(updatedData)
+    handleSubmit(updatedData)
   }
 
   const hasError = Object.values(errors).some((error) => error)
