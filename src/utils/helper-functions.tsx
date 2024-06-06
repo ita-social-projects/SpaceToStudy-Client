a45be982277b01d16208b3751f1edb13aa6dce52
Анна-Марія <anna-mariia.kurhanevych.pz.2021@lpnu.ua>
2024-06-06 11:45:56 +0300

    Added helper function for initials

--- src/utils/helper-functions.tsx
+++ src/utils/helper-functions.tsx
@@ -300,3 +300,6 @@ export const parseFileName = (
     fileExtension: fileArr[fileArr.length - 1]
   }
 }
+
+export const getInitials = (firstName: string, lastName: string): string =>
+  `${firstName[0]}${lastName[0]}`.toUpperCase()
