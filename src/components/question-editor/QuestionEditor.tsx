155e5c41cf074ce7a9510b8d20d9407715f6bc01
docia <matiushenko107@gmail.com>
2024-06-14 23:16:17 +0300

    Fix styles when Ukrainian language is used

--- src/components/question-editor/QuestionEditor.tsx
+++ src/components/question-editor/QuestionEditor.tsx
@@ -186,7 +186,7 @@ const QuestionEditor: FC<QuestionEditorProps> = ({
       <Box sx={styles.header}>
         <Box sx={styles.options}>
           {option && <Box sx={styles.iconWrapper}>{option.icon}</Box>}
-          <AppSelect
+          <AppSelect<string>
             fields={sortOptions}
             setValue={handleTypeChange}
             sx={styles.selectContainer}
