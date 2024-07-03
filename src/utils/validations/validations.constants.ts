7b5615e7f2932200534d8c95762ce54cbb704ca1
docia <matiushenko107@gmail.com>
2024-07-03 16:56:46 +0300

    Update validations.constants.ts

--- src/utils/validations/validations.constants.ts
+++ src/utils/validations/validations.constants.ts
@@ -3,5 +3,5 @@ export const validationPatterns = {
   number: /^-?(?:\d+|\d*\.\d+)(?:[eE][+-]?\d+)?$/,
   email: /^([a-z\d]+([._-][a-z\d]+)*)@([a-z\d]+([.-][a-z\d]+)*\.[a-z]{2,})$/i,
   passwordValid: /^\S+$/i,
-  passwordAlphabeticAndNumeric: /(?=.*\d)(?=.*[a-zа-яєії])/i
+  passwordAlphabeticAndNumeric: /^(?=.*[a-zа-яєії])(?=.*\d).+$/i
 }
