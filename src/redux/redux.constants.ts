d460bc6360159ab372c0e1dae7c6f1e184eef7b4
YaroslavLys <74725159+YaroslavLys@users.noreply.github.com>
2024-07-08 13:27:26 +0300

    Created LoadingStatus enum

--- src/redux/redux.constants.ts
+++ src/redux/redux.constants.ts
@@ -3,3 +3,16 @@ export const sliceNames = {
   snackbar: 'snackbarSlice',
   editProfile: 'editProfileSlice'
 }
+
+export enum LoadingStatusEnum {
+  Idle = 'idle',
+  Pending = 'pending',
+  Fulfilled = 'fulfilled',
+  Rejected = 'rejected'
+}
+
+export type LoadingStatus =
+  | LoadingStatusEnum.Idle
+  | LoadingStatusEnum.Pending
+  | LoadingStatusEnum.Fulfilled
+  | LoadingStatusEnum.Rejected
