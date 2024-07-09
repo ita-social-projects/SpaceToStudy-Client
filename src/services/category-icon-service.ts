de785c1a9745979fbf190c600c5c32227029f13b
Artem Balanovskyi <108689551+abalanovsky@users.noreply.github.com>
2024-07-09 12:22:30 +0300

    Added new categories icons (#2068)
    * Added new categories icons
    
    * Fixed tests

--- src/services/category-icon-service.ts
+++ src/services/category-icon-service.ts
@@ -1,30 +1,33 @@
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance'
-import BiotechIcon from '@mui/icons-material/Biotech'
-import ColorLensIcon from '@mui/icons-material/ColorLens'
-import DesignServicesIcon from '@mui/icons-material/DesignServices'
-import DesktopMacOutlinedIcon from '@mui/icons-material/DesktopMacOutlined'
+import TagRoundedIcon from '@mui/icons-material/TagRounded'
+import ScienceRoundedIcon from '@mui/icons-material/ScienceRounded'
+import CampaignIcon from '@mui/icons-material/Campaign'
+import MusicNoteRoundedIcon from '@mui/icons-material/MusicNoteRounded'
+import ColorLensRoundedIcon from '@mui/icons-material/ColorLensRounded'
+import StackedBarChartIcon from '@mui/icons-material/StackedBarChart'
+import LegendToggleRoundedIcon from '@mui/icons-material/LegendToggleRounded'
 import LanguageIcon from '@mui/icons-material/Language'
-import LegendToggleIcon from '@mui/icons-material/LegendToggle'
-import MusicNoteIcon from '@mui/icons-material/MusicNote'
-import ScienceIcon from '@mui/icons-material/Science'
-import StarIcon from '@mui/icons-material/Star'
-import TagIcon from '@mui/icons-material/Tag'
-import HistoryEduIcon from '@mui/icons-material/HistoryEdu'
+import DesignServicesIcon from '@mui/icons-material/DesignServices'
+import HistoryEduRoundedIcon from '@mui/icons-material/HistoryEduRounded'
+import DesktopMacRoundedIcon from '@mui/icons-material/DesktopMacRounded'
+import AccountTreeRoundedIcon from '@mui/icons-material/AccountTreeRounded'
+import StarRoundedIcon from '@mui/icons-material/StarRounded'
+
 import { SvgIconComponent } from '@mui/icons-material'
 
 const CATEGORY_ICONS = {
   LanguageIcon,
-  ColorLensIcon,
-  BiotechIcon,
-  DesktopMacOutlinedIcon,
-  TagIcon,
-  AccountBalanceIcon,
-  StarIcon,
   DesignServicesIcon,
-  MusicNoteIcon,
-  ScienceIcon,
-  LegendToggleIcon,
-  HistoryEduIcon
+  TagRoundedIcon,
+  ScienceRoundedIcon,
+  CampaignIcon,
+  MusicNoteRoundedIcon,
+  ColorLensRoundedIcon,
+  StackedBarChartIcon,
+  LegendToggleRoundedIcon,
+  HistoryEduRoundedIcon,
+  DesktopMacRoundedIcon,
+  AccountTreeRoundedIcon,
+  StarRoundedIcon
 } as const
 
 type CategoryIconKey = keyof typeof CATEGORY_ICONS
