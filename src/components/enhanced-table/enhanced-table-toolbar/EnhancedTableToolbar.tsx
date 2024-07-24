7715e798fcfb1349d8cc93dd8fd1c63c8133cc15
Mike-Popovych <volt251988@gmail.com>
2024-07-24 17:24:24 +0300

    rewrote EnhancedTableToolbar to tsx

--- src/components/enhanced-table/enhanced-table-toolbar/EnhancedTableToolbar.tsx
+++ src/components/enhanced-table/enhanced-table-toolbar/EnhancedTableToolbar.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next'
+import { ReactNode } from 'react'
 
 import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
@@ -6,18 +7,35 @@ import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
 
 import { styles } from './EnhancedTableToolbar.styles'
+import { TableActionFunc } from '~/types'
 
-const EnhancedTableToolbar = ({ refetchData, itemIds, bulkActions }) => {
+interface BulkAction {
+  title: string
+  func: TableActionFunc
+  icon: ReactNode
+}
+
+interface EnhancedTableToolbarProps {
+  refetchData: () => void
+  itemIds: string[]
+  bulkActions: BulkAction[]
+}
+
+const EnhancedTableToolbar = ({
+  refetchData,
+  itemIds,
+  bulkActions
+}: EnhancedTableToolbarProps) => {
   const { t } = useTranslation()
 
-  const onAction = async (actionFunc) => {
-    await actionFunc({ itemIds })
+  const onAction = async (actionFunc: TableActionFunc) => {
+    await actionFunc(itemIds)
     refetchData()
   }
 
   const actionButtons = bulkActions.map(({ title, func, icon }) => (
     <Tooltip key={title} placement='top' title={title}>
-      <IconButton onClick={() => onAction(func)}>{icon}</IconButton>
+      <IconButton onClick={() => void onAction(func)}>{icon}</IconButton>
     </Tooltip>
   ))
 
