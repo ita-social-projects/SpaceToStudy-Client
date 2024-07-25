e9e4b058823dc0d19db51f1f91a70af6cdef6ded
ValyaDudchak <48631216+ValyaDudchak@users.noreply.github.com>
2024-07-25 21:44:57 +0300

    fixed warning about useMemo

--- src/components/complete-profile/CompleteProfileBlock.tsx
+++ src/components/complete-profile/CompleteProfileBlock.tsx
@@ -58,7 +58,7 @@ const CompleteProfileBlock: FC<CompleteProfileBlockProps> = ({
           role={role}
         />
       )),
-    [profileItems, checkProfileData]
+    [profileItems, checkProfileData, role]
   )
 
   const handleToggleMenu = () => {
