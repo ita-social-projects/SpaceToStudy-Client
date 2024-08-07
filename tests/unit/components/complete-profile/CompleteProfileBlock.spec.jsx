17b9f37a9480ae1d228191529d84114410709b39
Valentyna Dudchak <dudchak.valya@gmail.com>
2024-08-07 20:27:40 +0300

    fixed tests

--- tests/unit/components/complete-profile/CompleteProfileBlock.spec.jsx
+++ tests/unit/components/complete-profile/CompleteProfileBlock.spec.jsx
@@ -1,5 +1,5 @@
 import { vi } from 'vitest'
-import { screen } from '@testing-library/react'
+import { screen, fireEvent } from '@testing-library/react'
 import CompleteProfileBlock from '~/components/complete-profile/CompleteProfileBlock'
 import {
   profileItemsTutor,
@@ -122,46 +122,48 @@ describe('CompleteProfile test when user data is empty', () => {
   })
 })
 
-// it('Check button less or more for tutor', () => {
-//   renderWithProviders(
-//     <CompleteProfileBlock
-//       data={mockData}
-//       profileItems={profileItemsTutor}
-//       role='tutor'
-//     />,
-//     {
-//       initialEntries: badRoute,
-//       preloadedState: { appMain: { userRole: 'tutor' } }
-//     }
-//   )
-//   const lessOrMoreButton = screen.getByTestId('showOrHide')
-//   const moreIcon = screen.getByTestId('icon-more')
-//   expect(moreIcon).toBeInTheDocument()
+describe('CompleteProfile test ui', () => {
+  it('Check button less or more for tutor', () => {
+    renderWithProviders(
+      <CompleteProfileBlock
+        data={mockDataEmpty}
+        profileItems={profileItemsTutor}
+        role='tutor'
+      />,
+      {
+        initialEntries: badRoute,
+        preloadedState: { appMain: { userRole: 'tutor' } }
+      }
+    )
+    const lessOrMoreButton = screen.getByTestId('showOrHide')
+    const moreIcon = screen.getByTestId('icon-more')
+    expect(moreIcon).toBeInTheDocument()
 
-//   fireEvent.click(lessOrMoreButton)
+    fireEvent.click(lessOrMoreButton)
 
-//   const lessIcon = screen.getByTestId('icon-less')
-//   expect(lessIcon).toBeInTheDocument()
-// })
+    const lessIcon = screen.getByTestId('icon-less')
+    expect(lessIcon).toBeInTheDocument()
+  })
 
-// it('Check button less or more for student', () => {
-//   renderWithProviders(
-//     <CompleteProfileBlock
-//       data={mockData}
-//       profileItems={profileItemsStudent}
-//       role='student'
-//     />,
-//     {
-//       initialEntries: badRoute,
-//       preloadedState: { appMain: { userRole: 'student' } }
-//     }
-//   )
-//   const lessOrMoreButton = screen.getByTestId('showOrHide')
-//   const moreIcon = screen.getByTestId('icon-more')
-//   expect(moreIcon).toBeInTheDocument()
+  it('Check button less or more for student', () => {
+    renderWithProviders(
+      <CompleteProfileBlock
+        data={mockDataEmpty}
+        profileItems={profileItemsStudent}
+        role='student'
+      />,
+      {
+        initialEntries: badRoute,
+        preloadedState: { appMain: { userRole: 'student' } }
+      }
+    )
+    const lessOrMoreButton = screen.getByTestId('showOrHide')
+    const moreIcon = screen.getByTestId('icon-more')
+    expect(moreIcon).toBeInTheDocument()
 
-//   fireEvent.click(lessOrMoreButton)
+    fireEvent.click(lessOrMoreButton)
 
-//   const lessIcon = screen.getByTestId('icon-less')
-//   expect(lessIcon).toBeInTheDocument()
-// })
+    const lessIcon = screen.getByTestId('icon-less')
+    expect(lessIcon).toBeInTheDocument()
+  })
+})
