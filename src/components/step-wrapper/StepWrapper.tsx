c20dc8ccc84129efb4dad96ed5aa1e9d29e3394d
Pasichnichenkoilya <sheredan2003@gmail.com>
2024-08-07 14:57:36 +0300

    Fix issues

---
+++ src/components/step-wrapper/StepWrapper.tsx
@@ -0,0 +1,88 @@
+import { cloneElement } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+
+import EastIcon from '@mui/icons-material/East'
+import WestIcon from '@mui/icons-material/West'
+
+import AppButton from '~/components/app-button/AppButton'
+import useSteps from '~/hooks/use-steps'
+import { styles } from '~/components/step-wrapper/StepWrapper.styles'
+import { SizeEnum } from '~/types'
+
+interface StepWrapperProps {
+  children: JSX.Element[]
+  steps: string[]
+}
+
+const StepWrapper = ({ children, steps }: StepWrapperProps) => {
+  const { activeStep, stepErrors, isLastStep, loading, stepOperation } =
+    useSteps({
+      steps
+    })
+  const { next, back, setActiveStep, handleSubmit } = stepOperation
+  const { t } = useTranslation()
+
+  const stepLabels = steps.map((step, index) => (
+    <Box
+      color={stepErrors[index] ? 'error.500' : 'primary.500'}
+      key={step}
+      onClick={() => setActiveStep(index)}
+      sx={[styles.defaultTab, index === activeStep && styles.activeTab]}
+      typography='caption'
+    >
+      {t(`step.stepLabels.${step}`)}
+    </Box>
+  ))
+
+  const nextButton = isLastStep ? (
+    <AppButton
+      loading={loading}
+      onClick={() => void handleSubmit()}
+      size={SizeEnum.Small}
+      sx={styles.finishBtn}
+      variant='contained'
+    >
+      {t('common.finish')}
+    </AppButton>
+  ) : (
+    <AppButton
+      onClick={next}
+      size={SizeEnum.Small}
+      sx={styles.btn}
+      variant='contained'
+    >
+      {t('common.next')}
+      <EastIcon fontSize='small' />
+    </AppButton>
+  )
+
+  const btnsBox = (
+    <Box sx={styles.btnWrapper}>
+      <AppButton
+        disabled={activeStep === 0}
+        onClick={back}
+        size={SizeEnum.Small}
+        sx={styles.btn}
+        variant='outlined'
+      >
+        <WestIcon fontSize='small' />
+        {t('common.back')}
+      </AppButton>
+      {nextButton}
+    </Box>
+  )
+
+  return (
+    <Container sx={styles.root}>
+      <Box sx={styles.steps}>{stepLabels}</Box>
+      <Box sx={styles.stepContent}>
+        {cloneElement(children[activeStep], { btnsBox })}
+      </Box>
+    </Container>
+  )
+}
+
+export default StepWrapper
