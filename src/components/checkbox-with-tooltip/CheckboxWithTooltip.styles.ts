acb6ccece3532c4389d00f8df24340254d7e32e1
YaroslavChuiko <jarik.tchuicko@gmail.com>
2024-08-13 20:14:44 +0300

    Created CheckboxWithTooltip component

---
+++ src/components/checkbox-with-tooltip/CheckboxWithTooltip.styles.ts
@@ -0,0 +1,22 @@
+import palette from '~/styles/app-theme/app.pallete'
+
+export const styles = {
+  root: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  label: {
+    mr: '8px'
+  },
+  typography: {
+    fontSize: '14px'
+  },
+  icon: {
+    color: palette.primary[500]
+  },
+  tooltip: {
+    fontSize: '12px',
+    lineHeight: 1.3,
+    fontWeight: 400
+  }
+}
