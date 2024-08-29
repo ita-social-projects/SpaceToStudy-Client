bcdd6963a98ff1ef93bf58199f0090fccabbae07
Anastasiia Matiushenko <102433497+docia@users.noreply.github.com>
2024-08-29 21:42:17 +0300

    Changed videolink type to string (#2412)
    * changed videolink type to string
    
    * fixed tests

--- tests/unit/redux/editProfileSlice.spec.js
+++ tests/unit/redux/editProfileSlice.spec.js
@@ -28,7 +28,7 @@ const userDataMock = {
     src: 'url',
     name: 'profile_photo'
   },
-  videoLink: { [UserRoleEnum.Tutor]: 'link', [UserRoleEnum.Student]: '' },
+  videoLink: { [UserRoleEnum.Tutor]: '', [UserRoleEnum.Student]: '' },
   mainSubjects: { [UserRoleEnum.Tutor]: [], [UserRoleEnum.Student]: [] },
   professionalBlock: {
     education: 'Education',
@@ -51,7 +51,7 @@ const expectedUserData = {
   city: 'New York',
   professionalSummary: 'Summary',
   nativeLanguage: 'English',
-  videoLink: { [UserRoleEnum.Tutor]: 'link', [UserRoleEnum.Student]: '' },
+  videoLink: '',
   photo: {
     src: 'url',
     name: 'profile_photo'
@@ -85,7 +85,7 @@ const initialState = {
   city: '',
   professionalSummary: '',
   nativeLanguage: '',
-  videoLink: { [UserRoleEnum.Tutor]: '', [UserRoleEnum.Student]: '' },
+  videoLink: '',
   photo: null,
   categories: { [UserRoleEnum.Tutor]: [], [UserRoleEnum.Student]: [] },
   professionalBlock: {
