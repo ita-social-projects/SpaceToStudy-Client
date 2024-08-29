00556e78a4ffd439585c13f6d0befb93658849e8
Renatavl <86105228+Renatavl@users.noreply.github.com>
2024-08-29 15:04:16 +0300

    Misaligned Character Count in TextArea (#2324)
    * Misaligned Character Count in TextArea
    
    * hot fix

--- src/components/app-text-area/AppTextArea.styles.ts
+++ src/components/app-text-area/AppTextArea.styles.ts
@@ -1,6 +1,6 @@
 export const styles = {
   container: { position: 'relative' },
-  textLength: { position: 'absolute', right: 0, bottom: 0 },
+  textLength: { position: 'absolute', left: 0, bottom: 0 },
   title: { typography: 'body2', color: 'primary.500', mr: '8px' },
   textarea: { '& .MuiInputBase-root': { p: '12.5px 14px' } }
 }
