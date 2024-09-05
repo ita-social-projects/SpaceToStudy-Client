e6def13237c5e08aa15b2a697fac0837e4d04e1e
OliaHrytsak <127191734+OliaHrytsak@users.noreply.github.com>
2024-09-05 11:03:03 +0300

    Photo step - TS bugfix (#2442)
    * photo step ts changes
    
    * fix sonar issue

--- src/containers/tutor-home-page/add-photo-step/AddPhotoStep.tsx
+++ src/containers/tutor-home-page/add-photo-step/AddPhotoStep.tsx
@@ -22,59 +22,67 @@ const AddPhotoStep: FC<AddPhotoStepProps> = ({ btnsBox }) => {
   const [photoError, setPhotoError] = useState('')
   const { t } = useTranslation()
   const { stepData, handlePhoto } = useStepContext()
-  const photo: File[] = stepData.photo as unknown as File[]
 
-  const addPhoto = ({ files, error }: UploadFileEmitterArgs) => {
-    if (files.length > 0 && files[0] instanceof File) {
-      resizeImage(files[0]).catch((err) => {
-        console.error('Error in addPhoto:', err)
-      })
+  const photo = stepData.photo
+
+  const addPhoto = async ({ files, error }: UploadFileEmitterArgs) => {
+    if (files.length && !files[0].src) {
+      await resizeImage(files[0])
+    } else {
+      handlePhoto(files)
     }
+
     setPhotoError(error)
+  }
+
+  const handleAddPhoto = (args: UploadFileEmitterArgs) => {
+    addPhoto(args).catch((err) => {
+      console.error('Error in addPhoto:', err)
+    })
   }
 
   const resizeImage = async (photo: File) => {
     const originalPhotoPath = URL.createObjectURL(photo)
+
     const photoSizes = { newWidth: 440, newHeight: 440 }
     const photoName = photo.name
     const lastModified = photo.lastModified
 
-    try {
-      const resizedPhotoBlob = await imageResize(originalPhotoPath, photoSizes)
-      const resizedPhotoFile = new File([resizedPhotoBlob], photoName, {
+    const resizedPhoto = await imageResize(originalPhotoPath, photoSizes)
+    handlePhoto([
+      {
+        ...photo,
+        src: resizedPhoto,
+        name: photoName,
         type: 'image/png',
         lastModified
-      })
-      handlePhoto([resizedPhotoFile] as unknown as string[])
-    } catch (error) {
-      console.error('Error resizing image:', error)
-    }
+      }
+    ])
   }
 
-  const photoPreview =
-    photo.length && photo[0] instanceof File ? (
-      <Box sx={style.imgContainer}>
-        <Box
-          alt={t('becomeTutor.photo.imageAlt') as unknown as string}
-          component='img'
-          src={URL.createObjectURL(photo[0])}
-          sx={style.img}
-        />
-      </Box>
-    ) : (
-      <DragAndDrop
-        emitter={addPhoto}
-        initialState={photo}
-        style={{
-          root: style.imgContainer,
-          uploadBox: style.uploadBox,
-          activeDrag: style.activeDrag
-        }}
-        validationData={validationData}
-      >
-        <Typography>{t('becomeTutor.photo.placeholder')}</Typography>
-      </DragAndDrop>
-    )
+  const photoPreview = photo?.length ? (
+    <Box sx={style.imgContainer}>
+      <Box
+        alt={t('becomeTutor.photo.imageAlt') as unknown as string}
+        component='img'
+        src={photo[0].src}
+        sx={style.img}
+      />
+    </Box>
+  ) : (
+    <DragAndDrop
+      emitter={handleAddPhoto}
+      initialState={photo}
+      style={{
+        root: style.imgContainer,
+        uploadBox: style.uploadBox,
+        activeDrag: style.activeDrag
+      }}
+      validationData={validationData}
+    >
+      <Typography>{t('becomeTutor.photo.placeholder')}</Typography>
+    </DragAndDrop>
+  )
 
   return (
     <Box sx={style.root}>
@@ -86,7 +94,7 @@ const AddPhotoStep: FC<AddPhotoStepProps> = ({ btnsBox }) => {
           </Typography>
           <FileUploader
             buttonText={t('becomeTutor.photo.button')}
-            emitter={addPhoto}
+            emitter={handleAddPhoto}
             initialError={photoError}
             initialState={photo}
             isImages
