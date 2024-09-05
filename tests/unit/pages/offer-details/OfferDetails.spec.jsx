804d9efd58e6f8e400b3aa4d6254e4215c21cff6
docia <matiushenko107@gmail.com>
2024-09-05 22:01:54 +0300

    added tests

--- tests/unit/pages/offer-details/OfferDetails.spec.jsx
+++ tests/unit/pages/offer-details/OfferDetails.spec.jsx
@@ -5,6 +5,10 @@ import OfferDetails from '~/pages/offer-details/OfferDetails'
 import useBreakpoints from '~/hooks/use-breakpoints'
 import { mockOffer } from '~tests/unit/pages/offer-details/OfferDetails.spec.constants'
 import { renderWithProviders, mockAxiosClient } from '~tests/test-utils'
+import { setField } from '~/redux/features/editProfileSlice'
+import { openAlert } from '~/redux/features/snackbarSlice'
+import { getErrorKey } from '~/utils/get-error-key'
+import { StatusEnum } from '~/types'
 
 vi.mock('~/hooks/use-breakpoints')
 vi.mock('react-router-dom', async () => {
@@ -19,6 +23,27 @@ vi.mock('react-router-dom', async () => {
     })
   }
 })
+
+vi.mock('~/redux/features/editProfileSlice', async () => {
+  const actual = await vi.importActual('~/redux/features/editProfileSlice')
+  return {
+    ...actual,
+    setField: vi.fn()
+  }
+})
+vi.mock('~/redux/features/snackbarSlice', async () => {
+  const actual = await vi.importActual('~/redux/features/snackbarSlice')
+  return {
+    ...actual,
+    openAlert: vi.fn()
+  }
+})
+
+vi.mock('~/utils/get-error-key', () => ({
+  getErrorKey: vi.fn(() => 'error-message')
+}))
+
+const dispatch = vi.fn()
 
 const desktopData = {
   isLaptopAndAbove: true,
@@ -135,6 +160,84 @@ describe('Offer details with student role', () => {
     const modalTitle = screen.getByText('offerDetailsPage.enrollOffer.title')
 
     expect(modalTitle).toBeInTheDocument()
+  })
+
+  it('should dispatch setField with the correct payload in handleResponse', () => {
+    const handleResponse = (response) => {
+      dispatch(setField({ field: 'bookmarkedOffers', value: response }))
+    }
+
+    const response = ['offer1', 'offer2']
+
+    handleResponse(response)
+
+    expect(setField).toHaveBeenCalledWith({
+      field: 'bookmarkedOffers',
+      value: response
+    })
+  })
+
+  it('should dispatch openAlert with an error message in handleResponseError', () => {
+    const handleResponseError = (error) => {
+      dispatch(
+        openAlert({
+          severity: 'error',
+          message: getErrorKey(error)
+        })
+      )
+    }
+
+    const error = { code: 400, message: 'Invalid request' }
+
+    handleResponseError(error)
+
+    expect(getErrorKey).toHaveBeenCalledWith(error)
+    expect(dispatch).toHaveBeenCalledWith(
+      openAlert({
+        severity: 'error',
+        message: 'error-message'
+      })
+    )
+  })
+
+  it('should call toggleBookmark inside onBookmarkClick()', () => {
+    const toggleBookmark = vi.fn()
+
+    const onBookmarkClick = (id) => {
+      void toggleBookmark(id)
+    }
+
+    const id = 'offerId'
+
+    onBookmarkClick(id)
+
+    expect(toggleBookmark).toHaveBeenCalledWith(id)
+  })
+
+  it('should toggle offer status in handleToggleOfferStatus', async () => {
+    const offerData = {
+      status: StatusEnum.Draft
+    }
+
+    const fetchDataUpdateOffer = vi.fn()
+    const fetchDataOffer = vi.fn()
+
+    const handleToggleOfferStatus = async () => {
+      const status =
+        offerData?.status === StatusEnum.Draft
+          ? StatusEnum.Active
+          : StatusEnum.Draft
+
+      await fetchDataUpdateOffer({ status })
+      void fetchDataOffer()
+    }
+
+    await handleToggleOfferStatus()
+
+    expect(fetchDataUpdateOffer).toHaveBeenCalledWith({
+      status: StatusEnum.Active
+    })
+    expect(fetchDataOffer).toHaveBeenCalled()
   })
 })
 
