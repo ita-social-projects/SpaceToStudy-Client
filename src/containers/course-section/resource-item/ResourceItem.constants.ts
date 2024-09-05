ee836591078175c9cc06253dcd9ec14310ae1f21
Olena Hryk <olena.andrushchakevych@gmail.com>
2024-09-05 18:14:08 +0300

    Refactored ResourceItem container

--- src/containers/course-section/resource-item/ResourceItem.constants.ts
+++ src/containers/course-section/resource-item/ResourceItem.constants.ts
@@ -1,5 +1,9 @@
 import i18n from 'i18next'
-import { ResourceAvailabilityStatusEnum } from '~/types'
+import {
+  ResourceAvailabilityStatusEnum,
+  ResourcesTypesEnum as ResourceType
+} from '~/types'
+import { resourcesData } from '~/containers/course-section/CourseSectionContainer.constants'
 
 import openIcon from '~/assets/img/cooperation-details/resource-availability/open-icon.svg'
 import openFrom from '~/assets/img/cooperation-details/resource-availability/open-from.svg'
@@ -25,4 +29,10 @@ export const availabilityIcons: Record<ResourceAvailabilityStatusEnum, string> =
     [ResourceAvailabilityStatusEnum.Open]: openIcon,
     [ResourceAvailabilityStatusEnum.OpenFrom]: openFrom,
     [ResourceAvailabilityStatusEnum.Closed]: closedIcon
+  }
+
+export const resourceIcons: Partial<Record<ResourceType, JSX.Element | null>> =
+  {
+    [ResourceType.Lesson]: resourcesData.lessons.icon,
+    [ResourceType.Quiz]: resourcesData.quizzes.icon
   }
