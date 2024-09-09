bd2b81fdfb3e8fd893cd09665a90cc19340bdbcd
Bohdan Mylyi <123481658+BohdanMylyi@users.noreply.github.com>
2024-09-09 13:18:44 +0300

    fix pagination scroll and make it smooth (#2443)

--- src/containers/find-offer/filter-bar-menu/FilterBarMenu.tsx
+++ src/containers/find-offer/filter-bar-menu/FilterBarMenu.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import React, { forwardRef } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import Box from '@mui/material/Box'
@@ -31,70 +31,78 @@ interface FilterBarMenuProps {
   filters: FindOffersFilters
 }
 
-const FilterBarMenu: FC<FilterBarMenuProps> = ({
-  chosenFiltersQty,
-  toggleFilters,
-  updateFilters,
-  filters,
-  handleOffersView,
-  onToggleTutorOffers,
-  additionalParams,
-  offersView
-}) => {
-  const { isLaptopAndAbove, isMobile } = useBreakpoints()
-
-  const { t } = useTranslation()
-
-  const translatedSwitcherOptions = {
-    left: {
-      text: t('findOffers.topMenu.tutorsOffers'),
-      tooltip: t('findOffers.contentSwitcher.switcher-tutor')
+const FilterBarMenu = forwardRef<HTMLDivElement, FilterBarMenuProps>(
+  (
+    {
+      chosenFiltersQty,
+      toggleFilters,
+      updateFilters,
+      filters,
+      handleOffersView,
+      onToggleTutorOffers,
+      additionalParams,
+      offersView
     },
-    right: {
-      text: t('findOffers.topMenu.studentsRequests'),
-      tooltip: t('findOffers.contentSwitcher.switcher-student')
+    ref
+  ) => {
+    const { isLaptopAndAbove, isMobile } = useBreakpoints()
+
+    const { t } = useTranslation()
+
+    const translatedSwitcherOptions = {
+      left: {
+        text: t('findOffers.topMenu.tutorsOffers'),
+        tooltip: t('findOffers.contentSwitcher.switcher-tutor')
+      },
+      right: {
+        text: t('findOffers.topMenu.studentsRequests'),
+        tooltip: t('findOffers.contentSwitcher.switcher-student')
+      }
     }
-  }
 
-  const handleSortBy = (value: string) => {
-    updateFilters({ ...additionalParams, sort: value })
-  }
-  const sortOptions = sortTranslationKeys.map(({ title, value }) => ({
-    title: t(title),
-    value
-  }))
+    const handleSortBy = (value: string) => {
+      updateFilters({ ...additionalParams, sort: value })
+    }
 
-  return (
-    <Box sx={isMobile ? styles.mobileContainer : styles.container}>
-      <FiltersToggle
-        chosenFiltersQty={chosenFiltersQty}
-        handleToggle={toggleFilters}
-      />
-      {isLaptopAndAbove ? (
-        <AppContentSwitcher
-          active={filters.authorRole === UserRoleEnum.Student}
-          data-testid='switch'
-          onChange={onToggleTutorOffers}
-          switchOptions={translatedSwitcherOptions}
-          typographyVariant='button'
+    const sortOptions = sortTranslationKeys.map(({ title, value }) => ({
+      title: t(title),
+      value
+    }))
+
+    return (
+      <Box ref={ref} sx={isMobile ? styles.mobileContainer : styles.container}>
+        <FiltersToggle
+          chosenFiltersQty={chosenFiltersQty}
+          handleToggle={toggleFilters}
         />
-      ) : null}
-      {!isMobile ? (
-        <Box data-testid='app-select-block' sx={styles.container}>
-          <AppSelect
-            fields={sortOptions}
-            selectTitle={t('filters.sortBy.sortByTitle')}
-            setValue={handleSortBy}
-            sx={isLaptopAndAbove ? styles.selectContainer : {}}
-            value={filters.sort}
+        {isLaptopAndAbove ? (
+          <AppContentSwitcher
+            active={filters.authorRole === UserRoleEnum.Student}
+            data-testid='switch'
+            onChange={onToggleTutorOffers}
+            switchOptions={translatedSwitcherOptions}
+            typographyVariant='button'
           />
-          {isLaptopAndAbove ? (
-            <ViewSwitcher onChange={handleOffersView} value={offersView} />
-          ) : null}
-        </Box>
-      ) : null}
-    </Box>
-  )
-}
+        ) : null}
+        {!isMobile ? (
+          <Box data-testid='app-select-block' sx={styles.container}>
+            <AppSelect
+              fields={sortOptions}
+              selectTitle={t('filters.sortBy.sortByTitle')}
+              setValue={handleSortBy}
+              sx={isLaptopAndAbove ? styles.selectContainer : {}}
+              value={filters.sort}
+            />
+            {isLaptopAndAbove ? (
+              <ViewSwitcher onChange={handleOffersView} value={offersView} />
+            ) : null}
+          </Box>
+        ) : null}
+      </Box>
+    )
+  }
+)
+
+FilterBarMenu.displayName = 'FilterBarMenu'
 
 export default FilterBarMenu
