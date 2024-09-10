a30e771414c7cf456be47c2516cf32fcb690e149
PavloDolia <paveldolia93@gmail.com>
2024-09-10 22:19:20 +0300

    fixed more tests

--- tests/unit/containers/about-chat-sidebar/AboutChatSidebar.spec.jsx
+++ tests/unit/containers/about-chat-sidebar/AboutChatSidebar.spec.jsx
@@ -4,8 +4,6 @@ import { render, screen } from '@testing-library/react'
 
 import AboutChatSidebar from '~/containers/about-chat-sidebar/AboutChatSidebar'
 
-import someAvatar from '~/assets/img/user-profile-page/avatar.png'
-
 vi.mock('simplebar-react', () => {
   return {
     default: ({ children }) => <div>{children}</div>
@@ -21,27 +19,6 @@ const mockUser = {
 }
 
 const mockMember = { user: mockUser, role: 'tutor' }
-
-const mockMedia = [
-  {
-    _id: 'm1',
-    name: 'Cool pic',
-    path: someAvatar,
-    createdAt: '2023-06-11T11:16:06.685Z',
-    updatedAt: '2023-06-11T11:16:06.685Z'
-  }
-]
-
-const mockFiles = [
-  {
-    _id: 'f1',
-    name: 'Cool book.pdf',
-    size: 13.4,
-    createdAt: '2023-08-24T11:16:06.685Z',
-    updatedAt: '2023-08-24T11:16:06.685Z',
-    url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'
-  }
-]
 
 const mockLinks = [
   {
@@ -68,8 +45,6 @@ describe('AboutChatSidebar component test', () => {
   it('should  render user information', () => {
     setup({
       member: mockMember,
-      media: [],
-      files: [],
       links: []
     })
 
@@ -78,28 +53,6 @@ describe('AboutChatSidebar component test', () => {
 
     expect(userName).toBeInTheDocument()
     expect(viewButton).toBeInTheDocument()
-  })
-
-  it('should render media', () => {
-    setup({
-      member: mockMember,
-      media: mockMedia,
-      files: [],
-      links: []
-    })
-
-    expect(screen.getByAltText('Cool pic')).toBeInTheDocument()
-  })
-
-  it('should render files', () => {
-    setup({
-      member: mockMember,
-      media: [],
-      files: mockFiles,
-      links: []
-    })
-
-    expect(screen.getByText('Cool book.pdf')).toBeInTheDocument()
   })
 
   it('should render links', () => {
