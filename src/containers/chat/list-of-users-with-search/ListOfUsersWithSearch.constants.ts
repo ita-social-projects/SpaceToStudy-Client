bcf01e4d20433a4b170e76dfba6291b01b1c6ffa
Bohdan Mylyi <mylyibohdan18@gmail.com>
2024-09-14 15:03:27 +0300

    implement function for sorting chats by date

--- src/containers/chat/list-of-users-with-search/ListOfUsersWithSearch.constants.ts
+++ src/containers/chat/list-of-users-with-search/ListOfUsersWithSearch.constants.ts
@@ -7,13 +7,22 @@ export const isCorrectUser = (item: ChatResponse, userId: string): boolean => {
   return correctUser.length > 0
 }
 
+const sortChatsByDate = (listOfChats: ChatResponse[]): ChatResponse[] => {
+  return listOfChats.sort((a, b) => {
+    const dateA = new Date(a.latestMessage.createdAt).getTime()
+    const dateB = new Date(b.latestMessage.createdAt).getTime()
+    return dateB - dateA
+  })
+}
+
 export const filterChats = (
   listOfChats: ChatResponse[],
   userId: string,
   search: string
 ): ChatResponse[] => {
+  const sortedChats = sortChatsByDate(listOfChats)
   const lowerCaseSearch = search.toLocaleLowerCase()
-  return listOfChats.filter((item) => {
+  return sortedChats.filter((item) => {
     if (!isCorrectUser(item, userId)) return false
 
     const { firstName, lastName } = item.members[0].user
