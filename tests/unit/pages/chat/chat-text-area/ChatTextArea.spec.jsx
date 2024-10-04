dea9fe836b4725e9b2066df2f6a76bd3161f896a
YaroslavChuiko <jarik.tchuicko@gmail.com>
2024-10-04 19:54:52 +0300

    fixed tests

--- tests/unit/pages/chat/chat-text-area/ChatTextArea.spec.jsx
+++ tests/unit/pages/chat/chat-text-area/ChatTextArea.spec.jsx
@@ -8,13 +8,13 @@ import { renderWithProviders } from '~tests/test-utils'
 
 vi.mock('~/hooks/use-breakpoints')
 
-global.window.matchMedia = vi.fn().mockImplementation((query) => ({
+vi.stubGlobal('matchMedia', (query) => ({
   matches: false,
   media: query,
   onchange: null,
-  addListener: vi.fn(),
-  removeListener: vi.fn()
-}))
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+}));
 
 global.IntersectionObserver = vi.fn().mockImplementation(() => ({
   observe: vi.fn(),
@@ -76,7 +76,8 @@ describe('ChatTextArea component test', () => {
 
     const input = screen.getByLabelText('Enter some text')
 
-    await waitFor(() => user.type(input, 'new message'))
+    // await waitFor(() => user.type(input, 'new message'))
+    await user.type(input, 'new message')
 
     expect(input.value).toBe('new message')
 
