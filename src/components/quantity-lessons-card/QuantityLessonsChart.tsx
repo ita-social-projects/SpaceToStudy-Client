6b894f3e65bafea988b7a714bbdcceae84157d32
renata_vl <renata.vladyka1@gmail.com>
2024-10-14 21:01:38 +0300

    Block with statistics by lessons quantity

---
+++ src/components/quantity-lessons-card/QuantityLessonsChart.tsx
@@ -0,0 +1,95 @@
+import React, { useMemo } from 'react'
+import { Bar } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Box } from '@mui/material'
+import useBreakpoints from '~/hooks/use-breakpoints'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+const data = {
+  labels: [
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC'
+  ],
+  datasets: [
+    {
+      label: 'Lessons',
+      data: [5, 5, 10, 10, 12, 15, 14, 14, 10, 10, 12, 12],
+      backgroundColor: '#79B260',
+      borderColor: '#79B260',
+      borderWidth: 1,
+      barThickness: 28
+    }
+  ]
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: false
+    }
+  },
+  scales: {
+    x: {
+      grid: {
+        display: false
+      },
+      ticks: {
+        color: '#455A64',
+        weight: 'normal'
+      },
+      border: {
+        display: false
+      }
+    },
+    y: {
+      beginAtZero: true,
+      max: 15,
+      ticks: {
+        stepSize: 5
+      },
+      border: {
+        display: false
+      }
+    }
+  }
+}
+
+const QuantityLessonsChart = () => {
+  const { isMobile } = useBreakpoints()
+
+  const dataResponsive = useMemo(() => {
+    const obj = data
+    obj.datasets[0].barThickness = isMobile ? 20 : 28
+
+    return obj
+  }, [isMobile])
+
+  return (
+    <Box height={'181px'} key={+isMobile}>
+      <Bar data={dataResponsive} options={options} />
+    </Box>
+  )
+}
+
+export default QuantityLessonsChart
