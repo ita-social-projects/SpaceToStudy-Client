2dbdf00cee7ddd69839e5336639080d58b609e1b
Valentyna Dudchak <dudchak.valya@gmail.com>
2024-10-23 21:48:25 +0300

    added tests for CreateOrEditOffer

--- tests/unit/containers/offer-page/create-or-edit-offer/CreateOrEditOffer.spec.jsx
+++ tests/unit/containers/offer-page/create-or-edit-offer/CreateOrEditOffer.spec.jsx
@@ -3,10 +3,12 @@ import { renderWithProviders } from '~tests/test-utils'
 import CreateOrEditOffer from '~/containers/offer-page/create-or-edit-offer/CreateOrEditOffer'
 import { openAlert } from '~/redux/features/snackbarSlice'
 import { snackbarVariants } from '~/constants'
+import { expect } from 'vitest'
 
 const mockDispatch = vi.fn()
 const mockCloseDrawer = vi.fn()
 const mockService = vi.fn()
+const mockNavigate = vi.fn()
 
 vi.mock('~/redux/features/snackbarSlice', async () => {
   const actual = await vi.importActual('~/redux/features/snackbarSlice')
@@ -25,6 +27,12 @@ vi.mock('~/hooks/use-redux', async () => {
   }
 })
 
+vi.mock('react-router-dom', async () => ({
+  ...(await vi.importActual('react-router-dom')),
+  useNavigate: () => mockNavigate
+}))
+
+
 describe('CreateOrEditOffer', () => {
   beforeEach(() => {
     renderWithProviders(
@@ -37,15 +45,41 @@ describe('CreateOrEditOffer', () => {
     mockDispatch.mockReset()
   })
 
-  it('should call a dispatch on successful response', async () => {
+  it('should call a dispatch and navigate on successful response', () => {
     const saveButton = screen.getByRole('button', {name: /offerPage.createOffer.buttonTitles.tutor/i})
     fireEvent.click(saveButton)
 
-    await waitFor(() => {
-      expect(openAlert).toHaveBeenCalledWith({
-        severity: snackbarVariants.success,
-        message: 'offerPage.createOffer.successMessage'
-      })
+    waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledWith(openAlert({
+          severity: snackbarVariants.success,
+          message: 'offerPage.createOffer.successMessage'
+        }
+      ))
+      expect(mockCloseDrawer).toHaveBeenCalled()
+      expect(mockNavigate).toHaveBeenCalledWith('/offer-details')
+    })
+  })
+
+  it('should call different dispatch and navigate with #offer on successful response', () => {
+    vi.mock('react-router-dom', async () => ({
+      ...(await vi.importActual('react-router-dom')),
+      useLocation: () => ({hash: '#offer'})
+    }))
+    
+    const saveButton = screen.getByRole('button', {name: /offerPage.createOffer.buttonTitles.tutor/i})
+    fireEvent.click(saveButton)
+
+    waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledWith(openAlert({
+          severity: snackbarVariants.success,
+          message: 'offerPage.createOffer.extendedSuccessMessage.tutor',
+          duration: 10000,
+          isExtended: true,
+          route: '/my-offers'
+        }
+      ))
+      expect(mockCloseDrawer).toHaveBeenCalled()
+      expect(mockNavigate).toHaveBeenCalledWith('/my-profile#complete')
     })
   })
 })
