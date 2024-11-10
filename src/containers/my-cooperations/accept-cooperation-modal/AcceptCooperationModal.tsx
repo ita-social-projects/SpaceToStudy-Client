2bd516074834c49df6da526c55082117830562f1
PavloDolia <paveldolia93@gmail.com>
2024-11-10 11:27:48 +0200

    fixed cooperation containers

--- src/containers/my-cooperations/accept-cooperation-modal/AcceptCooperationModal.tsx
+++ src/containers/my-cooperations/accept-cooperation-modal/AcceptCooperationModal.tsx
@@ -19,202 +19,202 @@ import { cooperationService } from '~/services/cooperation-service'
 import { OfferService } from '~/services/offer-service'
 
 import {
-  ButtonTypeEnum,
-  ComponentEnum,
-  type Cooperation,
-  StatusEnum,
-  type UpdateCooperationsParams
+    ButtonTypeEnum,
+    ComponentEnum,
+    type Cooperation,
+    StatusEnum,
+    type UpdateCooperationsParams
 } from '~/types'
 import { snackbarVariants } from '~/constants'
 import { styles } from '~/containers/my-cooperations/accept-cooperation-modal/AcceptCooperation.styles'
 import useSnackbarAlert from '~/hooks/use-snackbar-alert'
 
 interface AcceptCooperationModalProps {
-  cooperation: Cooperation
+    cooperation: Cooperation
 }
 
 const AcceptCooperationModal: React.FC<AcceptCooperationModalProps> = ({
-  cooperation
+    cooperation
 }) => {
-  const { t } = useTranslation()
-  const { isDesktop } = useBreakpoints()
-  const { closeModal } = useModalContext()
-  const { checkConfirmation } = useConfirm()
-  const { handleAlert, handleErrorAlert } = useSnackbarAlert()
-  const [minPrice, maxPrice] = minMaxPrice(cooperation.offer.price, 0.25)
+    const { t } = useTranslation()
+    const { isDesktop } = useBreakpoints()
+    const { closeModal } = useModalContext()
+    const { checkConfirmation } = useConfirm()
+    const { handleAlert, handleErrorAlert } = useSnackbarAlert()
+    const [minPrice, maxPrice] = minMaxPrice(cooperation.offer.price, 0.25)
 
-  const needAction = cooperation.user.role !== cooperation.needAction.role
+    const needAction = cooperation.user.role !== cooperation.needAction.role
 
-  const handleUpdateCooperation = useCallback(
-    (params: Omit<UpdateCooperationsParams, '_id'>) => {
-      return cooperationService.updateCooperation({
-        _id: cooperation._id,
-        ...params
-      })
-    },
-    [cooperation._id]
-  )
-
-  const handleUpdateOffer = useCallback(
-    () =>
-      OfferService.updateOfferWithBaseService(cooperation.offer._id, {
-        enrolledUsers: []
-      }),
-    [cooperation.offer._id]
-  )
-
-  const onResponse = () => {
-    handleAlert({
-      severity: snackbarVariants.success,
-      message: 'cooperationsPage.acceptModal.successMessage'
-    })
-
-    closeModal()
-  }
-
-  const { isPending: isUpdateCooperationPending, mutate: updateCooperation } =
-    useMutation({
-      mutationFn: handleUpdateCooperation,
-      queryKey: ['cooperations'],
-      onSuccess: onResponse,
-      onError: handleErrorAlert
-    })
-
-  const { isPending: updateLoading, mutate: updateOffer } = useMutation({
-    mutationFn: handleUpdateOffer,
-    queryKey: ['cooperations'],
-    onError: handleErrorAlert
-  })
-
-  const handleAcceptCooperation = useCallback(async () => {
-    const confirmed = await checkConfirmation({
-      message: t('cooperationsPage.acceptModal.confirm.accept', {
-        price: cooperation.price
-      }),
-      title: 'titles.confirmTitle',
-      check: true
-    })
-
-    if (confirmed) {
-      updateCooperation({ status: StatusEnum.Active })
-    }
-  }, [checkConfirmation, cooperation.price, t, updateCooperation])
-
-  const handleDeclineCooperation = useCallback(async () => {
-    const confirmed = await checkConfirmation({
-      message: t('cooperationsPage.acceptModal.confirm.decline'),
-      title: 'titles.confirmTitle',
-      check: true
-    })
-
-    if (confirmed) {
-      updateOffer()
-      updateCooperation({ status: StatusEnum.Closed })
-    }
-  }, [checkConfirmation, updateOffer, t, updateCooperation])
-
-  const handleResendCooperation = useCallback(
-    async (data?: Record<'price', number>) => {
-      if (!data) {
-        return
-      }
-
-      const confirmed = await checkConfirmation({
-        message: t('cooperationsPage.acceptModal.confirm.resend', {
-          price: data.price
-        }),
-        title: 'titles.confirmTitle',
-        check: true
-      })
-
-      if (confirmed) {
-        updateCooperation({ price: data.price })
-      }
-    },
-    [checkConfirmation, t, updateCooperation]
-  )
-
-  const { isDirty, handleNonInputValueChange, handleSubmit } = useForm({
-    initialValues: { price: cooperation.price },
-    onSubmit: handleResendCooperation,
-    submitWithData: true
-  })
-
-  const handlePriceChange = (value: number) => {
-    handleNonInputValueChange('price', value)
-  }
-
-  const onCooperationAccept = () => void handleAcceptCooperation()
-
-  const acceptButtonText = isDirty
-    ? 'cooperationsPage.acceptModal.resend'
-    : 'cooperationsPage.acceptModal.accept'
-  const acceptButtonType = isDirty
-    ? ButtonTypeEnum.Submit
-    : ButtonTypeEnum.Button
-
-  const buttons =
-    isUpdateCooperationPending || updateLoading ? (
-      <Loader size={50} />
-    ) : (
-      <Box sx={styles.buttonRow}>
-        {needAction && (
-          <>
-            <Button onClick={onCooperationAccept} type={acceptButtonType}>
-              {t(acceptButtonText)}
-            </Button>
-            <Button
-              onClick={() => void handleDeclineCooperation()}
-              variant='tonal'
-            >
-              {t('cooperationsPage.acceptModal.decline')}
-            </Button>
-          </>
-        )}
-      </Box>
+    const handleUpdateCooperation = useCallback(
+        (params: Omit<UpdateCooperationsParams, '_id'>) => {
+            return cooperationService.updateCooperation({
+                _id: cooperation._id,
+                ...params
+            })
+        },
+        [cooperation._id]
     )
 
-  return (
-    <Box sx={styles.root}>
-      {isDesktop && (
-        <CooperationCard cooperation={cooperation} sx={styles.card} />
-      )}
-      <Box
-        component={ComponentEnum.Form}
-        onSubmit={handleSubmit}
-        sx={styles.form}
-      >
-        <Typography sx={styles.title}>
-          {t('cooperationsPage.acceptModal.title')}
-        </Typography>
-        <TitleWithDescription
-          description={cooperation.proficiencyLevel}
-          style={styles.titleDescription}
-          title={t('cooperationsPage.acceptModal.level')}
-        />
-        <TitleWithDescription
-          description={`${cooperation.offer.price} ${t('common.uah')}`}
-          style={styles.titleDescription}
-          title={t('cooperationsPage.acceptModal.price')}
-        />
-        <TitleWithDescription
-          description={`${cooperation.price} ${t('common.uah')}`}
-          style={styles.titleDescription}
-          title={t('cooperationsPage.acceptModal.suggested')}
-        />
-        {needAction && (
-          <SliderWithInput
-            defaultValue={cooperation.price}
-            max={maxPrice}
-            min={minPrice}
-            onChange={handlePriceChange}
-            title={t('cooperationsPage.acceptModal.riseOrLow')}
-          />
-        )}
-        <Box sx={styles.buttons}>{buttons}</Box>
-      </Box>
-    </Box>
-  )
+    const handleUpdateOffer = useCallback(
+        () =>
+            OfferService.updateOfferWithBaseService(cooperation.offer._id, {
+                enrolledUsers: []
+            }),
+        [cooperation.offer._id]
+    )
+
+    const onResponse = () => {
+        handleAlert({
+            severity: snackbarVariants.success,
+            message: 'cooperationsPage.acceptModal.successMessage'
+        })
+
+        closeModal()
+    }
+
+    const { isPending: isUpdateCooperationPending, mutate: updateCooperation } =
+        useMutation({
+            mutationFn: handleUpdateCooperation,
+            queryKey: ['cooperations'],
+            onSuccess: onResponse,
+            onError: handleErrorAlert
+        })
+
+    const { isPending: updateLoading, mutate: updateOffer } = useMutation({
+        mutationFn: handleUpdateOffer,
+        queryKey: ['cooperations'],
+        onError: handleErrorAlert
+    })
+
+    const handleAcceptCooperation = useCallback(async () => {
+        const confirmed = await checkConfirmation({
+            message: t('cooperationsPage.acceptModal.confirm.accept', {
+                price: cooperation.price
+            }),
+            title: 'titles.confirmTitle',
+            check: true
+        })
+
+        if (confirmed) {
+            updateCooperation({ status: StatusEnum.Active })
+        }
+    }, [checkConfirmation, cooperation.price, t, updateCooperation])
+
+    const handleDeclineCooperation = useCallback(async () => {
+        const confirmed = await checkConfirmation({
+            message: t('cooperationsPage.acceptModal.confirm.decline'),
+            title: 'titles.confirmTitle',
+            check: true
+        })
+
+        if (confirmed) {
+            updateOffer()
+            updateCooperation({ status: StatusEnum.Closed })
+        }
+    }, [checkConfirmation, updateOffer, t, updateCooperation])
+
+    const handleResendCooperation = useCallback(
+        async (data?: Record<'price', number>) => {
+            if (!data) {
+                return
+            }
+
+            const confirmed = await checkConfirmation({
+                message: t('cooperationsPage.acceptModal.confirm.resend', {
+                    price: data.price
+                }),
+                title: 'titles.confirmTitle',
+                check: true
+            })
+
+            if (confirmed) {
+                updateCooperation({ price: data.price })
+            }
+        },
+        [checkConfirmation, t, updateCooperation]
+    )
+
+    const { isDirty, handleNonInputValueChange, handleSubmit } = useForm({
+        initialValues: { price: cooperation.price },
+        onSubmit: handleResendCooperation,
+        submitWithData: true
+    })
+
+    const handlePriceChange = (value: number) => {
+        handleNonInputValueChange('price', value)
+    }
+
+    const onCooperationAccept = () => void handleAcceptCooperation()
+
+    const acceptButtonText = isDirty
+        ? 'cooperationsPage.acceptModal.resend'
+        : 'cooperationsPage.acceptModal.accept'
+    const acceptButtonType = isDirty
+        ? ButtonTypeEnum.Submit
+        : ButtonTypeEnum.Button
+
+    const buttons =
+        isUpdateCooperationPending || updateLoading ? (
+            <Loader size={50} />
+        ) : (
+            <Box sx={styles.buttonRow}>
+                {needAction && (
+                    <>
+                        <Button onClick={onCooperationAccept} type={acceptButtonType}>
+                            {t(acceptButtonText)}
+                        </Button>
+                        <Button
+                            onClick={() => void handleDeclineCooperation()}
+                            variant='tonal'
+                        >
+                            {t('cooperationsPage.acceptModal.decline')}
+                        </Button>
+                    </>
+                )}
+            </Box>
+        )
+
+    return (
+        <Box sx={styles.root}>
+            {isDesktop && (
+                <CooperationCard cooperation={cooperation} sx={styles.card} />
+            )}
+            <Box
+                component={ComponentEnum.Form}
+                onSubmit={handleSubmit}
+                sx={styles.form}
+            >
+                <Typography sx={styles.title}>
+                    {t('cooperationsPage.acceptModal.title')}
+                </Typography>
+                <TitleWithDescription
+                    description={cooperation.proficiencyLevel}
+                    style={styles.titleDescription}
+                    title={t('cooperationsPage.acceptModal.level')}
+                />
+                <TitleWithDescription
+                    description={`${cooperation.price} ${t('common.uah')}`}
+                    style={styles.titleDescription}
+                    title={t('cooperationsPage.acceptModal.price')}
+                />
+                <TitleWithDescription
+                    description={`${cooperation.price} ${t('common.uah')}`}
+                    style={styles.titleDescription}
+                    title={t('cooperationsPage.acceptModal.suggested')}
+                />
+                {needAction && (
+                    <SliderWithInput
+                        defaultValue={cooperation.price}
+                        max={maxPrice}
+                        min={minPrice}
+                        onChange={handlePriceChange}
+                        title={t('cooperationsPage.acceptModal.riseOrLow')}
+                    />
+                )}
+                <Box sx={styles.buttons}>{buttons}</Box>
+            </Box>
+        </Box>
+    )
 }
 
 export default AcceptCooperationModal
