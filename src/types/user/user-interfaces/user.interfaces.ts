662856f7e52418475044e52f0b57cf01c4135b8e
markgol777 <markgol777@gmail.com>
2024-11-12 21:53:40 +0100

    converted ResetPassword component from jsx to tsx

--- src/types/user/user-interfaces/user.interfaces.ts
+++ src/types/user/user-interfaces/user.interfaces.ts
@@ -103,6 +103,15 @@ export interface ChangePasswordParams {
   confirmPassword: string
 }
 
+export interface NewPassword {
+  confirmPassword?: string
+  password: string
+}
+
+export interface SendResetPasswordPayload {
+  password: string
+}
+
 export interface SignupResponse {
   userId: string
   userEmail: string
