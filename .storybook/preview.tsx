f860081cb2697fc98123af6053520b5215477609
Mike-Popovych <volt251988@gmail.com>
2024-11-15 16:19:39 +0200

    resolved conflicts

---
+++ .storybook/preview.tsx
@@ -0,0 +1,34 @@
+import { StyledEngineProvider } from '@mui/material/styles';
+import React from 'react'
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../src/styles/app-theme/custom-mui.styles'
+import i18n from '../src/plugins/i18n'
+import { I18nextProvider } from 'react-i18next'
+import type { Preview } from '@storybook/react'
+
+import '../src/styles/index.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <I18nextProvider i18n={i18n}>
+            <Story />
+          </I18nextProvider>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    )
+  ],
+}
+
+export default preview
+
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ['src/assets/*', 'src/stories/assets/*']
+    ignores: ['src/assets/*', 'src/stories/assets/*', '.storybook/**/*']
   },
   ...compat.extends(
     'eslint:recommended',
