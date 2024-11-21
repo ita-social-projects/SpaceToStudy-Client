01f6d42b05d536172fc20177fcfef84ae2f4e01b
renata_vl <renata.vladyka1@gmail.com>
2024-11-22 00:02:58 +0200

    hot fix

--- src/containers/guest-home-page/login-form/LoginForm.tsx
+++ src/containers/guest-home-page/login-form/LoginForm.tsx
@@ -54,6 +54,11 @@ const LoginForm: React.FC<LoginFormProps> = ({
     openModal({ component: <ForgotPassword /> })
   }
 
+  const isDisabled =
+    !data.email ||
+    !data.password ||
+    !Object.values(errors).every((elem) => elem === '')
+
   return (
     <Box component='form' onSubmit={handleSubmit} sx={styles.form}>
       <AppTextField
@@ -108,11 +113,7 @@ const LoginForm: React.FC<LoginFormProps> = ({
       </Box>
 
       <AppButton
-        disabled={
-          !data.email ||
-          !data.password ||
-          !Object.values(errors).every((elem) => elem === '')
-        }
+        disabled={isDisabled}
         loading={authLoading}
         sx={styles.loginButton}
         type='submit'
