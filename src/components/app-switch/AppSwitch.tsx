ce7810719216c88549774f89facd6d659251eb25
Zhayvoronok Kateryna <1gayv425@gmail.com>
2024-11-21 13:11:18 +0200

    Added unit tests and added AppSwitch to storybook

--- src/components/app-switch/AppSwitch.tsx
+++ src/components/app-switch/AppSwitch.tsx
@@ -1,35 +1,30 @@
 import { SizeEnum } from '~/types'
 import Switch, { SwitchProps } from '@mui/material/Switch'
-import { styles } from './AppSwitch.styles'
+import { switchStyles, formLabelStyles } from './AppSwitch.styles'
 import { FormControlLabel } from '@mui/material'
 interface AppSwitchProps extends Omit<SwitchProps, 'size'> {
   labelPosition?: 'start' | 'end' | 'top' | 'bottom'
   size?: SizeEnum
-  label: string
+  label?: string
   loading?: boolean
 }
 export const AppSwitch = ({
-  labelPosition,
-  size = SizeEnum.Medium,
+  labelPosition = 'end',
+  size = SizeEnum.Small,
   label,
   loading,
   disabled,
   ...props
 }: AppSwitchProps) => {
-  const sizeStyle = styles[size]
+  const sizeStyle = switchStyles[size]
   return (
     <FormControlLabel
       control={
         <Switch disabled={loading || disabled} sx={sizeStyle} {...props} />
       }
-      label={label}
+      label={label ? label : ''}
       labelPlacement={labelPosition}
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: '10px',
-        overflow: 'visible'
-      }}
+      sx={formLabelStyles.formLabelBox}
     />
   )
 }
