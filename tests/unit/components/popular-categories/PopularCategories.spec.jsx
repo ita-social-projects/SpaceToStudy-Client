9c471d490e8bab2c8c0c75b899f35fba136e3269
Zhayvoronok Kateryna <1gayv425@gmail.com>
2024-11-25 15:16:38 +0200

    Modified tests PopularCategories

--- tests/unit/components/popular-categories/PopularCategories.spec.jsx
+++ tests/unit/components/popular-categories/PopularCategories.spec.jsx
@@ -2,7 +2,14 @@ import { screen, waitFor } from '@testing-library/react'
 import PopularCategories from '~/components/popular-categories/PopularCategories'
 import { URLs } from '~/constants/request'
 import { renderWithProviders, mockAxiosClient } from '~tests/test-utils'
+import { useTranslation } from 'react-i18next'
+const { t } = useTranslation()
 
+vi.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key, options) => options?.defaultValue || key.split('.').pop(),
+  }),
+}));
 const items = [
   {
     _id: '1',
@@ -47,7 +54,7 @@ describe('PopularCategories', () => {
   })
 
   it('render card correctly', async () => {
-    const card = await screen.findByText('Math')
+    const card = await screen.findByText(t('categories.Math', { defaultValue: 'Math' }))
 
     expect(card).toBeInTheDocument()
   })
