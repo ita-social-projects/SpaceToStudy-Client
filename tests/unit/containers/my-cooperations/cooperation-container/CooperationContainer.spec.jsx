c1410874d502c25b0d8acaa2ec42d0ff2dbc6fe0
nebby2105 <lopatkaoa@gmail.com>
2024-11-28 19:23:20 +0200

    add test for CooperationContainer

--- tests/unit/containers/my-cooperations/cooperation-container/CooperationContainer.spec.jsx
+++ tests/unit/containers/my-cooperations/cooperation-container/CooperationContainer.spec.jsx
@@ -1,7 +1,10 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import { renderWithProviders } from '~tests/test-utils'
 import CooperationContainer from '~/containers/my-cooperations/cooperations-container/CooperationContainer'
 import { mockedCoop } from '~tests/unit/containers/my-cooperations/MyCooperations.spec.constants'
+import { vi } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import { StatusEnum } from '~/types'
 
 const filterOptionsMock = {
   filters: {
@@ -17,6 +20,13 @@ const preloadedState = {
   socket: { usersOnline: [] }
 }
 
+const navigateMock = vi.fn()
+
+vi.mock('react-router-dom', async () => ({
+  ...(await vi.importActual('react-router-dom')),
+  useNavigate: () => navigateMock
+}))
+
 describe('CooperationContainer component ', () => {
   it('should render card in container', () => {
     renderWithProviders(
@@ -30,5 +40,23 @@ describe('CooperationContainer component ', () => {
     const level = screen.getByText(mockedCoop.proficiencyLevel)
 
     expect(level).toBeInTheDocument()
+  })
+
+  it('navigates to cooperation detail for Active status', async () => {
+    const activeCoop = { ...mockedCoop, status: StatusEnum.Active }
+    renderWithProviders(
+      <CooperationContainer
+        filterOptions={filterOptionsMock}
+        items={[activeCoop]}
+      />,
+      { preloadedState }
+    )
+
+    const card = screen.getByText(activeCoop.offer.subject.name)
+    userEvent.click(card)
+
+    await waitFor(() => {
+      expect(navigateMock).toHaveBeenCalledWith(`./${activeCoop._id}`)
+    })
   })
 })
