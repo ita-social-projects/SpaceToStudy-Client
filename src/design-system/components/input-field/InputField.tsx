3bc7fa178a7b0eafd1ec5b641d2835d47f2b9523
unknown <pavlobutynez@gmail.com>
2024-11-28 16:05:36 +0200

    rewrote class names using the cn function

--- src/design-system/components/input-field/InputField.tsx
+++ src/design-system/components/input-field/InputField.tsx
@@ -3,6 +3,7 @@ import { InputFieldVariantEnum } from '~/types'
 import SearchIcon from '@mui/icons-material/Search'
 import ClearIcon from '@mui/icons-material/Clear'
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline'
+import { cn } from '~/utils/cn'
 
 import { InputBaseProps } from '@mui/material/InputBase'
 
@@ -40,9 +41,10 @@ const InputField: React.FC<InputFieldProps> = ({
   return (
     <Box sx={sx as SxProps}>
       <div
-        className={`s2s-input-container s2s-input-container_${variant} ${
-          disabled ? 's2s-input-container_disabled' : ''
-        } ${error ? 's2s-input-container_error' : ''}`}
+        className={cn('s2s-input-container', `s2s-input-container_${variant}`, {
+          's2s-input-container_disabled': disabled,
+          's2s-input-container_error': error
+        })}
       >
         {search && <SearchIcon className='s2s-search-icon' />}
 
@@ -62,8 +64,12 @@ const InputField: React.FC<InputFieldProps> = ({
         )}
       </div>
       {helperText && (
-        <div className={`s2s-helper-text-container`}>
-          <span className={error ? 's2s-helper-text-error' : 's2s-helper-text'}>
+        <div className='s2s-helper-text-container'>
+          <span
+            className={cn('s2s-helper-text', {
+              's2s-helper-text-error': error
+            })}
+          >
             {helperText}
           </span>
         </div>
