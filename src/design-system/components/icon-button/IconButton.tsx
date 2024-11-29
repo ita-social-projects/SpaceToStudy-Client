e621e3519b085073ea72a0bf2e0bea40f0714301
Zhayvoronok Kateryna <1gayv425@gmail.com>
2024-11-29 15:37:35 +0200

    Added all styles

--- src/design-system/components/icon-button/IconButton.tsx
+++ src/design-system/components/icon-button/IconButton.tsx
@@ -1,40 +1,63 @@
-import { IconButtonProps } from '@mui/material'
-import React from 'react'
-import { IconButtonVariant, SizeEnum } from '~/types'
-import AddIcon from '@mui/icons-material/Add'
+import { CircularProgress, IconButtonProps } from '@mui/material'
+import React, { useState } from 'react'
+import { IconButtonVariant } from '~/types'
+import AddRoundedIcon from '@mui/icons-material/AddRounded'
 import { IconButton as MuiIconButton } from '@mui/material'
 import './IconButton.scss'
 interface S2SIconButtonProps extends Omit<IconButtonProps, 'size'> {
   variant?: IconButtonVariant
-  size?: SizeEnum.Small | SizeEnum.Medium | SizeEnum.Large | SizeEnum.ExtraLarge
+  size?: 'xs' | 'sm' | 'md' | 'lg'
   loading?: boolean
   disabled?: boolean
+  toggleAble?: boolean
 }
 
 export const IconButton: React.FC<S2SIconButtonProps> = ({
   variant = IconButtonVariant.Primary,
-  size = SizeEnum.ExtraLarge,
+  size = 'md',
   loading = false,
   disabled = false,
+  toggleAble = false,
   ...props
 }) => {
+  const [isToggled, setIsToggled] = useState<boolean>(false)
+  const handleClick = () => {
+    if (toggleAble) {
+      setIsToggled((prevState) => !prevState)
+    }
+  }
   const classNamesContainerIconBG = [
     's2s-icon-button',
     `s2s-icon-button--${size}`,
-    `s2s-icon-button--${variant}`
+    `s2s-icon-button--${variant}${isToggled ? '-toggle-able' : ''}`
   ].join(' ')
   const classNamesContainerIcon = [
     's2s-icon',
     `s2s-icon--${size}`,
-    `s2s-icon--${variant}`
+    `s2s-icon--${variant}${isToggled ? '-toggle-able' : ''}`
   ].join(' ')
+  const loaderSizes = {
+    xs: 12,
+    sm: 16,
+    md: 20,
+    lg: 24
+  }
+
+  const loader = (
+    <CircularProgress data-testid='loader' size={loaderSizes[size]} />
+  )
   return (
     <MuiIconButton
       className={classNamesContainerIconBG}
       disabled={loading || disabled}
+      onClick={handleClick}
       {...props}
     >
-      <AddIcon className={classNamesContainerIcon} />
+      {loading ? (
+        loader
+      ) : (
+        <AddRoundedIcon className={classNamesContainerIcon} />
+      )}
     </MuiIconButton>
   )
 }
