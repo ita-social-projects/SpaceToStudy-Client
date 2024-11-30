bd5bfe88bd3fae78e96feaa09db703140a580982
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2024-11-30 22:12:25 +0200

    Fixed tests

--- tests/unit/containers/offer-page/create-or-edit-offer/CreateOrEditOffer.spec.jsx
+++ tests/unit/containers/offer-page/create-or-edit-offer/CreateOrEditOffer.spec.jsx
@@ -89,7 +89,9 @@ describe('CreateOrEditOffer', () => {
         })
       )
       expect(mockCloseDrawer).toHaveBeenCalled()
-      expect(mockNavigate).toHaveBeenCalledWith('/offer-details')
+      expect(mockNavigate).toHaveBeenCalledWith(
+        expect.stringMatching(/^\/offer-details/)
+      )
     })
   })
 
@@ -116,6 +118,24 @@ describe('CreateOrEditOffer', () => {
       )
       expect(mockCloseDrawer).toHaveBeenCalled()
       expect(mockNavigate).toHaveBeenCalledWith('/my-profile#complete')
+    })
+  })
+
+  it('should not call a dispatch and navigate on rejected value', async () => {
+    const { OfferService } = await import('~/services/offer-service')
+    OfferService.createOffer.mockRejectedValue({})
+
+    const saveButton = screen.getByRole('button', {
+      name: /offerPage.createOffer.buttonTitles.tutor/i
+    })
+    fireEvent.click(saveButton)
+
+    waitFor(() => {
+      expect(mockDispatch).not.toHaveBeenCalled()
+      expect(mockCloseDrawer).not.toHaveBeenCalled()
+      expect(mockNavigate).not.toHaveBeenCalledWith(
+        expect.stringMatching(/^\/offer-details/)
+      )
     })
   })
 })
