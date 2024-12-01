242327b3c0e0202756539242076ba2b9f92688dc
nebby2105 <lopatkaoa@gmail.com>
2024-12-01 19:43:55 +0200

    add badge and stories

---
+++ src/design-system/components/badge/Badge.tsx
@@ -0,0 +1,39 @@
+import { Badge as MuiBadge, BadgeProps as MuiBadgeProps } from '@mui/material'
+import { FC } from 'react'
+
+type BadgeColor = 'primary' | 'success' | 'error'
+
+type SmallBadgeProps = {
+  variant: 'sm'
+  color?: BadgeColor
+  isVisible?: boolean
+}
+
+type LargeBadgeProps = {
+  variant: 'lg'
+  badgeContent: number
+  maxContent?: number
+  color?: BadgeColor
+  isVisible?: boolean
+}
+
+type BadgeProps = (LargeBadgeProps | SmallBadgeProps) &
+  Omit<MuiBadgeProps, 'variant'>
+
+const Badge: FC<BadgeProps> = ({ children, ...props }) => {
+  const displayBadge = (props.isVisible ?? true) ? props.badgeContent : 0
+  const badgeVariant = props.variant === 'sm' ? 'dot' : 'standard'
+  return (
+    <MuiBadge
+      badgeContent={displayBadge}
+      color={props.color}
+      max={props.variant === 'lg' ? (props.maxContent ?? 10) : undefined}
+      overlap='circular'
+      variant={badgeVariant}
+    >
+      {children}
+    </MuiBadge>
+  )
+}
+
+export default Badge
---
+++ src/design-system/stories/Badge.stories.tsx
@@ -0,0 +1,157 @@
+import { IconButton } from '@mui/material'
+import type { Meta, StoryObj } from '@storybook/react'
+import Badge from '~scss-components/badge/Badge'
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded'
+
+const meta: Meta<typeof Badge> = {
+  title: 'Components/Badge',
+  component: Badge,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+The \`Badge\` component is a versatile and reusable component designed to enhance your application's UI by displaying notifications, counts, or statuses. Built on top of Material-UI's Badge, this custom implementation ensures consistency with your design system while adding additional customization options.
+
+#### Key Features:
+
+- **Variants:** 
+  - \`sm\`: Displays the badge as a small dot (ideal for simple status indicators).
+  - \`lg\`: Displays the badge with a numeric or textual value.
+- **Visibility Control:** Use the \`isVisible\` prop to toggle the visibility of the badge dynamically. When set to \`false\`, the badge content is hidden.
+- **Content Customization:** 
+  - \`badgeContent\`: Render a numeric value or any textual content.
+  - \`maxContent\`: Specify the maximum value to display. If badgeContent exceeds this value, it displays \`maxContent+\`.
+- **Color Options:** Supports predefined color options: \`primary\`, \`success\`, and \`error\`, aligning with the design system.
+- **Children Support:** Seamlessly wrap any element (e.g., icons, text, buttons) with the badge, making it highly adaptable to your application's requirements.
+`
+      }
+    }
+  },
+  argTypes: {
+    variant: {
+      description:
+        'Specifies the style of the badge. `sm` displays the badge as a small dot for status indicators, while `lg` displays a numeric or textual value.',
+      control: { type: 'radio' },
+      options: ['sm', 'lg']
+    },
+    color: {
+      description:
+        'Defines the color of the badge, affecting its visual style. Can be used to align the badge with your application`s theme.',
+      control: { type: 'radio' },
+      options: ['primary', 'success', 'error']
+    },
+    badgeContent: {
+      description:
+        'The content to be displayed inside the badge. Typically used for numeric values or short text. If the variant is set to "sm," this prop is ignored.',
+      control: { type: 'number' }
+    },
+    maxContent: {
+      description:
+        'Controls the visibility of the badge. When set to `false`, the badge content is hidden.',
+      control: { type: 'number' },
+      defaultValue: 10
+    },
+    children: {
+      description:
+        'The element to be wrapped by the badge. This is typically an icon, button, or other visual element.',
+      control: { type: 'text' }
+    }
+  }
+}
+
+export default meta
+
+type Story = StoryObj<typeof Badge>
+
+export const Default: Story = {
+  args: {
+    variant: 'sm',
+    color: 'primary',
+    isVisible: true,
+    children: (
+      <IconButton>
+        <NotificationsRoundedIcon color='info' />
+      </IconButton>
+    )
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'The default configuration of the badge with a primary color, `sm` size displaying as small dot, and visibility enabled'
+      }
+    }
+  }
+}
+
+export const SmallSuccessBadge: Story = {
+  args: {
+    ...Default.args,
+    color: 'success'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'A small badge with success color'
+      }
+    }
+  }
+}
+
+export const FullBadge: Story = {
+  args: {
+    variant: 'lg',
+    color: 'primary',
+    isVisible: true,
+    badgeContent: 7,
+    maxContent: 10,
+    children: (
+      <IconButton>
+        <NotificationsRoundedIcon color='info' />
+      </IconButton>
+    )
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A large badge (`lg` variant) with numeric content and a maximum display value of 10.'
+      }
+    }
+  }
+}
+
+export const SuccessFullBadge: Story = {
+  args: {
+    ...FullBadge.args,
+    color: 'success',
+    maxContent: 99,
+    badgeContent: 102
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A large badge (`lg` variant) with numeric content, success color, and a maximum display value of 99.'
+      }
+    }
+  }
+}
+
+export const ErrorFullBadge: Story = {
+  args: {
+    ...FullBadge.args,
+    color: 'error',
+    maxContent: 10,
+    badgeContent: 7
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A large badge (`lg` variant) with numeric content, error color, and a content of 7.'
+      }
+    }
+  }
+}
