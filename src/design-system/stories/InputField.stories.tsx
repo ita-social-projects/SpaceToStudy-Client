82c7e6cf790730e17075b7b87f5c27af968adb52
unknown <pavlobutynez@gmail.com>
2024-12-01 21:57:06 +0200

    changed the imports using the ~scss-components alias

--- src/design-system/stories/InputField.stories.tsx
+++ src/design-system/stories/InputField.stories.tsx
@@ -1,9 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { fn } from '@storybook/test'
 import { useState } from 'react'
-import InputField from '~/design-system/components/input-field/InputField'
-import { InputFieldVariantEnum } from '~/design-system/components/input-field/InputField.constants'
-import { InputFieldProps } from '~/design-system/components/input-field/InputField'
+import { InputFieldVariantEnum } from '~scss-components/input-field/InputField.constants'
+import InputField, {
+  InputFieldProps
+} from '~scss-components/input-field/InputField'
 
 const meta: Meta<typeof InputField> = {
   title: 'Components/InputField',
