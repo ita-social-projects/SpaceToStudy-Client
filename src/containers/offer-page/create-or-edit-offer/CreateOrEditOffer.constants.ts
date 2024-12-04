4556f176ac7551ce7252ffbd253e26f2352eb31c
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2024-12-04 17:31:39 +0200

    Changed order

--- src/containers/offer-page/create-or-edit-offer/CreateOrEditOffer.constants.ts
+++ src/containers/offer-page/create-or-edit-offer/CreateOrEditOffer.constants.ts
@@ -6,12 +6,12 @@ export const getInitialValues = (offer: Offer | null) => ({
   subject: offer?.subject._id ?? '',
   proficiencyLevel: offer?.proficiencyLevel ?? [],
   languages: offer?.languages ?? [],
-  enrolledUsers: offer?.enrolledUsers ?? [],
   title: offer?.title ?? '',
   description: offer?.description ?? '',
   price: offer?.price.toString() ?? '',
   status: offer?.status ?? StatusEnum.Active,
-  FAQ: offer?.FAQ ?? [{ question: '', answer: '', id: `${Date.now()}` }]
+  FAQ: offer?.FAQ ?? [{ question: '', answer: '', id: `${Date.now()}` }],
+  enrolledUsers: offer?.enrolledUsers ?? []
 })
 
 export const validations = {
