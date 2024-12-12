a0c35900ff8cc2e64a4c89c2b9f71984ad6b5753
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2024-12-12 15:27:29 +0200

    Remove unused styles

--- src/containers/layout/app-snackbar/AppSnackbar.styles.ts
+++ src/containers/layout/app-snackbar/AppSnackbar.styles.ts
@@ -1,13 +1,6 @@
 export const styles = {
-  alert: {
-    color: 'basic.white'
-  },
   action: {
     p: '4px 8px 0 30px',
     cursor: 'pointer'
-  },
-  secondMessage: {
-    fontSize: '12px',
-    fontWeight: '300'
   }
 }
