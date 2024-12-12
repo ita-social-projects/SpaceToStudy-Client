bf7e886e5969cefcb17056db06b527cd57ba27fd
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-12-12 19:35:45 +0200

    remove unused styles

--- src/containers/my-cooperations/accept-cooperation-close/AcceptCooperationClosing.styles.ts
+++ src/containers/my-cooperations/accept-cooperation-close/AcceptCooperationClosing.styles.ts
@@ -1,27 +1,5 @@
-import palette from '~/styles/app-theme/app.pallete'
-
 export const styles = {
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-    border: `1px solid ${palette.basic.pinkishRed}`,
-    backgroundColor: palette.basic.softGray,
-    borderRadius: '5px',
-    padding: '24px',
-    justifyContent: 'space-between',
-    gap: '16px'
-  },
-  span: {
-    fontWeight: '500'
-  },
-  title: {
-    color: palette.basic.mediumRed,
-    fontWeight: '500',
-    display: 'flex',
-    gap: '8px',
-    mb: '4px'
-  },
-  body: {
-    color: palette.basic.darkGray
+  boldText: {
+    fontWeight: 500
   }
 }
