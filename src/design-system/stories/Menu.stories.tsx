e3d25a16556688c02bed766e1aca68581e1a5516
luiqor <vohnenna@gmail.com>
2024-12-14 17:05:53 +0200

    add onClick to stories

--- src/design-system/stories/Menu.stories.tsx
+++ src/design-system/stories/Menu.stories.tsx
@@ -18,9 +18,17 @@ export const Default: Story = {
   args: {
     open: true,
     menuItems: [
-      { title: 'Lesson', graphics: <Book /> },
-      { title: 'Quiz', graphics: <Checkbox /> },
-      { title: 'Attachment' }
+      {
+        title: 'Lesson',
+        graphics: <Book />,
+        onClick: () => alert('Lesson clicked')
+      },
+      {
+        title: 'Quiz',
+        graphics: <Checkbox />,
+        onClick: () => alert('Quiz clicked')
+      },
+      { title: 'Attachment', onClick: () => alert('Attachment clicked') }
     ]
   }
 }
@@ -33,17 +41,20 @@ export const WithAdditionalInfo: Story = {
       {
         title: 'Lesson',
         additionalInfo: 'Explore comprehensive lessons on various topics',
-        graphics: <Book />
+        graphics: <Book />,
+        onClick: () => alert('Lesson clicked')
       },
       {
         title: 'Quiz',
         additionalInfo: 'Test your knowledge with engaging quizzes',
-        graphics: <Book />
+        graphics: <Book />,
+        onClick: () => alert('Quiz clicked')
       },
       {
         title: 'Attachment',
         additionalInfo: 'Access all your important files and documents',
-        graphics: <Book />
+        graphics: <Book />,
+        onClick: () => alert('Attachment clicked')
       }
     ]
   }
@@ -59,9 +70,21 @@ export const WithNestedMenuItems: Story = {
         additionalInfo: 'Explore comprehensive lessons on various topics',
         graphics: <Book />,
         nestedMenuItems: [
-          { title: 'Math', graphics: <Book /> },
-          { title: 'Science', graphics: <Book /> },
-          { title: 'History', graphics: <Book /> }
+          {
+            title: 'Math',
+            graphics: <Book />,
+            onClick: () => alert('Math clicked')
+          },
+          {
+            title: 'Science',
+            graphics: <Book />,
+            onClick: () => alert('Science clicked')
+          },
+          {
+            title: 'History',
+            graphics: <Book />,
+            onClick: () => alert('History clicked')
+          }
         ]
       },
       {
@@ -69,9 +92,21 @@ export const WithNestedMenuItems: Story = {
         additionalInfo: 'Test your knowledge with engaging quizzes',
         graphics: <Book />,
         nestedMenuItems: [
-          { title: 'Math', graphics: <Book /> },
-          { title: 'Science', graphics: <Book /> },
-          { title: 'History', graphics: <Book /> }
+          {
+            title: 'Geometry',
+            graphics: <Book />,
+            onClick: () => alert('Geometry clicked')
+          },
+          {
+            title: 'Chemistry',
+            graphics: <Book />,
+            onClick: () => alert('Chemistry clicked')
+          },
+          {
+            title: 'Modern History',
+            graphics: <Book />,
+            onClick: () => alert('Modern History clicked')
+          }
         ]
       },
       {
@@ -79,9 +114,21 @@ export const WithNestedMenuItems: Story = {
         additionalInfo: 'Files and documents',
         graphics: <Book />,
         nestedMenuItems: [
-          { title: 'Math', graphics: <Book /> },
-          { title: 'Science', graphics: <Book /> },
-          { title: 'History', graphics: <Book /> }
+          {
+            title: 'Solid of revolution',
+            graphics: <Book />,
+            onClick: () => alert('Solid of revolution clicked')
+          },
+          {
+            title: 'Molecule',
+            graphics: <Book />,
+            onClick: () => alert('Molecule clicked')
+          },
+          {
+            title: 'Modern world map',
+            graphics: <Book />,
+            onClick: () => alert('Modern world map clicked')
+          }
         ]
       }
     ]
