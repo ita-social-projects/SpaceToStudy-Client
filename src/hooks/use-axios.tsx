67d44660c7d6e7f4a69d46cb5dca71cefdc5642d
Zhayvoronok Kateryna <1gayv425@gmail.com>
2024-12-18 13:56:01 +0200

    change throw error in use axios

--- src/hooks/use-axios.tsx
+++ src/hooks/use-axios.tsx
@@ -52,7 +52,16 @@ const useAxios = <
       try {
         setLoading(true)
         const res = await service(params)
-        if (res.status >= 400 && res.status <= 526) throw res
+        if (res.status >= 400 && res.status <= 526) {
+          // throw res
+          throw new AxiosError(
+            'Request failed',
+            res.statusText,
+            res.config,
+            res.request,
+            res
+          )
+        }
         const responseData = transform ? transform(res.data) : res.data
         setResponse(responseData as TransformedResponse)
         setError(null)
