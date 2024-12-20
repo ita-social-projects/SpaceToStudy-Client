9af3761cf46c961ae2fde30e829a41b1287a5f11
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2024-12-20 15:37:41 +0200

    Fixed query provider types

--- src/QueryProvider.tsx
+++ src/QueryProvider.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import { type ReactNode } from 'react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
@@ -8,7 +8,7 @@ type QueryProviderProps = {
   children: ReactNode
 }
 
-const QueryProvider: FC<QueryProviderProps> = ({ children }) => {
+const QueryProvider: React.FC<QueryProviderProps> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
