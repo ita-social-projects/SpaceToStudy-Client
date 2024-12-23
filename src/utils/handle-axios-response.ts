3ef67fcfe8f65cb6d2efa7399df69467bd366cb4
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2024-12-23 21:23:44 +0200

    Added handleAxiosResponse utility

---
+++ src/utils/handle-axios-response.ts
@@ -0,0 +1,15 @@
+import { AxiosResponse } from 'axios'
+
+import { ErrorResponse } from '~/types'
+
+export async function handleAxiosResponse<T>(
+  promise: Promise<AxiosResponse<T>>
+): Promise<T> {
+  const response = await promise
+  if (response.status < 200 || response.status > 299) {
+    const errorResponse = response.data as ErrorResponse
+    throw new Error(errorResponse.message)
+  }
+
+  return response.data
+}
