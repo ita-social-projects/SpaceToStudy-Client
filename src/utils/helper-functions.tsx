dbe99c2449d739e53946d144c5cbdc38068fed05
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2025-01-01 22:24:03 +0200

    update getFullUrl function and its tests

--- src/utils/helper-functions.tsx
+++ src/utils/helper-functions.tsx
@@ -225,11 +225,17 @@ export const createUrlPath = (
   return `${trimmedUrl}${paramsString}${queryParamsString}`
 }
 
+type SearchParameterValue =
+  | string
+  | string[]
+  | number
+  | number[]
+  | null
+  | undefined
+  | Record<string, string | number | null | undefined>
+
 const getSearchParametersEntries = (
-  searchParameters: Record<
-    string,
-    string | string[] | number | number[] | undefined
-  >
+  searchParameters: Record<string, SearchParameterValue>
 ) => {
   const searchParametersEntries: [string, string][] = []
 
@@ -245,6 +251,20 @@ const getSearchParametersEntries = (
         searchParametersEntries.push([
           searchParameterName,
           String(parameterValue)
+        ])
+      }
+    } else if (typeof searchParameterValue === 'object') {
+      for (const parameterKey in searchParameterValue) {
+        const searchParameterObjectKey = `${searchParameterName}[${parameterKey}]`
+        const searchParameterObjectValue = searchParameterValue[parameterKey]
+
+        if (!searchParameterObjectValue) {
+          continue
+        }
+
+        searchParametersEntries.push([
+          searchParameterObjectKey,
+          String(searchParameterObjectValue)
         ])
       }
     } else {
@@ -265,10 +285,7 @@ export const getFullUrl = ({
 }: {
   parameters?: Record<string, string>
   pathname: string
-  searchParameters?: Record<
-    string,
-    string | string[] | number | number[] | undefined
-  >
+  searchParameters?: Record<string, SearchParameterValue>
 }) => {
   let resultUrl = pathname
 
