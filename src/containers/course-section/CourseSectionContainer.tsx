fa207bffacfc308d5dcf981820234362f3a9802b
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2025-01-04 16:40:32 +0200

    Changed resourse service returned value

--- src/containers/course-section/CourseSectionContainer.tsx
+++ src/containers/course-section/CourseSectionContainer.tsx
@@ -1,6 +1,5 @@
 import { useState, FC, useMemo, useCallback, FocusEvent } from 'react'
 import { useTranslation } from 'react-i18next'
-import { v4 as uuidv4 } from 'uuid'
 
 import { MenuItem } from '@mui/material'
 import Box from '@mui/material/Box'
@@ -256,7 +255,7 @@ const CourseSectionContainer: FC<SectionProps> = ({
             isDuplicate: isDuplicate
           })
 
-          newResources.push(newLesson.data as Lesson)
+          newResources.push(newLesson)
         } else if (resource.resourceType === ResourcesTypesEnum.Quiz) {
           const quiz = resource as Quiz
           const newQuiz = await ResourceService.addQuiz({
@@ -266,7 +265,7 @@ const CourseSectionContainer: FC<SectionProps> = ({
             isDuplicate: isDuplicate,
             resourceType: ResourcesTypesEnum.Quiz,
             category: null,
-            id: uuidv4()
+            id: crypto.randomUUID()
           })
           newResources.push(newQuiz.data as Quiz)
         }
