76863d7ca6de119d988161c8db9d6390a8641e12
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2025-01-04 17:32:43 +0200

    Fixed tests

--- tests/unit/pages/create-or-edit-lesson/CreateOrEditLessonWithId.spec.jsx
+++ tests/unit/pages/create-or-edit-lesson/CreateOrEditLessonWithId.spec.jsx
@@ -2,14 +2,14 @@ import { fireEvent, screen, waitFor } from '@testing-library/react'
 
 import CreateOrEditLesson from '~/pages/create-or-edit-lesson/CreateOrEditLesson'
 
-import { createUrlPath } from '~/utils/helper-functions'
 import { ResourceService } from '~/services/resource-service'
-import { URLs } from '~/constants/request'
-import { mockAxiosClient, renderWithProviders } from '~tests/test-utils'
+import { baseService } from '~/services/base-service'
+import { renderWithProviders } from '~tests/test-utils'
 
 const mockParams = {
   id: 'id'
 }
+
 const mockLesson = {
   title: 'Lesson 1',
   author: 'authorId',
@@ -25,23 +25,32 @@ vi.mock('react-router-dom', async () => ({
 
 describe('CreateOrEditLesson component with id', () => {
   beforeAll(() => {
-    mockAxiosClient
-      .onGet(createUrlPath(URLs.resources.lessons.get, mockParams.id))
-      .reply(200, mockLesson)
+    vi.spyOn(ResourceService, 'editLesson')
+
+    vi.spyOn(baseService, 'request').mockImplementation((config) => {
+      if (config.method === 'PATCH') {
+        return Promise.resolve({ data: {} })
+      }
+
+      if (config.method === 'GET') {
+        return Promise.resolve({ data: mockLesson })
+      }
+
+      return Promise.reject(new Error('Unexpected request'))
+    })
   })
 
   beforeEach(async () => {
     await waitFor(() => renderWithProviders(<CreateOrEditLesson />))
+    await screen.findByDisplayValue(mockLesson.title)
   })
 
   it('should load a lesson info', () => {
     const titleInput = screen.getByDisplayValue(mockLesson.title)
-
     expect(titleInput).toBeInTheDocument()
   })
 
   it('should edit a lesson', async () => {
-    mockAxiosClient.onPatch(URLs.resources.lessons.patch).reply(200)
     const editLessonSpy = vi.spyOn(ResourceService, 'editLesson')
     const titleInput = await screen.findByDisplayValue(mockLesson.title)
     const descriptionInput = screen.getByLabelText('lesson.labels.description')
