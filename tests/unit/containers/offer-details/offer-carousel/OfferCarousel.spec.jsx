515edf89083cee0e54ce0be2b663ba097af0b387
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-01-14 17:40:45 +0200

    Rewrote tests for OfferCarousel

--- tests/unit/containers/offer-details/offer-carousel/OfferCarousel.spec.jsx
+++ tests/unit/containers/offer-details/offer-carousel/OfferCarousel.spec.jsx
@@ -1,14 +1,34 @@
-import { screen } from '@testing-library/react'
-import { renderWithProviders } from '~tests/test-utils'
+import { screen, waitFor } from '@testing-library/react'
+import { mockAxiosClient, renderWithProviders } from '~tests/test-utils'
 import useAxios from '~/hooks/use-axios'
-import { vi } from 'vitest'
+import { afterAll, beforeEach, vi } from 'vitest'
 import OfferCarousel from '~/containers/offer-details/offer-carousel/OfferCarousel'
 import { mockOffer } from '~tests/unit/pages/offer-details/OfferDetails.spec.constants'
+import { URLs } from '~/constants/request'
 
 vi.mock('~/hooks/use-axios')
 
-describe('OfferCarousel test', () => {
+const mockData = {
+  loading: false,
+  response: { count: 1, items: [{ ...mockOffer, _id: 'id2' }] }
+}
+
+const emptyData = {
+  loading: false,
+  response: { count: 0, items: [] }
+}
+
+describe('OfferCarousel with data', () => {
   beforeAll(() => {
+    mockAxiosClient
+      .onGet(
+        new RegExp(
+          `^${URLs.offers.getBySubjectId.replace(':subjectId', mockOffer.subject._id)}`
+        )
+      )
+      .reply(200, { items: [{ ...mockOffer, _id: 'id2' }], count: 1 })
+    useAxios.mockImplementation(() => mockData)
+
     global.ResizeObserver = vi.fn().mockImplementation(() => ({
       observe: vi.fn(),
       unobserve: vi.fn(),
@@ -27,39 +47,65 @@ describe('OfferCarousel test', () => {
     }))
   })
 
-  it('should render OfferCarousel', async () => {
-    const fakeData = {
-      loading: false,
-      response: { count: 1, items: [{ ...mockOffer, _id: 'id2' }] }
-    }
-    useAxios.mockImplementation(() => fakeData)
+  beforeEach(() => {
     renderWithProviders(<OfferCarousel offer={mockOffer} />)
+  })
 
-    const title = screen.getByText('findOffers.otherOffers.title')
+  afterAll(() => {
+    vi.clearAllMocks()
+  })
 
+  it('should render OfferCarousel', async () => {
+    const title = await screen.findByText('findOffers.otherOffers.title')
     expect(title).toBeInTheDocument()
   })
-  it('should not render OfferCarousel', async () => {
-    const fakeData = {
-      loading: false,
-      response: { count: 0, items: [] }
-    }
-    useAxios.mockImplementation(() => fakeData)
-    renderWithProviders(<OfferCarousel offer={mockOffer} />)
+})
 
-    const title = screen.queryByText('findOffers.otherOffers.title')
+describe('OfferCarousel without data', () => {
+  beforeAll(() => {
+    mockAxiosClient
+      .onGet(
+        new RegExp(
+          `^${URLs.offers.getBySubjectId.replace(':subjectId', mockOffer.subject._id)}`
+        )
+      )
+      .reply(200, { items: [], count: 0 })
+    useAxios.mockImplementation(() => emptyData)
 
-    expect(title).not.toBeInTheDocument()
+    global.ResizeObserver = vi.fn().mockImplementation(() => ({
+      observe: vi.fn(),
+      unobserve: vi.fn(),
+      disconnect: vi.fn()
+    }))
+
+    window.matchMedia = vi.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn()
+    }))
   })
-  it('should not render OfferCardSquare when no offers', async () => {
-    const fakeData = {
-      loading: false,
-      response: { count: 0, items: [] }
-    }
-    useAxios.mockImplementation(() => fakeData)
 
+  beforeEach(() => {
     renderWithProviders(<OfferCarousel offer={mockOffer} />)
+  })
 
+  afterAll(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should not render OfferCarousel', async () => {
+    await waitFor(() => {
+      const title = screen.queryByText('findOffers.otherOffers.title')
+      expect(title).not.toBeInTheDocument()
+    })
+  })
+
+  it('should not render OfferCardSquare when no offers', async () => {
     const offerCard = screen.queryByTestId('OfferContainer')
     expect(offerCard).not.toBeInTheDocument()
   })
