e3860056192d3f12d1acb92864a115a850eff220
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-01-14 16:09:38 +0200

    Rewrote unit tests for FindOffers

--- tests/unit/pages/find-offers/FindOffers.spec.jsx
+++ tests/unit/pages/find-offers/FindOffers.spec.jsx
@@ -1,17 +1,15 @@
 import { fireEvent, screen, waitFor, act } from '@testing-library/react'
+import { afterEach, beforeEach, expect, vi } from 'vitest'
+import { mockAxiosClient, renderWithProviders } from '~tests/test-utils'
 
 import FindOffers from '~/pages/find-offers/FindOffers'
-
-import { renderWithProviders } from '~tests/test-utils'
 import useBreakpoints from '~/hooks/use-breakpoints'
 import { useFilterQuery } from '~/hooks/use-filter-query'
-
 import { offersMock } from '~tests/unit/pages/find-offers/FindOffers.constants'
-import useQuery from '~/hooks/use-query'
+import { URLs } from '~/constants/request'
 
 vi.mock('~/hooks/use-breakpoints')
 vi.mock('~/hooks/use-filter-query')
-vi.mock('~/hooks/use-query')
 
 const preloadedState = { appMain: { userRole: 'tutor' } }
 
@@ -41,22 +39,21 @@ const filterQueryMock = {
 
 const scrollIntoViewMock = vi.fn()
 
-describe('FindOffers component', () => {
+describe('FindOffers component with data', () => {
   const desktopData = {
     isLaptopAndAbove: true,
     isMobile: false,
     isTablet: false
   }
+
   beforeEach(async () => {
     window.HTMLElement.prototype.scrollIntoView = scrollIntoViewMock
+    mockAxiosClient
+      .onGet(new RegExp(`^${URLs.offers.get}`))
+      .reply(200, offersMock)
     await waitFor(() => {
       useFilterQuery.mockReturnValue(filterQueryMock)
       useBreakpoints.mockImplementation(() => desktopData)
-      useQuery.mockReturnValue({
-        data: offersMock,
-        isLoading: false,
-        isError: false
-      })
       renderWithProviders(<FindOffers />, {
         preloadedState
       })
@@ -67,19 +64,17 @@ describe('FindOffers component', () => {
     vi.clearAllMocks()
   })
 
-  it('should renders FindOffers component with data and change role', async () => {
-    await waitFor(() => {
-      const toggle = screen.getByRole('checkbox')
-      fireEvent.click(toggle)
+  it('should render FindOffers component with data and change role', async () => {
+    const toggle = screen.getByRole('checkbox')
+    fireEvent.click(toggle)
 
-      expect(
-        filterQueryMock.filterQueryActions.updateFiltersInQuery
-      ).toHaveBeenCalled()
-    })
+    expect(
+      filterQueryMock.filterQueryActions.updateFiltersInQuery
+    ).toHaveBeenCalled()
   })
 
   it('should open modal window', async () => {
-    await act(() => {
+    act(() => {
       const filter = screen.getByText('filters.filtersListTitle')
       fireEvent.click(filter)
     })
@@ -93,7 +88,6 @@ describe('FindOffers component', () => {
 
   it('should change page', async () => {
     const secondPage = screen.getByLabelText('Go to page 2')
-
     fireEvent.click(secondPage)
 
     expect(scrollIntoViewMock).toHaveBeenCalled()
@@ -103,21 +97,20 @@ describe('FindOffers component', () => {
   })
 })
 
-describe('FindOffers component', () => {
+describe('FindOffers component without data', () => {
   const desktopData = {
     isLaptopAndAbove: true,
     isMobile: false,
     isTablet: false
   }
+
   beforeEach(async () => {
+    mockAxiosClient
+      .onGet(new RegExp(`^${URLs.offers.get}`))
+      .reply(200, { items: [], count: 0 })
     await waitFor(() => {
       useFilterQuery.mockReturnValue(filterQueryMock)
       useBreakpoints.mockImplementation(() => desktopData)
-      useQuery.mockReturnValue({
-        data: { items: [], count: 0 },
-        isLoading: false,
-        isError: false
-      })
       renderWithProviders(<FindOffers />, {
         preloadedState
       })
@@ -128,7 +121,7 @@ describe('FindOffers component', () => {
     vi.clearAllMocks()
   })
 
-  it('should renders FindOffers component without data', async () => {
+  it('should render FindOffers component without data', async () => {
     expect(
       screen.getByText('findOffers.offerRequestBlock.title.tutor')
     ).toBeInTheDocument()
@@ -141,21 +134,20 @@ describe('FindOffers component', () => {
   })
 })
 
-describe('FindOffers component', () => {
+describe('FindOffers component with no scroll', () => {
   const mobileData = {
     isLaptopAndAbove: false,
     isMobile: true,
     isTablet: false
   }
+
   beforeEach(async () => {
+    mockAxiosClient
+      .onGet(new RegExp(`^${URLs.offers.get}`))
+      .reply(200, offersMock)
     await waitFor(() => {
       useFilterQuery.mockReturnValue(filterQueryMock)
       useBreakpoints.mockImplementation(() => mobileData)
-      useQuery.mockReturnValue({
-        data: offersMock,
-        isLoading: false,
-        isError: false
-      })
       renderWithProviders(<FindOffers />, {
         preloadedState
       })
@@ -166,7 +158,7 @@ describe('FindOffers component', () => {
     vi.clearAllMocks()
   })
 
-  it('should renders FindOffers component with data and change role', async () => {
+  it('should render FindOffers component with data', async () => {
     const existingName = screen.getByText('Anastasiia Mashchenko')
     expect(existingName).toBeInTheDocument()
   })
