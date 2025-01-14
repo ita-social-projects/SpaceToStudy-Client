fb0e36d3bd0df26f1fc200b0ec929d1f8223f7a8
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-01-14 20:55:26 +0200

    Added tests to check whether getFullUrl is called with correct params

--- tests/unit/services/offer-service.spec.jsx
+++ tests/unit/services/offer-service.spec.jsx
@@ -4,9 +4,21 @@ import { OfferService } from '~/services/offer-service'
 import { mockOffer } from '~tests/unit/pages/offer-details/OfferDetails.spec.constants'
 import { offersMock } from '~tests/unit/pages/find-offers/FindOffers.constants'
 import { URLs } from '~/constants/request'
+import * as getFullUrl from '~/utils/get-full-url'
 
 const categoryId = mockOffer.category._id
 const subjectId = mockOffer.subject._id
+const searchParams = {
+  sort: 'createdAt',
+  language: '',
+  native: 'false',
+  rating: '0',
+  authorRole: 'student',
+  search: '',
+  proficiencyLevel: [],
+  price: null,
+  page: '1'
+}
 
 describe('offerService getOffers function tests', () => {
   afterEach(() => {
@@ -14,6 +26,8 @@ describe('offerService getOffers function tests', () => {
   })
 
   it('should find offer by categoryId and subjectId', async () => {
+    const getFullUrlSpy = vi.spyOn(getFullUrl, 'getFullUrl')
+
     mockAxiosClient
       .onGet(
         new RegExp(
@@ -23,14 +37,24 @@ describe('offerService getOffers function tests', () => {
         )
       )
       .reply(200, mockOffer)
+
     const result = await OfferService.getOffers({
       categoryId,
-      subjectId
+      subjectId,
+      ...searchParams
     })
+
     expect(result).toEqual(mockOffer)
+    expect(getFullUrlSpy).toHaveBeenCalledWith({
+      pathname: URLs.offers.getByCategoryAndSubjectId,
+      parameters: { categoryId, subjectId },
+      searchParameters: searchParams
+    })
   })
 
   it('should find offer by categoryId', async () => {
+    const getFullUrlSpy = vi.spyOn(getFullUrl, 'getFullUrl')
+
     mockAxiosClient
       .onGet(
         new RegExp(
@@ -38,27 +62,52 @@ describe('offerService getOffers function tests', () => {
         )
       )
       .reply(200, mockOffer)
+
     const result = await OfferService.getOffers({
-      categoryId
+      categoryId,
+      ...searchParams
     })
+
     expect(result).toEqual(mockOffer)
+    expect(getFullUrlSpy).toHaveBeenCalledWith({
+      pathname: URLs.offers.getByCategoryId,
+      parameters: { categoryId },
+      searchParameters: searchParams
+    })
   })
 
   it('should find offer by subjectId', async () => {
+    const getFullUrlSpy = vi.spyOn(getFullUrl, 'getFullUrl')
+
     mockAxiosClient
       .onGet(
         new RegExp(URLs.offers.getBySubjectId.replace(':subjectId', subjectId))
       )
       .reply(200, mockOffer)
+
     const result = await OfferService.getOffers({
-      subjectId
+      subjectId,
+      ...searchParams
     })
+
     expect(result).toEqual(mockOffer)
+    expect(getFullUrlSpy).toHaveBeenCalledWith({
+      pathname: URLs.offers.getBySubjectId,
+      parameters: { subjectId },
+      searchParameters: searchParams
+    })
   })
 
   it('should find all offers', async () => {
+    const getFullUrlSpy = vi.spyOn(getFullUrl, 'getFullUrl')
     mockAxiosClient.onGet(new RegExp(URLs.offers.get)).reply(200, offersMock)
-    const result = await OfferService.getOffers({})
+
+    const result = await OfferService.getOffers(searchParams)
     expect(result).toEqual(offersMock)
+
+    expect(getFullUrlSpy).toHaveBeenCalledWith({
+      pathname: URLs.offers.get,
+      searchParameters: searchParams
+    })
   })
 })
