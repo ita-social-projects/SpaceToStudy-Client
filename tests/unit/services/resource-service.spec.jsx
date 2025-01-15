ffced7509fa5253bf675c4d359da72ba956aa695
Mike-Popovych <volt251988@gmail.com>
2025-01-15 17:27:52 +0200

    added test for getQuizQuery

--- tests/unit/services/resource-service.spec.jsx
+++ tests/unit/services/resource-service.spec.jsx
@@ -24,4 +24,36 @@ describe('resourseService tests', () => {
       URLs.resources.lessons.patch.replace(':id', lessonId)
     )
   })
+
+  it('should fetch a quiz by ID', async () => {
+    const quizId = '6641388f36ebdb0432a3a2e5';
+    const mockQuizData = {
+      _id: quizId,
+      title: 'Sample Quiz',
+      items: [],
+      author: '12345',
+      category: null,
+      resourceType: 'Quiz',
+      settings: {
+        view: 'Stepper',
+        shuffle: false,
+        pointValues: true,
+        scoredResponses: true,
+        correctAnswers: true,
+      },
+      createdAt: '2024-01-01T00:00:00.000Z',
+      updatedAt: '2024-01-01T00:00:00.000Z',
+    };
+
+    mockAxiosClient
+      .onGet(URLs.quizzes.getById.replace(':id', quizId))
+      .reply(200, mockQuizData);
+
+    const result = await ResourceService.getQuizQuery(quizId);
+
+    expect(mockAxiosClient.history.get[0].url).toBe(
+      URLs.quizzes.getById.replace(':id', quizId)
+    );
+    expect(result).toEqual(mockQuizData);
+  })
 })
