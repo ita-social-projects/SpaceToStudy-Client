03a1784661ad9aa725aa393ac7dff11b834e0294
Zhayvoronok Kateryna <1gayv425@gmail.com>
2025-01-15 19:12:28 +0200

    added more tests

--- tests/unit/services/resource-service.spec.jsx
+++ tests/unit/services/resource-service.spec.jsx
@@ -24,4 +24,33 @@ describe('resourseService tests', () => {
       URLs.resources.lessons.patch.replace(':id', lessonId)
     )
   })
+  it('should get resource categories names', async () => {
+    const mockResponse = [
+      { _id: '1', name: 'Category 1' },
+      { _id: '2', name: 'Category 2' }
+    ]
+
+    mockAxiosClient
+      .onGet(URLs.resources.resourcesCategories.getNames)
+      .reply(200, mockResponse)
+
+    const response = await ResourceService.getResourcesCategoriesName()
+
+    expect(mockAxiosClient.history.get[0].url).toBe(URLs.resources.resourcesCategories.getNames)
+    expect(response).toEqual(mockResponse)
+  })
+  it('should create a resource category', async () => {
+    const mockResponse = { _id: '3', name: 'New Category' }
+    const params = { name: 'New Category' }
+
+    mockAxiosClient
+      .onPost(URLs.resources.resourcesCategories.post)
+      .reply(200, mockResponse)
+
+    const response = await ResourceService.createResourceCategory(params)
+
+    expect(mockAxiosClient.history.post[0].url).toBe(URLs.resources.resourcesCategories.post)
+    expect(mockAxiosClient.history.post[0].data).toBe(JSON.stringify(params))
+    expect(response).toEqual(mockResponse)
+  })
 })
