8a8068d7a12149278aba3ae2b9181f6bf08da6fa
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2025-01-19 23:40:14 +0200

    Extracted styles for timer into a separate file

---
+++ src/components/timer/Timer.styles.ts
@@ -0,0 +1,18 @@
+import { theme } from '~/styles/app-theme/custom-mui.styles'
+
+const styles = {
+  chip: {
+    gap: theme.spacing(1),
+    padding: theme.spacing(0.75, 1),
+    borderWidth: '1px',
+    borderRadius: '12px'
+  },
+  label: {
+    fontWeight: 500,
+    fontSize: '18px',
+    lineHeight: '24px',
+    textAlign: 'center'
+  }
+}
+
+export default styles
--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,6 +1,8 @@
 import Chip from '@mui/material/Chip/Chip'
 import TimerOutlined from '@mui/icons-material/TimerOutlined'
 
+import styles from '~/components/timer/Timer.styles'
+
 type TimerProps = {
   label: string
   isTimeEnds: boolean
@@ -23,19 +25,14 @@ const Timer = ({ label, isTimeEnds }: TimerProps) => {
         backgroundColor: isTimeEnds
           ? 'rgba(250, 238, 238, 1)'
           : 'rgba(226, 242, 220, 1)',
-        borderWidth: '1px',
-        borderRadius: '12px',
         borderColor: isTimeEnds
           ? 'rgba(205, 54, 54, 1)'
           : 'rgba(87, 154, 64, 1)',
-        gap: '8px',
+        ...styles.chip,
         padding: '6px 8px',
         '& .MuiChip-label': {
           color: isTimeEnds ? 'rgba(82, 22, 22, 1)' : 'rgba(44, 77, 32, 1)',
-          fontWeight: 500,
-          fontSize: '18px',
-          lineHeight: '24px',
-          textAlign: 'center'
+          ...styles.label
         }
       }}
       variant='outlined'
