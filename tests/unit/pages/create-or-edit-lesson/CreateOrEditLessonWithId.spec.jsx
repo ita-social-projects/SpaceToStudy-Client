aeea666059ea86866050a8e05f048d49adf239f6
luiqor <vohnenna@gmail.com>
2025-01-22 00:09:38 +0200

    fix CreateOrEditLessonWithId.spec.jsx test suite

--- tests/unit/pages/create-or-edit-lesson/CreateOrEditLessonWithId.spec.jsx
+++ tests/unit/pages/create-or-edit-lesson/CreateOrEditLessonWithId.spec.jsx
@@ -2,10 +2,9 @@ import { screen } from '@testing-library/react'
 import { vi } from 'vitest'
 
 import CreateOrEditLesson from '~/pages/create-or-edit-lesson/CreateOrEditLesson'
-import { ResourceService } from '~/services/resource-service'
-import { baseService } from '~/services/base-service'
 import { renderWithProviders, mockAxiosClient } from '~tests/test-utils'
 import { URLs } from '~/constants/request'
+
 const mockParams = {
   id: 'id'
 }
@@ -38,8 +37,8 @@ describe('CreateOrEditLesson with id', () => {
       .reply(204)
   })
 
-  beforeEach(async () => {
-    await renderWithProviders(<CreateOrEditLesson />)
+  beforeEach(() => {
+    renderWithProviders(<CreateOrEditLesson />)
   })
 
   it('should display the lesson title and description when the lesson is loaded', async () => {
