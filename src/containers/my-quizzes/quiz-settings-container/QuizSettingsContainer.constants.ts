85ed56a39fa111049b60a77934655551329545a2
markgol777 <markgol777@gmail.com>
2025-01-21 16:03:58 +0100

    Fixed the case in json files

--- src/containers/my-quizzes/quiz-settings-container/QuizSettingsContainer.constants.ts
+++ src/containers/my-quizzes/quiz-settings-container/QuizSettingsContainer.constants.ts
@@ -16,30 +16,30 @@ export const getQuizViewFields = (t: TFunction) => [
 export const getQuizTimeLimitFields = (t: TFunction) => [
   {
     value: QuizTimeLimit.NoLimit,
-    title: t('myResourcesPage.quizzes.types.NoLimit')
+    title: t('myResourcesPage.quizzes.types.noLimit')
   },
   {
     value: QuizTimeLimit.Minute15,
-    title: t('myResourcesPage.quizzes.types.Minute15')
+    title: t('myResourcesPage.quizzes.types.minute15')
   },
   {
     value: QuizTimeLimit.Minute30,
-    title: t('myResourcesPage.quizzes.types.Minute30')
+    title: t('myResourcesPage.quizzes.types.minute30')
   },
   {
     value: QuizTimeLimit.Minute45,
-    title: t('myResourcesPage.quizzes.types.Minute45')
+    title: t('myResourcesPage.quizzes.types.minute45')
   },
   {
     value: QuizTimeLimit.Hour1,
-    title: t('myResourcesPage.quizzes.types.Hour1')
+    title: t('myResourcesPage.quizzes.types.hour1')
   }
 ]
 
 export const getQuizAttemptFields = (t: TFunction) => [
   {
     value: QuizAttempt.NoLimit,
-    title: t('myResourcesPage.quizzes.types.NoLimit')
+    title: t('myResourcesPage.quizzes.types.noLimit')
   },
   {
     value: QuizAttempt.Attempt1,
