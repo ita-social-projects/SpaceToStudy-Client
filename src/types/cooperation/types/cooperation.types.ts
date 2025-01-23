b44b89d3bcc4c1fa9bb4192a2ddb93a6e5358f38
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2025-01-23 14:13:26 +0200

    update cooperation types

--- src/types/cooperation/types/cooperation.types.ts
+++ src/types/cooperation/types/cooperation.types.ts
@@ -1,12 +1,14 @@
 import { Cooperation } from '~/types'
 
 export type UpdateCooperationsParams = Partial<
-  Pick<Cooperation, 'status' | 'price' | '_id'>
->
+  Pick<Cooperation, 'status' | 'price'>
+> &
+  Pick<Cooperation, '_id'>
 
 export type UpdateCooperationsSections = Partial<
-  Pick<Cooperation, 'sections' | '_id'>
->
+  Pick<Cooperation, 'sections'>
+> &
+  Pick<Cooperation, '_id'>
 
 export type UpdateCooperationStatusParams = Partial<
   Pick<Cooperation, 'status' | '_id'>
