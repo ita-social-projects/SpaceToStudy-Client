10b8145ce114b1ad6379da24ef456c96f89457de
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-01-24 15:13:12 +0200

    Added more tests to increase coverage of the component

--- tests/unit/containers/my-quizzes/CreateOrEditQuizContainer.spec.jsx
+++ tests/unit/containers/my-quizzes/CreateOrEditQuizContainer.spec.jsx
@@ -1,11 +1,13 @@
 import { fireEvent, screen, waitFor } from '@testing-library/react'
-import { renderWithProviders } from '~tests/test-utils'
+import { mockAxiosClient, renderWithProviders } from '~tests/test-utils'
+import { URLs } from '~/constants/request'
 
 import CreateOrEditQuizContainer from '~/containers/my-quizzes/create-or-edit-quiz-container/CreateOrEditQuizContainer'
 
 const setTitle = vi.fn()
 const setDescription = vi.fn()
 const category = 'mock-category'
+const mockId = '676728f88a5ae7b4b41f5e89'
 
 describe('CreateOrEditQuizContainer', () => {
   beforeEach(async () => {
@@ -17,6 +19,10 @@ describe('CreateOrEditQuizContainer', () => {
         />
       )
     })
+  })
+
+  afterAll(() => {
+    vi.clearAllMocks()
   })
 
   it('should change title and description inputs', () => {
@@ -70,13 +76,47 @@ describe('CreateOrEditQuizContainer', () => {
   })
 })
 
-describe('CreateOrEditQuizContainer', () => {
-  beforeEach(() => {
-    renderWithProviders(<CreateOrEditQuizContainer category={category} />)
+describe('CreateOrEditQuizContainer with id', () => {
+  beforeAll(() => {
+    vi.mock('react-router-dom', async () => {
+      const original = await vi.importActual('react-router-dom')
+      return {
+        ...original,
+        useParams: () => ({ id: mockId })
+      }
+    })
   })
 
-  it('should save quiz with category', () => {
+  beforeEach(async () => {
+    mockAxiosClient
+      .onGet(new RegExp(URLs.quizzes.get.replace(':id', mockId)))
+      .reply(200, {
+        _id: mockId,
+        title: 'Mock title',
+        description: 'Mock description',
+        category
+      })
+
+    await waitFor(() => {
+      renderWithProviders(
+        <CreateOrEditQuizContainer
+          questions={[]}
+          category={category}
+          setTitle={setTitle}
+        />
+      )
+    })
+  })
+
+  afterAll(() => {
+    vi.clearAllMocks()
+    vi.resetAllMocks()
+  })
+
+  it('should save quiz with category', async () => {
     const saveBtn = screen.getByText('common.save')
     fireEvent.click(saveBtn)
+
+    expect(setTitle).toHaveBeenCalled()
   })
 })
