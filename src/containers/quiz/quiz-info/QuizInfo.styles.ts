06a18ee4c610106e95f046568a3f9f639cdc0614
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2025-01-26 23:23:56 +0200

    Added adaptive styles for quiz header

--- src/containers/quiz/quiz-info/QuizInfo.styles.ts
+++ src/containers/quiz/quiz-info/QuizInfo.styles.ts
@@ -7,17 +7,29 @@ const styles = {
     flexDirection: 'row',
     alignItems: 'center',
     gap: '48px',
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      gap: theme.spacing(2)
+    }
   },
   divider: {
     borderWidth: '1px',
     height: '32px',
-    color: 'var(--s2s-neutral-250)'
+    color: 'var(--s2s-neutral-250)',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    }
   },
   smallDivider: {
     borderWidth: '1px',
     height: '16px',
-    color: 'var(--s2s-neutral-250)'
+    color: 'var(--s2s-neutral-250)',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+      gap: theme.spacing(2)
+    }
   },
   questionsAnsweredWrapper: {
     display: 'flex',
@@ -34,7 +46,10 @@ const styles = {
   },
   buttonWrapper: {
     marginLeft: 'auto',
-    marginTop: theme.spacing(1)
+    marginTop: theme.spacing(1),
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: 0
+    }
   }
 }
 
