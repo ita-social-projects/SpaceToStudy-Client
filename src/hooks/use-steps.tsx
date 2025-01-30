3fd4ba27cbd8275091eda18046160a13372157f7
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2025-01-30 22:08:52 +0200

    fix sonar issue

--- src/hooks/use-steps.tsx
+++ src/hooks/use-steps.tsx
@@ -92,7 +92,7 @@ const useSteps = ({ steps }: UseSteps) => {
         country: country ?? '',
         city: city ?? ''
       },
-      professionalSummary: professionalSummary
+      professionalSummary: professionalSummary?.length
         ? professionalSummary
         : undefined,
       mainSubjects: stepData.subjects,
