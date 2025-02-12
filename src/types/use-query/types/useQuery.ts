db942cc92d49b17997808342d4f7c5cc07349701
Mike-Popovych <volt251988@gmail.com>
2025-02-13 00:53:09 +0200

    removed unnecessary type

--- src/types/use-query/types/useQuery.ts
+++
@@ -1,6 +0,0 @@
-import type { QueryObserverResult } from '@tanstack/react-query'
-import type { ResponseError } from '~/exceptions'
-
-export type QueryRefetch<T, E = ResponseError> = () => Promise<
-  QueryObserverResult<T, E>
->
