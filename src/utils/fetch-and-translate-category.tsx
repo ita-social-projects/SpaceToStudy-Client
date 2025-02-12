388bbc7550f59a1ea6f4d467d67e4fd0f5774d55
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-02-12 21:12:27 +0200

    Refactored fetch-and-translate-categories to use updated service

--- src/utils/fetch-and-translate-category.tsx
+++ src/utils/fetch-and-translate-category.tsx
@@ -1,28 +1,22 @@
-import { AxiosHeaders, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 import { titleToCamel } from './title-to-camel-case'
+import { ServiceFunctionNew } from '~/types'
 
 export async function fetchAndTranslateData<T extends { name: string }>(
-  serviceMethod: () => Promise<AxiosResponse<T[]>>,
+  serviceMethod: ServiceFunctionNew<T[]>,
   translationKey: string,
   t: (key: string, options?: { defaultValue: string }) => string
-): Promise<AxiosResponse<Array<T & { displayName: string }>>> {
+): Promise<Array<T & { displayName: string }>> {
   try {
     const response = await serviceMethod()
-    const translatedData = response.data.map((item) => ({
+    const translatedData = response.map((item) => ({
       ...item,
       displayName: t(`${translationKey}.${titleToCamel(item.name)}`, {
         defaultValue: item.name
       })
     }))
-    return { ...response, data: translatedData }
+    return translatedData
   } catch (error: unknown) {
     console.error(`Error fetching ${translationKey}:`, error)
-    return {
-      data: [],
-      status: 500,
-      statusText: 'Error',
-      headers: new AxiosHeaders(),
-      config: {} as InternalAxiosRequestConfig
-    } as AxiosResponse<Array<T & { displayName: string }>>
+    return []
   }
 }
