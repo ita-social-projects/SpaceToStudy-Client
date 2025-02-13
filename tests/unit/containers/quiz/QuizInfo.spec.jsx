b639c15aeb19358089a8b4ec2859d5dc29cf68ae
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2025-02-13 15:23:24 +0200

    Fixed tests in QuizInfo.spec.jsx

--- tests/unit/containers/quiz/QuizInfo.spec.jsx
+++ tests/unit/containers/quiz/QuizInfo.spec.jsx
@@ -1,49 +1,19 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { describe, it, expect } from 'vitest'
 import { renderWithProviders } from '~tests/test-utils'
 import {
-  UngradedQuizInfo,
-  GradedQuizInfo,
+  TutorQuizInfo,
   StartViewQuizInfo
 } from '~/containers/quiz/quiz-info/QuizInfo'
 
 import { QuizAttempt, QuizTimeLimit } from '~/types'
 
-describe('UngradedQuizInfo', () => {
+describe('TutorQuizInfo', () => {
   beforeEach(() => {
-    renderWithProviders(<UngradedQuizInfo />)
-  })
-
-  it('should render ungraded quiz info with a proper content', () => {
-    
-    const attemptFinished = screen.getByText((_, element) => {
-      return (
-        element?.textContent?.includes('quiz.attemptFinished') &&
-        element.tagName.toLowerCase() === 'p'
-      )
-    })
-
-    expect(attemptFinished).toBeInTheDocument()
-
-    const duration = screen.getByText((_, element) => {
-      return (
-        element?.textContent?.includes('quiz.duration') &&
-        element.tagName.toLowerCase() === 'p'
-      )
-    })
-
-    expect(duration).toBeInTheDocument()
-    expect(screen.getByText('quiz.evaluate')).toBeInTheDocument()
-  })
-})
-
-describe('GradedQuizInfo', () => {
-  beforeEach(() => {
-    renderWithProviders(<GradedQuizInfo />)
+    renderWithProviders(<TutorQuizInfo />)
   })
 
   it('should render graded quiz info with a proper content', () => {
-
     expect(screen.getByText('quiz.points')).toBeInTheDocument()
     expect(screen.getByText('quiz.save')).toBeInTheDocument()
   })
@@ -67,26 +37,36 @@ describe('StartViewQuizInfo', () => {
     expect(screen.getByText('quiz.attemptLimit:')).toBeInTheDocument()
     expect(screen.getByText('0/2')).toBeInTheDocument()
     expect(screen.getByText('quiz.timeLimit:')).toBeInTheDocument()
-    expect(screen.getByText('myResourcesPage.quizzes.types.minute30')).toBeInTheDocument()
+    expect(
+      screen.getByText('myResourcesPage.quizzes.types.minute30')
+    ).toBeInTheDocument()
     expect(screen.getByText('quiz.startQuiz')).toBeInTheDocument()
   })
 
   it('should call handleStartButton on start button click', () => {
-    renderWithProviders(<StartViewQuizInfo {...defaultProps}
-      attempts={QuizAttempt.Attempt1}
-      timeLimit={QuizTimeLimit.NoLimit}/>)
+    renderWithProviders(
+      <StartViewQuizInfo
+        {...defaultProps}
+        attempts={QuizAttempt.Attempt1}
+        timeLimit={QuizTimeLimit.NoLimit}
+      />
+    )
 
     const button = screen.getByText('quiz.startQuiz')
     fireEvent.click(button)
 
-    expect(mockHandleStartButton).toHaveBeenCalledWith(false)
+    expect(mockHandleStartButton).toHaveBeenCalledOnce()
   })
 
   it('should render try again button if not first attempt', () => {
-    renderWithProviders(<StartViewQuizInfo {...defaultProps}
-      attempts={QuizAttempt.Attempt3}
-      timeLimit={QuizTimeLimit.NoLimit}
-      usedAttempts={2}/>)
+    renderWithProviders(
+      <StartViewQuizInfo
+        {...defaultProps}
+        attempts={QuizAttempt.Attempt3}
+        timeLimit={QuizTimeLimit.NoLimit}
+        usedAttempts={2}
+      />
+    )
 
     expect(screen.getByText('quiz.tryAgain')).toBeInTheDocument()
   })
