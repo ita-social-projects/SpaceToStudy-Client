73bef19329812be4cfa5db89c27dd0b0642e5398
Vadym Pavlyk <vadim.pavlyk2002@gmail.com>
2025-02-18 10:22:29 +0200

    Removed regexp

--- tests/unit/pages/quiz-preview/QuizPreview.spec.jsx
+++ tests/unit/pages/quiz-preview/QuizPreview.spec.jsx
@@ -48,15 +48,13 @@ describe('QuizPage for student', () => {
       .onGet(URLs.quizzes.getById.replace(':id', ''))
       .reply(200, mockQuiz)
     mockAxiosClient
-      .onGet(new RegExp(URLs.quizzes.getById.replace(':id', mockQuizId)))
+      .onGet(URLs.quizzes.getById.replace(':id', mockQuizId))
       .reply(200, mockQuiz)
     mockAxiosClient
       .onGet(URLs.finishedQuizzes.getById.replace(':id', ''))
       .reply(200, mockQuiz)
     mockAxiosClient
-      .onGet(
-        new RegExp(URLs.finishedQuizzes.getById.replace(':id', mockQuizId))
-      )
+      .onGet(URLs.finishedQuizzes.getById.replace(':id', mockQuizId))
       .reply(200, mockQuiz)
   })
 
