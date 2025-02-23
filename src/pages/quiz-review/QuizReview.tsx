f559746944759b7dc0ecc4c590fcd73e869dcea7
markgol777 <markgol777@gmail.com>
2025-02-23 18:44:35 +0100

    Renamed and changed import

--- src/pages/quiz-review/QuizReview.tsx
+++ src/pages/quiz-review/QuizReview.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
-import { styles } from '~/pages/quiz-review/QuizPreview.styles'
+import { styles } from '~/pages/quiz-review/QuizReview.styles'
 import { FinishedQuiz } from '~/pages/quiz/QuizVariants'
 
 const QuizReview: React.FC = () => {
