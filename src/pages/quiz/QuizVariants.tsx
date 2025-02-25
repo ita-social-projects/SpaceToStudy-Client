6d68948bab46800b02dc27ad32c609e17bfd88e6
markgol777 <markgol777@gmail.com>
2025-02-25 16:02:27 +0100

    Added spacing between if's

--- src/pages/quiz/QuizVariants.tsx
+++ src/pages/quiz/QuizVariants.tsx
@@ -277,6 +277,7 @@ const FinishedQuiz: React.FC<FinishedQuizProps> = ({ finishedQuizId }) => {
     finishedQuiz?.results?.forEach(({ question, answers }) => {
       const quizQuestion = quiz?.items.find((item) => item.text === question)
       const quizQuestionId = quizQuestion?._id
+
       if (quizQuestionId) {
         const chosenAnswers = answers.filter((answer) => answer.isChosen)
         const textAnswers = chosenAnswers.map(
