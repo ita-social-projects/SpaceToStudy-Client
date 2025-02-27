14b450bd0c40ededa2910fada2d6385f5b760741
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-02-27 21:03:51 +0200

    Refactored unit tests for use-categories-names

--- tests/unit/hooks/use-categories-names.spec.jsx
+++ tests/unit/hooks/use-categories-names.spec.jsx
@@ -2,12 +2,10 @@ import { afterEach, vi } from 'vitest'
 import { renderHook, waitFor } from '@testing-library/react'
 import useCategoriesNames from '~/hooks/use-categories-names'
 import { categoryService } from '~/services/category-service'
-import { baseService } from '~/services/base-service'
 import QueryProvider from '~/QueryProvider'
 import { queryClient } from '~/plugins/queryClient'
-
-vi.mock('~/services/category-service')
-vi.mock('~/services/base-service')
+import { mockAxiosClient } from '~tests/test-utils'
+import { URLs } from '~/constants/request'
 
 const mockCategoriesNames = [
   { _id: '1', name: 'Category 1' },
@@ -26,18 +24,23 @@ describe('useCategoriesNames', () => {
   })
 
   it('fetches categories names successfully', async () => {
-    categoryService.getCategoriesNames.mockResolvedValueOnce(
-      mockCategoriesNames
+    mockAxiosClient
+      .onGet(URLs.categories.getNames)
+      .reply(200, mockCategoriesNames)
+
+    const getCategoriesNamesSpy = vi.spyOn(
+      categoryService,
+      'getCategoriesNames'
     )
 
     const { result } = renderHook(() => useCategoriesNames(), {
       wrapper: QueryProvider
     })
 
-    expect(result.current.loading).toBe(false)
+    expect(result.current.loading).toBe(true)
     expect(result.current.response).toEqual([])
 
-    expect(categoryService.getCategoriesNames).toHaveBeenCalled()
+    expect(getCategoriesNamesSpy).toHaveBeenCalled()
 
     await waitFor(() => {
       expect(result.current.loading).toBe(false)
@@ -46,20 +49,28 @@ describe('useCategoriesNames', () => {
   })
 
   it('handles API errors', async () => {
-    baseService.request.mockRejectedValueOnce({ response: { data: mockError } })
+    mockAxiosClient.onGet(URLs.categories.getNames).reply(400, mockError)
+
+    const getCategoriesNamesSpy = vi.spyOn(
+      categoryService,
+      'getCategoriesNames'
+    )
 
     const { result } = renderHook(() => useCategoriesNames(), {
       wrapper: QueryProvider
     })
 
-    expect(result.current.loading).toBe(false)
+    expect(result.current.loading).toBe(true)
     expect(result.current.response).toEqual([])
 
-    expect(categoryService.getCategoriesNames).toHaveBeenCalled()
+    expect(getCategoriesNamesSpy).toHaveBeenCalled()
 
-    await waitFor(() => {
-      expect(result.current.loading).toBe(false)
-      expect(result.current.error).not.toEqual([])
-    })
-  })
+    await waitFor(
+      () => {
+        expect(result.current.loading).toBe(false)
+        expect(result.current.error.message).toEqual(mockError.message)
+      },
+      { timeout: 8000 }
+    )
+  }, 8000)
 })
