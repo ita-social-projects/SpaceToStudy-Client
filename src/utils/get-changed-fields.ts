026f8fe5576cf4097011d7292de54a4773bfe74a
ktr <pavlobutynez@gmail.com>
2025-02-27 17:04:21 +0200

    changed LF to CRLF

--- src/utils/get-changed-fields.ts
+++ src/utils/get-changed-fields.ts
@@ -1,1 +1,18 @@
-export function getChangedFields<T extends Record<string, unknown>>(  initialState: T,  currentState: T): Partial<T> {  const changes: Partial<T> = {}  Object.keys(currentState).forEach((key) => {    const typedKey = key as keyof T    const initialValue = initialState[typedKey]    const currentValue = currentState[typedKey]    if (JSON.stringify(initialValue) !== JSON.stringify(currentValue)) {      changes[typedKey] = currentValue    }  })  return changes}
+export function getChangedFields<T extends Record<string, unknown>>(
+  initialState: T,
+  currentState: T
+): Partial<T> {
+  const changes: Partial<T> = {}
+
+  Object.keys(currentState).forEach((key) => {
+    const typedKey = key as keyof T
+    const initialValue = initialState[typedKey]
+    const currentValue = currentState[typedKey]
+
+    if (JSON.stringify(initialValue) !== JSON.stringify(currentValue)) {
+      changes[typedKey] = currentValue
+    }
+  })
+
+  return changes
+}
