b7b6aad0a7e5eb11ee260b89f1e4cb7078c50d12
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2025-03-07 15:49:10 +0200

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,7 +22,13 @@ const compat = new FlatCompat({
 export default [
   ...pluginQuery.configs['flat/recommended'],
   {
-    ignores: ['src/assets/*', 'src/stories/assets/*', '.storybook/**/*']
+    ignores: [
+      'src/assets/*',
+      'src/stories/assets/*',
+      '.storybook/**/*',
+      'tests/coverage/**/*',
+      'dist/**/*'
+    ]
   },
   ...compat.extends(
     'eslint:recommended',
@@ -132,11 +138,19 @@ export default [
     }
   },
   {
-    files: ['**/tests/**/*.*.js', '**/tests/**/*.js'],
+    files: [
+      '**/tests/**/*.*.js',
+      '**/tests/**/*.js',
+      '**/tests/**/*.*.jsx',
+      '**/tests/**/*.jsx'
+    ],
     languageOptions: {
       globals: {
         ...globals.jest
       }
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'off'
     }
   },
   {
