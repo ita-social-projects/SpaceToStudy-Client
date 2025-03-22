cd640884f6627172ac95f1858edd5738009af96d
nebby2105 <lopatkaoa@gmail.com>
2025-03-23 00:08:18 +0200

    changed === to !== in ProfileInfo

--- src/containers/user-profile/profile-info/ProfileInfo.tsx
+++ src/containers/user-profile/profile-info/ProfileInfo.tsx
@@ -165,7 +165,7 @@ const ProfileInfo = ({ userData, myRole }: ProfileInfoProps) => {
       >
         {t(
           `userProfilePage.profileInfo.${
-            myRole === Student ? 'studentRequests' : 'tutorOffers'
+            myRole !== Student ? 'studentRequests' : 'tutorOffers'
           }`
         )}
       </Button>
