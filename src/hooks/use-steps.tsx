52312ac98925457c2f766e40181144da97a6cfe3
Sofiia13 <sofiia.yevush@gmail.com>
2025-03-25 17:30:46 +0200

    add photo handling

--- src/hooks/use-steps.tsx
+++ src/hooks/use-steps.tsx
@@ -5,7 +5,12 @@ import { useModalContext } from '~/context/modal-context'
 import { useStepContext } from '~/context/step-context'
 import { userService } from '~/services/user-service'
 import { snackbarVariants } from '~/constants'
-import { type StepData, type UpdateUserParams } from '~/types'
+import {
+  type EditProfilePhoto,
+  type UpdatedPhoto,
+  type StepData,
+  type UpdateUserParams
+} from '~/types'
 import { openAlert } from '~/redux/features/snackbarSlice'
 import useSnackbarAlert from '~/hooks/use-snackbar-alert'
 
@@ -74,8 +79,23 @@ const useSteps = ({ steps }: UseSteps) => {
     const { firstName, lastName, country, city, professionalSummary } =
       stepData.generalInfo.data
 
+    let formattedPhoto: EditProfilePhoto | undefined = undefined
+
+    if (stepData.photo && stepData.photo.length > 0) {
+      const firstPhoto = stepData.photo[0]
+
+      if (firstPhoto instanceof File) {
+        formattedPhoto = {
+          src: URL.createObjectURL(firstPhoto),
+          name: firstPhoto.name
+        }
+      } else {
+        formattedPhoto = firstPhoto as UpdatedPhoto
+      }
+    }
+
     const data: UpdateUserParams = {
-      photo: stepData.photo[0] ? stepData.photo[0] : '',
+      photo: formattedPhoto,
       firstName,
       lastName,
       address: {
@@ -101,7 +121,13 @@ const useSteps = ({ steps }: UseSteps) => {
     setActiveStep
   }
 
-  return { activeStep, stepErrors, isLastStep, stepOperation, loading }
+  return {
+    activeStep,
+    stepErrors,
+    isLastStep,
+    stepOperation,
+    loading
+  }
 }
 
 export default useSteps
