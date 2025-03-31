35ac4b5075b8bcc1e69bc6707dae3105bc8fd969
nebby2105 <lopatkaoa@gmail.com>
2025-03-31 19:09:29 +0300

    add translations for cooperationDetailsText in comments

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import { useTranslation } from 'react-i18next'
 
 import AppRating from '~/components/app-rating/AppRating'
 import UserProfileInfo from '~/components/user-profile-info/UserProfileInfo'
@@ -9,6 +10,8 @@ import { ReviewResponse, UserRoleEnum } from '~/types'
 import { styles } from '~/components/comment/Comment.styles'
 import { Link } from 'react-router-dom'
 import { authRoutes } from '~/router/constants/authRoutes'
+
+import { titleToCamel } from '~/utils/title-to-camel-case'
 
 interface CommentProps {
   review: ReviewResponse
@@ -26,13 +29,29 @@ const Comment: FC<CommentProps> = ({ review }) => {
   } = review
   const { firstName, lastName, photo, _id } = author
   const { category, subject } = offer
+  const { t } = useTranslation()
 
   const authorRole =
     targetUserRole === UserRoleEnum.Tutor
       ? UserRoleEnum.Student
       : UserRoleEnum.Tutor
 
-  const cooperationDetailsText = `${category.name} - ${subject.name} - ${proficiencyLevel}`
+  const translatedCategory = t(`categories.${titleToCamel(category.name)}`, {
+    defaultValue: category.name
+  })
+
+  const translatedSubject = t(`subjects.${titleToCamel(subject.name)}`, {
+    defaultValue: subject.name
+  })
+
+  const translatedProficiencyLevel = t(
+    `common.levels.${titleToCamel(proficiencyLevel)}`,
+    {
+      defaultValue: proficiencyLevel
+    }
+  )
+
+  const cooperationDetailsText = `${translatedCategory} - ${translatedSubject} - ${translatedProficiencyLevel}`
 
   return (
     <Box sx={styles.root}>
