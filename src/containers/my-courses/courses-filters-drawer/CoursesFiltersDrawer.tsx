8f40367e98fb40149e0d7448bffb76efee2293ef
Daniil Bokach <daniilbokach@gmail.com>
2025-04-01 17:51:51 +0300

    Refactor filter change handler in CoursesFiltersDrawer

--- src/containers/my-courses/courses-filters-drawer/CoursesFiltersDrawer.tsx
+++ src/containers/my-courses/courses-filters-drawer/CoursesFiltersDrawer.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useCallback, SyntheticEvent } from 'react'
+import { FC, ReactNode, useCallback, SyntheticEvent, ChangeEvent } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import FilterListIcon from '@mui/icons-material/FilterList'
@@ -80,6 +80,13 @@ const CoursesFiltersDrawer: FC<CoursesFiltersDrawerProps> = ({
     updateFiltersInQuery(additionalParams)
     onClose()
   }
+
+  const handleFilterChange =
+    <K extends keyof CourseFilters>(key: K) =>
+    (event: ChangeEvent<HTMLInputElement>) => {
+      updateFilterByKey(key)(event.target.value as CourseFilters[K])
+    }
+
   return (
     <AppDrawer anchor={PositionEnum.Left} onClose={onClose} open={isOpen}>
       <Box sx={styles.titleWithIcon}>
@@ -160,7 +167,7 @@ const CoursesFiltersDrawer: FC<CoursesFiltersDrawerProps> = ({
         {t('myCoursesPage.coursesFilter.search')}:
       </Typography>
       <FilterInput
-        onChange={updateFilterByKey('title')}
+        onChange={handleFilterChange('title')}
         placeholder={t('common.search')}
         value={filters.title}
       />
