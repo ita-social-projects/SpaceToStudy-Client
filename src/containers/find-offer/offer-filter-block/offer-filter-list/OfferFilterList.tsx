175d1cf4992635ba1a928d7e66d8a6a95457c563
Iarynovskyi <dimayar20061@gmail.com>
2025-04-04 00:04:35 +0300

    fix bug with unreachable radio buttons

--- src/containers/find-offer/offer-filter-block/offer-filter-list/OfferFilterList.tsx
+++ src/containers/find-offer/offer-filter-block/offer-filter-list/OfferFilterList.tsx
@@ -117,7 +117,7 @@ const OfferFilterList: FC<OfferFilterListProps> = ({
       {filterTitle(t('findOffers.filterTitles.rating'))}
       <RadioButtonInputs
         items={radioOptions}
-        onChange={handleFilterChange('rating')}
+        onChange={updateFilterByKey('rating')}
         value={Number(filters.rating)}
       />
       {!isLaptopAndAbove && (
