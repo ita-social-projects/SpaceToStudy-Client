d2d19c4ee2f0d87f67efd2e735ebf3411fd78657
Daniil Bokach <daniilbokach@gmail.com>
2025-04-03 12:58:01 +0300

    Cleared comments in FilterInput component

--- tests/unit/components/filter-input/FilterInput.spec.jsx
+++ tests/unit/components/filter-input/FilterInput.spec.jsx
@@ -22,9 +22,8 @@ describe('FilterInput', () => {
     const input = getByRole('textbox')
     fireEvent.change(input, { target: { value: 'test' } })
 
-    // Проверяем, что handleChange был вызван с строкой 'test', а не с объектом
     const firstArgument = handleChange.mock.calls[0][0]
-    expect(firstArgument).toBe('test') // Ожидаем строку, а не объект события
+    expect(firstArgument).toBe('test')
   })
 
   it('clears the input when the clear button is clicked', () => {
@@ -34,8 +33,7 @@ describe('FilterInput', () => {
     const clearButton = getByTestId('clear-button')
     fireEvent.click(clearButton)
 
-    // Ожидаем, что handleChange был вызван с пустой строкой, а не объектом
     expect(handleChange).toHaveBeenCalledTimes(1)
-    expect(handleChange).toHaveBeenCalledWith('') // Ожидаем, что будет передана пустая строка
+    expect(handleChange).toHaveBeenCalledWith('')
   })
 })
