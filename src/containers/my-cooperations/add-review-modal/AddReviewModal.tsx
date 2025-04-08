856acb3a86779ed316e09d4b999bb825a1445c10
nebby2105 <lopatkaoa@gmail.com>
2025-04-08 17:40:26 +0300

    fix comments

--- src/containers/my-cooperations/add-review-modal/AddReviewModal.tsx
+++ src/containers/my-cooperations/add-review-modal/AddReviewModal.tsx
@@ -21,9 +21,16 @@ import {
 
 import {
   ComponentEnum,
+<<<<<<< HEAD
   type ReviewDataFromCooperation,
   type ReviewData,
   type ErrorResponse
+=======
+  ReviewDataFromCooperation,
+  ReviewData,
+  ErrorResponse
+  // DataFromCooperation
+>>>>>>> cf70fbd9 (fix comments)
 } from '~/types'
 import { styles } from '~/containers/my-cooperations/add-review-modal/AddReviewModal.styles'
 import useMutation from '~/hooks/use-mutation'
