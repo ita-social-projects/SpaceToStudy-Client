b2159643222f54ad668468481ea9af34ff04d7bc
nika <vvvveronikaaaa666@gmail.com>
2025-04-13 13:00:15 +0300

    upgraded certain packages

--- src/plugins/axiosClient.ts
+++ src/plugins/axiosClient.ts
@@ -4,6 +4,7 @@ import qs from 'qs'
 export const axiosClient: AxiosInstance = axios.create({
   withCredentials: true,
   baseURL: import.meta.env.VITE_API_BASE_PATH,
+  allowAbsoluteUrls: false,
   paramsSerializer: (params) => {
     return encodeURI(qs.stringify(params, { encode: false }))
   }
