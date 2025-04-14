7eb99e7c5a6d2b1a0cd94258b56e99b1a0726671
Iryna Khylchuk <khylchuk@icloud.com>
2025-04-14 18:03:12 +0300

    Implemented tests for "Last name"

--- tests/unit/containers/edit-profile/ProfileTab.spec.jsx
+++ tests/unit/containers/edit-profile/ProfileTab.spec.jsx
@@ -233,4 +233,35 @@ describe('ProfileTab', () => {
     expect(errorMessage).toBeInTheDocument()
     expect(lastNameInput).toHaveValue(tooManyCharacters)
   })
+
+  it('should show an error when "Last name" empty', async () => {
+    renderWithMockData()
+
+    const lastNameInput = screen.getByPlaceholderText('lastName')
+
+    await userEvent.clear(lastNameInput)
+    expect(lastNameInput.value).toBe('')
+
+    const errorMessage = await screen.findByText(
+      /common.errorMessages.emptyField/i
+    )
+    expect(errorMessage).toBeInTheDocument()
+    expect(lastNameInput).toHaveAttribute('aria-invalid', 'true')
+  })
+
+  it('should show an error when "Last name" contains only spaces', async () => {
+    renderWithMockData()
+
+    const lastNameInput = screen.getByPlaceholderText('lastName')
+
+    await userEvent.clear(lastNameInput)
+    await userEvent.type(lastNameInput, '   ')
+    expect(lastNameInput.value).toBe('   ')
+
+    const errorMessage = await screen.findByText(
+      /common.errorMessages.hasOnlySpaces/i
+    )
+    expect(errorMessage).toBeInTheDocument()
+    expect(lastNameInput).toHaveAttribute('aria-invalid', 'true')
+  })
 })
