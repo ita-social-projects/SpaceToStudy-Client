4b216f92cd99ea0b9322b858b7aff0f012ac9bb4
Iarynovskyi <dimayar20061@gmail.com>
2025-04-16 20:54:26 +0300

    add some tests for changed files

--- tests/unit/containers/find-offer/create-new-subject/CreateNewSubject.spec.jsx
+++ tests/unit/containers/find-offer/create-new-subject/CreateNewSubject.spec.jsx
@@ -1,9 +1,8 @@
 import { fireEvent, screen } from '@testing-library/react'
-
+import { describe, it, beforeEach, vi } from 'vitest'
 import { ConfirmationDialogProvider } from '~/context/confirm-context'
-import CreateSubjectModal from '~/containers/find-offer/create-new-subject/CreateNewSubject'
+import CreateSubjectModal from '../../../../../src/containers/find-offer/create-subject/CreateSubject'
 import { categoryService } from '~/services/category-service'
-import { beforeEach, vi } from 'vitest'
 import { TestSnackbar, renderWithProviders } from '~tests/test-utils'
 
 vi.mock('~/services/category-service')
@@ -12,10 +11,11 @@ const mockSubjectsNames = [
   { _id: '1', name: 'Category 1' },
   { _id: '2', name: 'Category 2' }
 ]
-categoryService.getCategoriesNames.mockResolvedValue(mockSubjectsNames)
 
 describe('CreateSubjectModal container', () => {
   beforeEach(() => {
+    categoryService.getCategoriesNames.mockResolvedValue(mockSubjectsNames)
+
     renderWithProviders(
       <ConfirmationDialogProvider>
         <TestSnackbar>
@@ -27,28 +27,32 @@ describe('CreateSubjectModal container', () => {
 
   it('should render component', () => {
     const title = screen.getByText('categoriesPage.newSubject.title')
-
     expect(title).toBeInTheDocument()
   })
-  it('should change autocomplete by choosing option', () => {
+
+  it('should allow selecting and clearing category input', () => {
     const categoryInput = screen.getByLabelText('offerPage.labels.category')
 
-    expect(categoryInput).toBeInTheDocument()
-
-    fireEvent.click(categoryInput)
-    fireEvent.change(categoryInput, {
-      target: { value: 'Category 1' }
-    })
-    fireEvent.keyDown(categoryInput, { key: 'Escape' })
-
+    fireEvent.change(categoryInput, { target: { value: 'Category 1' } })
     expect(categoryInput.value).toBe('Category 1')
 
-    fireEvent.click(categoryInput)
-    fireEvent.change(categoryInput, {
-      target: { value: '' }
-    })
-    fireEvent.keyDown(categoryInput, { key: 'Enter' })
-
+    fireEvent.change(categoryInput, { target: { value: '' } })
     expect(categoryInput.value).toBe('')
+  })
+
+  it('should submit form with correct values', async () => {
+    const nameInput = screen.getByLabelText(
+      'categoriesPage.newSubject.labels.subject'
+    )
+    const categoryInput = screen.getByLabelText('offerPage.labels.category')
+    const submitBtn = screen.getByRole('button', {
+      name: /button.sendRequest/i
+    })
+
+    fireEvent.change(nameInput, { target: { value: 'Math' } })
+    fireEvent.change(categoryInput, { target: { value: 'Category 1' } })
+
+    fireEvent.click(submitBtn)
+    expect(screen.getByRole('combobox')).toHaveValue('Category 1')
   })
 })
