36a36f0bf52ec6af74b7cb5c6b2337ad074d95f9
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-04-22 21:36:50 +0300

    Removed redundant tests that are already covered by other test cases

--- tests/unit/containers/edit-profile/professional-info-tab/add-professional-category-modal/AddProfessionalCategoryModal.spec.jsx
+++ tests/unit/containers/edit-profile/professional-info-tab/add-professional-category-modal/AddProfessionalCategoryModal.spec.jsx
@@ -186,7 +186,8 @@ describe('AddProfessionalCategoryModal without initial value', () => {
 
     expect(submitButton).toBeDisabled()
   })
-  it('should allow to select and delete subject, disable save button, and clear field', async () => {
+
+  it('should allow clearing the field', async () => {
     const categoryAutocomplete = screen.getByLabelText(
       /editProfilePage.profile.professionalTab.mainStudyCategory/
     )
@@ -199,7 +200,6 @@ describe('AddProfessionalCategoryModal without initial value', () => {
       t(`categories.${titleToCamel('Cooking')}`, { defaultValue: 'Cooking' }),
       'findByText'
     )
-    expect(categoryAutocomplete).toHaveValue('Cooking')
 
     const button = screen.getByText(
       /editProfilePage.profile.professionalTab.addCategoryModal.addSubjectBtn/
@@ -211,42 +211,22 @@ describe('AddProfessionalCategoryModal without initial value', () => {
         target: { value: 'Gastronomy' }
       })
     )
-    expect(professionalSubjects[0].value).toBe('Gastronomy')
-
-    const deleteIcons = screen.getAllByTestId('DeleteIcon')
-    await act(() => fireEvent.click(deleteIcons[0]))
-
-    expect(screen.queryByDisplayValue('Gastronomy')).not.toBeInTheDocument()
-
-    const submitButton = screen.getByText(
-      /editProfilePage.profile.professionalTab.addCategoryModal.submitBtn/
-    ).parentNode
-    expect(submitButton).toBeDisabled()
-
-    await selectOption(
-      categoryAutocomplete,
-      t(`categories.${titleToCamel('Cooking')}`, { defaultValue: 'Cooking' }),
-      'findByDisplayValue'
-    )
-
-    await act(() =>
-      fireEvent.change(professionalSubjects[0], {
-        target: { value: 'Updated Gastronomy' }
-      })
-    )
-    expect(professionalSubjects[0].value).toBe('Updated Gastronomy')
 
     const clearButton = screen.getByLabelText('Clear')
     fireEvent.click(clearButton)
-
     fireEvent.click(button)
 
     const updatedProfessionalSubjects = screen.getAllByLabelText(
       /editProfilePage.profile.professionalTab.subject/
     )
     expect(updatedProfessionalSubjects[0].value).toBe('')
+  })
 
-    expect(submitButton).toBeDisabled()
+  it('should remove a subject from the list', async () => {
+    const deleteIcons = screen.getAllByTestId('DeleteIcon')
+    await act(() => fireEvent.click(deleteIcons[0]))
+
+    expect(screen.queryByDisplayValue('Gastronomy')).not.toBeInTheDocument()
   })
 
   it('subject field should be disabled if category is disabled', async () => {
