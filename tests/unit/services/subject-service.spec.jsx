07e6a0a029999ab3cfb412b3483c9e55e26d4059
Iarynovskyi <dimayar20061@gmail.com>
2025-04-22 22:21:02 +0300

    hid from me but I found it

--- tests/unit/services/subject-service.spec.jsx
+++ tests/unit/services/subject-service.spec.jsx
@@ -1,4 +1,4 @@
-import { afterEach, vi } from 'vitest'
+import { vi } from 'vitest'
 import { mockAxiosClient } from '~tests/test-utils'
 import { subjectService } from '~/services/subject-service'
 import { URLs } from '~/constants/request'
