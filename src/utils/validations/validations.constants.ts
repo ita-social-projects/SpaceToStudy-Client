08611e38e5e65d5394bc1cbd2a14d70c8ceaed54
nika <vvvveronikaaaa666@gmail.com>
2025-04-25 18:29:02 +0300

    lint fix

--- src/utils/validations/validations.constants.ts
+++ src/utils/validations/validations.constants.ts
@@ -1,7 +1,8 @@
 export const validationPatterns = {
   name: /^[a-zа-яєії' -]+$/i,
   number: /^-?(?:\d+|\d*\.\d+)(?:[eE][+-]?\d+)?$/,
-  email: /^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/,
+  email:
+    /^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/,
   passwordValid: /^\S+$/i,
   passwordAlphabeticAndNumeric: /^(?=.*[a-zа-яєії])(?=.*\d).+$/i
 }
