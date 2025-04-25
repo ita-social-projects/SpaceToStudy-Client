e0744e50ffaafa1fdbf6339a90cdbe95d7f4926c
nika <vvvveronikaaaa666@gmail.com>
2025-04-25 18:16:57 +0300

    modified regex

--- src/utils/validations/validations.constants.ts
+++ src/utils/validations/validations.constants.ts
@@ -1,7 +1,7 @@
 export const validationPatterns = {
   name: /^[a-zа-яєії' -]+$/i,
   number: /^-?(?:\d+|\d*\.\d+)(?:[eE][+-]?\d+)?$/,
-  email: /^([a-z\d]+([._-][a-z\d]+)*)@([a-z\d]+([.-][a-z\d]+)*\.[a-z]{2,})$/i,
+  email: /^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/,
   passwordValid: /^\S+$/i,
   passwordAlphabeticAndNumeric: /^(?=.*[a-zа-яєії])(?=.*\d).+$/i
 }
